3822e9ee81bf9602fb27ab20435e6131e2a40fff
Sam Kavanagh <hganavak@gmail.com>
2020-05-27 17:11:30 +1200

    Initial package package installations

--- research-hub-web/src/app/routing/routing.ts
+++ research-hub-web/src/app/routing/routing.ts
@@ -1,18 +1,19 @@
-import {Routes} from '@angular/router';
-import {CanActivateViaAuthGuard} from './routing.can-activate-via-auth-guard';
+import { Routes } from '@angular/router';
+import { CanActivateViaAuthGuard } from './routing.can-activate-via-auth-guard';
+import { AuthGuard, LoginSuccessGuard } from 'uoa-auth-angular';
 
 
 export const appRoutes: Routes = [
-  {path: 'home',  loadChildren: 'app/components/home/home.module#HomeModule'},
-  {path: 'search', loadChildren: 'app/components/search-results/search-results.module#SearchResultsModule'},
+  { path: 'home', loadChildren: 'app/components/home/home.module#HomeModule' },
+  { path: 'search', loadChildren: 'app/components/search-results/search-results.module#SearchResultsModule' },
 
-  {path: 'feedback', loadChildren: 'app/components/feedback/feedback.module#FeedbackModule'},
-  {path: 'userStudy', loadChildren: 'app/components/user-study/user-study.module#UserStudyModule'},
-  {path: 'about', loadChildren: 'app/components/about/about.module#AboutModule'},
-  {path: 'contact', loadChildren: 'app/components/contact/contact.module#ContactModule'},
+  { path: 'feedback', loadChildren: 'app/components/feedback/feedback.module#FeedbackModule' },
+  { path: 'userStudy', loadChildren: 'app/components/user-study/user-study.module#UserStudyModule' },
+  { path: 'about', loadChildren: 'app/components/about/about.module#AboutModule' },
+  { path: 'contact', loadChildren: 'app/components/contact/contact.module#ContactModule' },
 
-  {path: 'orgUnit/:orgUnitId', loadChildren: 'app/components/org-unit-details/org-unit-details.module#OrgUnitDetailsModule'},
-  {path: 'person/:personId', loadChildren: 'app/components/person-details/person-details.module#PersonDetailsModule'},
+  { path: 'orgUnit/:orgUnitId', loadChildren: 'app/components/org-unit-details/org-unit-details.module#OrgUnitDetailsModule' },
+  { path: 'person/:personId', loadChildren: 'app/components/person-details/person-details.module#PersonDetailsModule' },
 
   /**
    * Custom Route Redirects:
@@ -25,11 +26,12 @@ export const appRoutes: Routes = [
     pathMatch: 'full'
   },
 
-  {path: 'content/:contentId',  loadChildren: 'app/components/content-details/content-details.module#ContentDetailsModule'},
-  {path: 'guideCategory/:guideCategoryId', loadChildren: 'app/components/guide-category/guide-category.module#GuideCategoryModule'},
+  { path: 'content/:contentId', loadChildren: 'app/components/content-details/content-details.module#ContentDetailsModule' },
+  { path: 'guideCategory/:guideCategoryId', loadChildren: 'app/components/guide-category/guide-category.module#GuideCategoryModule' },
 
-  {path: 'requestVm', loadChildren: 'app/components/request-vm/request-vm.module#RequestVmModule', canActivate: [CanActivateViaAuthGuard]},
-  {path: 'requestStorage', loadChildren: 'app/components/request-storage/request-storage.module#RequestStorageModule', canActivate: [CanActivateViaAuthGuard]},
+  { path: 'requestVm', loadChildren: 'app/components/request-vm/request-vm.module#RequestVmModule', canActivate: [CanActivateViaAuthGuard] },
+  // {path: 'requestStorage', loadChildren: 'app/components/request-storage/request-storage.module#RequestStorageModule', canActivate: [CanActivateViaAuthGuard]},
+  { path: 'requestStorage', loadChildren: 'app/components/request-storage/request-storage.module#RequestStorageModule', canActivate: [AuthGuard] },
 
   /**
    * Custom Route Handlers:
@@ -43,6 +45,6 @@ export const appRoutes: Routes = [
     data: { contentId: 73 }
   },
 
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: '**', redirectTo: '/home', pathMatch: 'full'}
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
