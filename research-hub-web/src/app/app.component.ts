96ad214dbee27f592393f3cba38bda2f7630c48b
Sam Kavanagh <hganavak@gmail.com>
2020-05-28 11:35:37 +1200

    More minor package versioning tweaks

--- research-hub-web/src/app/app.component.ts
+++ research-hub-web/src/app/app.component.ts
@@ -1,23 +1,23 @@
 
-import {filter, distinctUntilChanged} from 'rxjs/operators';
-import {Component, OnDestroy, OnInit, ViewEncapsulation, ViewChild, AfterViewInit, ElementRef, NgZone} from '@angular/core';
-import {CategoryId, OptionsService, OptionType} from './services/options.service';
-import {SearchBarService} from './components/search-bar/search-bar.service';
-import {NavigationEnd, Router} from '@angular/router';
-import {Subscription,  Observable, fromEvent } from 'rxjs';
-import {debounceTime} from 'rxjs/operators';
-import {ResearchHubApiService} from './services/research-hub-api.service';
-import {AnalyticsService} from './services/analytics.service';
-import {isPlatformBrowser} from '@angular/common';
-import {AuthService} from './services/auth.service';
-import {ChangeDetectorRef} from '@angular/core';
+import { filter, distinctUntilChanged } from 'rxjs/operators';
+import { Component, OnDestroy, OnInit, ViewEncapsulation, ViewChild, AfterViewInit, ElementRef, NgZone } from '@angular/core';
+import { CategoryId, OptionsService, OptionType } from './services/options.service';
+import { SearchBarService } from './components/search-bar/search-bar.service';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription, Observable, fromEvent } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+import { ResearchHubApiService } from './services/research-hub-api.service';
+import { AnalyticsService } from './services/analytics.service';
+import { isPlatformBrowser } from '@angular/common';
+import { AuthService } from './services/auth.service';
+import { ChangeDetectorRef } from '@angular/core';
 import * as format from 'date-fns/format';
 
 
-import {HeaderService} from './components/header/header.service';
-import {Location} from '@angular/common';
-import {AppComponentService} from './app.component.service';
-import {Title} from '@angular/platform-browser';
+import { HeaderService } from './components/header/header.service';
+import { Location } from '@angular/common';
+import { AppComponentService } from './app.component.service';
+import { Title } from '@angular/platform-browser';
 import { ScrollDispatcher } from '@angular/cdk/scrolling';
 import {
   trigger,
@@ -45,7 +45,7 @@ import {
     ])
   ]
 })
-export class AppComponent implements OnInit, OnDestroy, AfterViewInit  {
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
 
   public aucklandUniUrl = 'https://auckland.ac.nz';
   public eResearchUrl = 'http://eresearch.auckland.ac.nz';
@@ -57,8 +57,8 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit  {
   private progressBarVisibilitySub: Subscription;
   private titleSub: Subscription;
   private contentSidenavVisibilitySub: Subscription;
-  private scrollSub : Subscription;
-  private winResizeSub : Subscription;
+  private scrollSub: Subscription;
+  private winResizeSub: Subscription;
 
   public selectedCategory = CategoryId.All;
   public searchText = '';
@@ -74,22 +74,22 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit  {
   public contentSidenavHeight = 0;
 
   @ViewChild('topbar', { static: true })
-  private topbarElement : ElementRef;
+  private topbarElement: ElementRef;
 
   @ViewChild('topContent', { static: true })
-  private topContentElement : ElementRef;
+  private topContentElement: ElementRef;
 
   @ViewChild('content', { static: true })
-  private contentElement : ElementRef;
-  private contentElementHeight : number;
+  private contentElement: ElementRef;
+  private contentElementHeight: number;
 
   constructor(private location: Location, public optionsService: OptionsService, private headerService: HeaderService,
-              private searchBarService: SearchBarService, private router: Router,
-              public apiService: ResearchHubApiService, public analyticsService: AnalyticsService,
-              public authService: AuthService, private ref: ChangeDetectorRef, public appComponentService: AppComponentService,
-              private titleService: Title,
-              private scrollDispatcher: ScrollDispatcher,
-              private ngZone : NgZone) {
+    private searchBarService: SearchBarService, private router: Router,
+    public apiService: ResearchHubApiService, public analyticsService: AnalyticsService,
+    public authService: AuthService, private ref: ChangeDetectorRef, public appComponentService: AppComponentService,
+    private titleService: Title,
+    private scrollDispatcher: ScrollDispatcher,
+    private ngZone: NgZone) {
 
     authService.loginChange.subscribe((loggedIn) => {
       this.showLoginBtn = !loggedIn;
@@ -101,9 +101,9 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit  {
     const type = item['type'];
 
     if (type === OptionType.Category) {
-      return {categoryId: item.id};
+      return { categoryId: item.id };
     } else {
-      return {researchActivityIds: [item.id]};
+      return { researchActivityIds: [item.id] };
     }
   }
 
@@ -120,7 +120,7 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit  {
     }
   }
 
-  setContentSidenavHasContent(hasContent: boolean){
+  setContentSidenavHasContent(hasContent: boolean) {
     this.appComponentService.setContentSidenavHasContent(hasContent);
   }
 
@@ -201,64 +201,64 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit  {
     }
   }
 
-  restyleContentSidenav(){
-    if (!this.appComponentService.isContentSidenavVisible){
+  restyleContentSidenav() {
+    if (!this.appComponentService.isContentSidenavVisible) {
       // If content sidenav is not visible, don't need to calculate style.
       return;
     }
     const topbar = this.topbarElement.nativeElement,
-    topContent = this.topContentElement.nativeElement,
-    topbarRect = topbar.getBoundingClientRect(),
-    contentHeight = topContent.clientHeight,
-    topbarBottom = topbarRect.bottom,
-    winY = window.pageYOffset,
-    winHeight = window.innerHeight;
+      topContent = this.topContentElement.nativeElement,
+      topbarRect = topbar.getBoundingClientRect(),
+      contentHeight = topContent.clientHeight,
+      topbarBottom = topbarRect.bottom,
+      winY = window.pageYOffset,
+      winHeight = window.innerHeight;
     let newFixedValue, newSidenavHeight;
-    if (topbarBottom < 0){
+    if (topbarBottom < 0) {
       // The topbar is now scrolled out of view, so we need to affix the
       // content sidenav if it is not affixed.
-      if (!this.isContentSidenavFixed){
-          newFixedValue = true;
+      if (!this.isContentSidenavFixed) {
+        newFixedValue = true;
       }
     } else {
       // The topbar is now in view, so we need to un-affix the content
       // sidenav if it is affixed.
-      if (this.isContentSidenavFixed){
+      if (this.isContentSidenavFixed) {
         newFixedValue = false;
       }
     }
-      // We calculate the height of the affixed content sidenav so that the sidenav does not
-      // overlap with the footer.
+    // We calculate the height of the affixed content sidenav so that the sidenav does not
+    // overlap with the footer.
     newSidenavHeight = Math.min(contentHeight - winY, winHeight);
 
-    if ((newFixedValue === false) || (newFixedValue === undefined && !this.isContentSidenavFixed)){
+    if ((newFixedValue === false) || (newFixedValue === undefined && !this.isContentSidenavFixed)) {
       // If the sidenav is not yet fixed - i.e. some of the topbar is still visible,
       // remove the visible topbar height from the sidenav height.
       newSidenavHeight -= topbarBottom;
     }
     this.ngZone.runGuarded(() => {
       setTimeout(() => {
-        if (newFixedValue !== undefined){
+        if (newFixedValue !== undefined) {
           this.isContentSidenavFixed = newFixedValue;
         }
-        if (newSidenavHeight !== undefined){
+        if (newSidenavHeight !== undefined) {
           this.contentSidenavHeight = newSidenavHeight;
         }
       }, 0);
     });
   }
 
-  setupContentSidenav(){
+  setupContentSidenav() {
     // If not running in a browser, do not do any listener setup.
-    if (!isPlatformBrowser){
+    if (!isPlatformBrowser) {
       return;
     }
     this.contentSidenavVisibilitySub = this.appComponentService.contentSidenavVisibility$.subscribe((isVisible) => {
       // Sets if we pop out the content sidenav.
-          if (isVisible){
-            // Do a restyle when the filter sidenav opens to initialise the height.
-            this.restyleContentSidenav();
-          }
+      if (isVisible) {
+        // Do a restyle when the filter sidenav opens to initialise the height.
+        this.restyleContentSidenav();
+      }
     });
     const restyleFn = () => (this.restyleContentSidenav());
     this.scrollSub = this.scrollDispatcher.scrolled(150).subscribe(restyleFn);
@@ -266,14 +266,14 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit  {
     this.contentElementHeight = this.contentElement.nativeElement.clientHeight;
   }
 
-  checkContentHeightChanged(){
-    if (!this.appComponentService.isContentSidenavVisible){
+  checkContentHeightChanged() {
+    if (!this.appComponentService.isContentSidenavVisible) {
       // If content sidenav is not visible, don't need to calculate style.
       return;
     }
 
     const contentHeight = this.contentElement.nativeElement.clientHeight;
-    if (contentHeight !== this.contentElementHeight){
+    if (contentHeight !== this.contentElementHeight) {
       // Recompute content sidenav size when the content has changed.
       this.restyleContentSidenav();
       setTimeout(() => {
@@ -283,7 +283,7 @@ export class AppComponent implements OnInit, OnDestroy, AfterViewInit  {
   }
 
   ngAfterViewInit() {
-      this.setupContentSidenav()
+    this.setupContentSidenav()
   }
 
   ngAfterViewChecked() {
