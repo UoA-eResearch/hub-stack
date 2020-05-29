7832c791db03454a0eae4b134268cb4e51b8ac14
Sam Kavanagh <hganavak@gmail.com>
2020-05-29 17:28:04 +1200

    Got Cognito partially working

--- research-hub-web/src/app/app.module.ts
+++ research-hub-web/src/app/app.module.ts
@@ -1,20 +1,23 @@
 
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {AppComponent} from './app.component';
-import {RoutingModule} from './routing/routing.module';
-import {SharedModule} from './components/shared/app.shared.module';
-import {ServicesModule} from './services/services.module';
-import {HeaderComponent} from './components/header/header.component';
-import {HeaderService} from './components/header/header.service';
-import {SearchBarComponent} from './components/search-bar/search-bar.component';
-import {SearchBarService} from './components/search-bar/search-bar.service';
-import {AppComponentService} from './app.component.service';
-import {SearchFiltersService} from './components/search-results/search-filters/search-filters.service';
+import { AppComponent } from './app.component';
+import { RoutingModule } from './routing/routing.module';
+import { SharedModule } from './components/shared/app.shared.module';
+import { ServicesModule } from './services/services.module';
+import { HeaderComponent } from './components/header/header.component';
+import { HeaderService } from './components/header/header.service';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { SearchBarService } from './components/search-bar/search-bar.service';
+import { AppComponentService } from './app.component.service';
+import { SearchFiltersService } from './components/search-results/search-filters/search-filters.service';
 
+import { AuthModule, CognitoConfigService } from 'uoa-auth-angular';
+import { AppAuthConfigService } from './services/app-auth-config.service';
+import { ErrorPagesModule } from 'uoa-error-pages-angular';
 
 @NgModule({
   declarations: [
@@ -28,9 +31,15 @@ import {SearchFiltersService} from './components/search-results/search-filters/s
     ServicesModule,
     SharedModule,
     RoutingModule,
+    ErrorPagesModule
   ],
   entryComponents: [],
-  providers: [HeaderService, SearchBarService, AppComponentService,SearchFiltersService],
+  providers: [
+    HeaderService,
+    SearchBarService,
+    AppComponentService,
+    SearchFiltersService,
+    { provide: CognitoConfigService, useClass: AppAuthConfigService }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
