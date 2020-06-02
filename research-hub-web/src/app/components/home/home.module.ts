e503b040eb660776e3200295929f3213b7ff8b58
Sam Kavanagh <hganavak@gmail.com>
2020-06-02 16:57:03 +1200

    Got Cognito working. AOT fixes still required

--- research-hub-web/src/app/components/home/home.module.ts
+++ research-hub-web/src/app/components/home/home.module.ts
@@ -6,15 +6,14 @@ import { HomeComponent } from './home.component';
 import { FeaturedComponent } from './featured/featured.component';
 import { BrowseComponent } from './browse/browse.component';
 import { ResearchActivityComponent } from './research-activity/research-activity.component';
-import { SharedModule } from '../shared/components/shared/app.shared.module';
+import { SharedModule } from 'app/components/shared/app.shared.module';
 
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
-    routing,
-    FeaturedComponent
+    routing
   ],
   declarations: [
     HomeComponent,
