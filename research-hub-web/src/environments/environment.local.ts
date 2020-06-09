12a04c9f159947423089fae8f2c442a0b4ac9bef
Sam Kavanagh <hganavak@gmail.com>
2020-06-09 13:11:19 +1200

    Updated env files

--- research-hub-web/src/environments/environment.local.ts
+++
@@ -1,7 +0,0 @@
-/* Environment settings for running in local mode using the deploy project. */
-export const environment = {
-  production: false,
-  researchHubApiUrl: (<any>window)._env.researchHubApiUrl || '',
-  cerApiUrl: (<any>window)._env.cerApiUrl || '',
-  analyticsCode: (<any>window)._env.analyticsCode || ''
-};
--- research-hub-web/src/environments/environment.prod.ts
+++ research-hub-web/src/environments/environment.prod.ts
@@ -1,6 +1,20 @@
 export const environment = {
   production: true,
-  researchHubApiUrl: (<any>window)._env.researchHubApiUrl || '',
-  cerApiUrl: (<any>window)._env.cerApiUrl || '',
-  analyticsCode: (<any>window)._env.analyticsCode || ''
+  researchHubApiUrl: 'https://dev.research-hub.cer.auckland.ac.nz/api/', // TODO: Replace hardcoded URLS
+  cerApiUrl: 'https://dev.research-hub.cer.auckland.ac.nz/cer-api/',
+  analyticsCode: 'UA-77710107-3',
+  auth: {
+    cognitoAwsRegion: 'ap-southeast-2',
+    cognitoUserPoolId: 'ap-southeast-2_pgErjyL4O',
+    cognitoDomain: 'uoapool-sandbox',
+    cognitoClientId: '53nj363gumskeibdir61nu1cs5',
+    redirectUri: 'http://localhost:4200',
+    scopes: 'openid profile https://test-domain.auckland.ac.nz/lambda-hello-world',
+    codeChallengeMethod: 'S256',
+    logout_uri: 'http://localhost:4200'
+  },
+  privateUrlKeyWords: {
+    whoNeedBearerToken: ['apigw.sandbox.amazon.auckland.ac.nz', 'some-other-api'],
+    whoNeedIdToken: []
+  }
 };
