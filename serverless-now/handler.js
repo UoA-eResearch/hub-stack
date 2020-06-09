ebd4e04a9633c05b66769e0bc1a2f850d38a5723
warrickct <warrickct@gmail.com>
2020-06-09 19:25:46 +1200

    returning user details

--- serverless-now/handler.js
+++ serverless-now/handler.js
@@ -5,28 +5,28 @@ const utils = require("@uoa/utilities");
 module.exports.main = async (event) => {
   const BASE_URL = `api.${process.env.ENV}.auckland.ac.nz`;
 
-  // try {
-  //   let cognitoDomain = process.env.COGNITO_DOMAIN;
-  //   let data = await utils.getUserInfo(event, cognitoDomain);
-  //   if (data.error) {
-  //     // return buildResponse(500, { 'message': 'User not found' });
-  //     console.log("User not found.");
-  //   } else {
-  //     // personId = data['custom:EmpID'];
-  //     console.log("User data:");
-  //     console.log(data);
-  //     return {
-  //       statusCode: 200,
-  //       headers: {
-  //         "Access-Control-Allow-Origin": "*",
-  //       },
-  //       body: JSON.stringify(data),
-  //     };
-  //   }
-  // } catch (e) {
-  //   // return buildResponse(500, { 'message': 'Error getting user' });
-  //   console.log("Error getting user.");
-  // }
+  try {
+    let cognitoDomain = process.env.COGNITO_DOMAIN;
+    let data = await utils.getUserInfo(event, cognitoDomain);
+    if (data.error) {
+      // return buildResponse(500, { 'message': 'User not found' });
+      console.log("User not found.");
+    } else {
+      // personId = data['custom:EmpID'];
+      console.log("User data:");
+      console.log(data);
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify(data),
+      };
+    }
+  } catch (e) {
+    // return buildResponse(500, { 'message': 'Error getting user' });
+    console.log("Error getting user.");
+  }
 
   // POST (Create) a new ServiceNow ticket
   if (event.httpMethod === "POST" && event.body) {
