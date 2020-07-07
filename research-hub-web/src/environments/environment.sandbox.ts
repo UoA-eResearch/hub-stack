d93ed4d93d1c2495f449e87069f025ac313ff72c
Hganavak <hganavak@gmail.com>
2020-07-07 16:11:02 +1200

    Updated callback URLs to https

--- research-hub-web/src/environments/environment.sandbox.ts
+++ research-hub-web/src/environments/environment.sandbox.ts
@@ -8,10 +8,10 @@ export const environment = {
     cognitoUserPoolId: 'ap-southeast-2_pgErjyL4O',
     cognitoDomain: 'uoapool-sandbox',
     cognitoClientId: '53nj363gumskeibdir61nu1cs5',
-    redirectUri: 'http://d35r3tpm7jlu7p.cloudfront.net',
+    redirectUri: 'https://d35r3tpm7jlu7p.cloudfront.net',
     scopes: 'openid profile https://test-domain.auckland.ac.nz/lambda-hello-world',
     codeChallengeMethod: 'S256',
-    logout_uri: 'http://d35r3tpm7jlu7p.cloudfront.net'
+    logout_uri: 'https://d35r3tpm7jlu7p.cloudfront.net'
   },
   privateUrlKeyWords: {
     whoNeedBearerToken: ['apigw.sandbox.amazon.auckland.ac.nz'],
