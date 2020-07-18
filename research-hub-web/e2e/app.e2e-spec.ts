a2d5a4f8bc09b290e7ca5d99d382023696065aba
Hganavak <hganavak@gmail.com>
2020-07-18 15:23:26 +1200

    Updated broken e2e test

--- research-hub-web/e2e/app.e2e-spec.ts
+++ research-hub-web/e2e/app.e2e-spec.ts
@@ -184,6 +184,7 @@ describe('ResearchHub\'s Research Impact Content', () => {
    * its title is 'Planning for Impact'.
    */
   it('can correctly load a sub-page (guideCategory) item', async () => {
+    await browser.driver.wait(protractor.ExpectedConditions.visibilityOf(element(by.css('mat-grid-list .browse-tile'))), TIMEOUT_PERIOD);
     await browser.driver.findElement(by.css('mat-grid-list .browse-tile')).click();
     await browser.waitForAngular();
     await browser.driver.wait(protractor.ExpectedConditions.visibilityOf(element(by.css('.description h1'))), TIMEOUT_PERIOD);
