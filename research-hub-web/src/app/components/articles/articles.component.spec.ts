216d6ca2edf0784d4b865cc00f30075f7c3eb5d8
Hganavak <hganavak@gmail.com>
2020-07-18 14:37:42 +1200

    Added articles. Fixed unit tests

--- research-hub-web/src/app/components/articles/articles.component.spec.ts
+++ research-hub-web/src/app/components/articles/articles.component.spec.ts
@@ -1,32 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { EquipmentComponent } from './equipment.component';
-import { SharedModule } from '../shared/app.shared.module';
-import { EquipmentModule } from './equipment.module';
-import { CommonModule } from '@angular/common';
+import { ArticlesComponent } from './articles.component';
 import { ApolloTestingController, ApolloTestingModule } from 'apollo-angular/testing';
 
-describe('EquipmentComponent', () => {
-  let component: EquipmentComponent;
-  let fixture: ComponentFixture<EquipmentComponent>;
+describe('ArticlesComponent', () => {
+  let component: ArticlesComponent;
+  let fixture: ComponentFixture<ArticlesComponent>;
   let backend: ApolloTestingController;
-
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        EquipmentComponent
-      ],
-      imports: [
-        ApolloTestingModule
-      ]
+      declarations: [ArticlesComponent],
+      imports: [ApolloTestingModule]
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
     backend = TestBed.get(ApolloTestingController);
-    fixture = TestBed.createComponent(EquipmentComponent);
+    fixture = TestBed.createComponent(ArticlesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ research-hub-web/src/app/components/articles/articles.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AllArticlesGQL, AllArticlesQuery } from '../../graphql/schema';
+import { Observable } from 'rxjs';
+import { pluck } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-articles',
+  templateUrl: './articles.component.html',
+  styleUrls: ['./articles.component.scss']
+})
+export class ArticlesComponent implements OnInit {
+
+  public allArticles$: Observable<AllArticlesQuery['articleCollection']>;
+
+  constructor(public allArticlesGQL: AllArticlesGQL) { }
+
+  ngOnInit(): void {
+    this.allArticles$ = this.allArticlesGQL.fetch().pipe(pluck('data', 'articleCollection'));
+  }
+
+}
