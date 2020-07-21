f74403c8aaccd65d913a00c02cf0149f3f8482fb
warrickct <warrickct@gmail.com>
2020-07-21 16:28:41 +1200

    WIP: getting slug specific data and page information.

--- research-hub-web/src/app/components/subhubs/subhubs.component.ts
+++ research-hub-web/src/app/components/subhubs/subhubs.component.ts
@@ -1,27 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { AllSubHubChildPagesGQL, AllSubHubChildPagesQuery } from '../../graphql/schema';
-import  { Observable } from 'rxjs';
-import { pluck } from 'rxjs/operators';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import {
+  AllSubHubChildPagesGQL,
+  AllSubHubChildPagesQuery,
+} from "../../graphql/schema";
+import { Observable } from "rxjs";
+import { pluck } from "rxjs/operators";
 
 @Component({
-  selector: 'app-subhubs',
-  templateUrl: './subhubs.component.html',
-  styleUrls: ['./subhubs.component.scss']
+  selector: "app-subhubs",
+  templateUrl: "./subhubs.component.html",
+  styleUrls: ["./subhubs.component.scss"],
 })
 export class SubhubsComponent implements OnInit {
+  public allSubHubChildPages$: Observable<
+    AllSubHubChildPagesQuery["subHubCollection"]
+  >;
 
-  public allSubHubChildPages$: Observable<AllSubHubChildPagesQuery['subHubCollection']>;
-
-  constructor(private route: ActivatedRoute, public AllSubHubChildPagesGQL: AllSubHubChildPagesGQL) { }
+  constructor(
+    private route: ActivatedRoute,
+    public AllSubHubChildPagesGQL: AllSubHubChildPagesGQL
+  ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      const slug = params['slug']
+    this.route.params.subscribe((params) => {
+      const slug = params["slug"];
       console.log(slug);
-    // todo: find out how pass in slug to the query.
-    // test slug: landing-page-for-a-sub-hub
-      this.allSubHubChildPages$ = this.AllSubHubChildPagesGQL.fetch({slug}).pipe(pluck('data', 'subHubCollection'));
-    })
+      // todo: find out how pass in slug to the query.
+      // test slug: landing-page-for-a-sub-hub
+
+      if (!!slug) {
+        console.log(slug);
+        // query for slug's target subhub info + items here
+      } else {
+        console.log('no slug');
+        // render generic test about all the subhubs of the r-hub
+      }
+
+      this.allSubHubChildPages$ = this.AllSubHubChildPagesGQL.fetch({
+        slug,
+      }).pipe(pluck("data", "subHubCollection"));
+    });
   }
 }
