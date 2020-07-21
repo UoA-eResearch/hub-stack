7932f19d3399469e4f368eea64d0ad2b66f24a34
warrickct <warrickct@gmail.com>
2020-07-21 16:19:26 +1200

    Subhub item query now accepting url slug as query conditional filter.

--- research-hub-web/src/app/components/subhubs/subhubs.component.ts
+++ research-hub-web/src/app/components/subhubs/subhubs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { AllSubHubChildPagesGQL, AllSubHubChildPagesQuery } from '../../graphql/schema';
 import  { Observable } from 'rxjs';
 import { pluck } from 'rxjs/operators';
@@ -12,10 +13,15 @@ export class SubhubsComponent implements OnInit {
 
   public allSubHubChildPages$: Observable<AllSubHubChildPagesQuery['subHubCollection']>;
 
-  constructor(public AllSubHubChildPagesGQL: AllSubHubChildPagesGQL) { }
+  constructor(private route: ActivatedRoute, public AllSubHubChildPagesGQL: AllSubHubChildPagesGQL) { }
 
   ngOnInit(): void {
-    this.allSubHubChildPages$ = this.AllSubHubChildPagesGQL.fetch().pipe(pluck('data', 'subHubCollection'));
+    this.route.params.subscribe(params => {
+      const slug = params['slug']
+      console.log(slug);
+    // todo: find out how pass in slug to the query.
+    // test slug: landing-page-for-a-sub-hub
+      this.allSubHubChildPages$ = this.AllSubHubChildPagesGQL.fetch({slug}).pipe(pluck('data', 'subHubCollection'));
+    })
   }
-
 }
