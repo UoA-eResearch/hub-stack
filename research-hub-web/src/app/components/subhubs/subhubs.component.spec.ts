9405619771809ce10406a2bbfbe8c7c5f5945ed5
Hganavak <hganavak@gmail.com>
2020-07-22 14:47:48 +1200

    Updated broken unit tests

--- research-hub-web/src/app/components/subhubs/subhubs.component.spec.ts
+++ research-hub-web/src/app/components/subhubs/subhubs.component.spec.ts
@@ -1,19 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SubhubsComponent } from './subhubs.component';
+import { ApolloTestingController, ApolloTestingModule } from 'apollo-angular/testing';
 
 describe('SubhubsComponent', () => {
   let component: SubhubsComponent;
   let fixture: ComponentFixture<SubhubsComponent>;
+  let backend: ApolloTestingController;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SubhubsComponent ]
+      declarations: [SubhubsComponent],
+      imports: [ApolloTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
+    backend = TestBed.get(ApolloTestingController);
     fixture = TestBed.createComponent(SubhubsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
