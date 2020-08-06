bc6e19a011fae0fac96a843fae03fde83ce2b9b4
warrickct <warrickct@gmail.com>
2020-08-06 14:34:30 +1200

    Added testt file for person-details component.

---
+++ research-hub-web/src/app/components/person-details/person-details.component.spec.ts
@@ -0,0 +1,49 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ApolloTestingController, ApolloTestingModule } from 'apollo-angular/testing';
+import { SharedModule } from '../shared/app.shared.module';
+import { RouterModule } from '@angular/router';
+import { By } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../../app.material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PersonDetailsComponent } from "./person-details.component";
+
+describe('Person Details Component', () => {
+  let component: PersonDetailsComponent;
+  let fixture: ComponentFixture<PersonDetailsComponent>;
+  let backend: ApolloTestingController;
+  let controller: ApolloTestingController;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [PersonDetailsComponent],
+      imports: [
+        ApolloTestingModule,
+        CommonModule,
+        MaterialModule,
+        SharedModule,
+        BrowserAnimationsModule,
+        RouterModule.forRoot([])
+      ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    controller = TestBed.get(ApolloTestingController);
+
+    backend = TestBed.get(ApolloTestingController);
+    fixture = TestBed.createComponent(PersonDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    controller.verify();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+});
