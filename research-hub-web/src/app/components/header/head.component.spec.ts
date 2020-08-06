6efe0c72a1216f7a7e81649829ab5f351f760677
warrickct <warrickct@gmail.com>
2020-08-06 17:11:36 +1200

    WIP: creating tests for featured component

--- research-hub-web/src/app/components/header/head.component.spec.ts
+++ research-hub-web/src/app/components/header/head.component.spec.ts
@@ -43,7 +43,6 @@ describe('Header Component', () => {
     });
 
     //   afterEach(() => {
-
     //   });
 
     it('should create', () => {
--- research-hub-web/src/app/components/home/featured/featured.component.spec.ts
+++ research-hub-web/src/app/components/home/featured/featured.component.spec.ts
@@ -7,6 +7,10 @@ import { MaterialModule } from '../../../app.material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FeaturedComponent } from "./featured.component";
 
+import { ResearchHubApiService } from "../../../services/research-hub-api.service";
+import { HttpClient, HttpHandler, HttpClientModule } from '@angular/common/http'
+import { AnalyticsService } from "../../../services/analytics.service";
+
 describe('Feature component', () => {
     let component: FeaturedComponent;
     let fixture: ComponentFixture<FeaturedComponent>;
@@ -22,7 +26,11 @@ describe('Feature component', () => {
                 RouterModule.forRoot([]),
             ],
             providers: [
-                FeaturedComponent
+                AnalyticsService,
+                ResearchHubApiService,
+                HttpClientModule,
+                HttpClient,
+                HttpHandler
             ],
         })
             .compileComponents();
