473dcfb968e50966126154b4ef78e392d652a93e
Hganavak <hganavak@gmail.com>
2020-08-12 15:09:56 +1200

    Refactored breadcrumb logic to include SubHub slug

--- research-hub-web/src/app/services/cer-graphql.service.ts
+++ research-hub-web/src/app/services/cer-graphql.service.ts
@@ -2,6 +2,15 @@ import { Injectable } from '@angular/core';
 import { GetAllSubHubChildPagesSlugsGQL } from '../graphql/schema';
 import { map } from 'rxjs/operators';
 
+/**
+ * The interface of the breadcrumbsArray object returned by this service's getParentSubHubs()
+ * method. It contains an array of SubHubs, with their title and slug.
+ */
+export interface SubHubTitleAndSlug {
+  title: String,
+  slug: String
+};
+
 @Injectable({
   providedIn: 'root'
 })
@@ -31,7 +40,7 @@ export class CerGraphqlService {
       if (!this._subHubCollectionWithChildPagesSlugs) {
         this._subHubCollectionWithChildPagesSlugs = await this._loadSubHubCollection();
       }
-      const breadCrumbsArray: String[] = []; // The array to be populated by the recursive function
+      const breadCrumbsArray: SubHubTitleAndSlug[] = []; // The array to be populated by the recursive function
       this._getBreadCrumbsArray(entrySlug, breadCrumbsArray); // Populate the breadcrumbs array
       return breadCrumbsArray;
     } catch (e) { console.error('Error loading breadcrumbs: ', e) }
@@ -46,14 +55,16 @@ export class CerGraphqlService {
    * @param entrySlug The slug of the content item we are searching for breadcrumbs for
    * @param breadcrumbsArray The array that will be populated by this function with any parent items
    */
-  private _getBreadCrumbsArray(entrySlug: string, breadcrumbsArray: String[]) {
+  private _getBreadCrumbsArray(entrySlug: string, breadcrumbsArray) {
     for (const item of this._subHubCollectionWithChildPagesSlugs) {
       item.subhubPagesCollection.items.forEach(subPage => {
         if (subPage.slug === entrySlug) { // The SubHub's childPages contains the current entry we're searching for
-          if (breadcrumbsArray.includes(item.title)) { // Check if it's already in the array (we have circular links)
-            return console.error('Circular SubHub structure detected');
-          }
-          breadcrumbsArray.push(item.title); // Push it to the breadcrumbsArray
+          for (const subHub of breadcrumbsArray) { // Check it's not already known
+            if (subHub.slug === item.slug) {
+              return console.error('Circular SubHub structure detected');
+            }
+          };
+          breadcrumbsArray.push({ title: item.title, slug: item.slug }); // Push it to the breadcrumbsArray
           return this._getBreadCrumbsArray(item.slug, breadcrumbsArray); // Call the function on the SubHub
         }
       });
