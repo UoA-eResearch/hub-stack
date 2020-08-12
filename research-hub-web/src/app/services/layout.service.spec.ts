ecbfdc35d2015263b65a6e02a3aec05621912b33
Warrick Corfe-Tan <warrickct@gmail.com>
2020-08-12 12:15:28 +1200

    Added more tests for layout service.

--- research-hub-web/src/app/services/layout.service.spec.ts
+++ research-hub-web/src/app/services/layout.service.spec.ts
@@ -31,11 +31,34 @@ describe('Layout service', () => {
     expect(service.getNumGridCols('')).toBe(4);
   });
 
-  it('Should return xs MQ Alias if window width is less than small', () => {
-    spyOnProperty(window, 'innerWidth').and.returnValue(100);
+  it('Should return xs MQ Alias if window width is less than 600px', () => {
+    spyOnProperty(window, 'innerWidth').and.returnValue(599);
     window.dispatchEvent(new Event('resize'));
-    console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@', window.innerWidth);
     expect(service.getMQAlias()).toBe('xs');
+  });
+
+  it('Should return s MQ Alias if window width is 599-959px', () => {
+    spyOnProperty(window, 'innerWidth').and.returnValue(600);
+    window.dispatchEvent(new Event('resize'));
+    expect(service.getMQAlias()).toBe('sm');
+  });
+
+  it('Should return md MQ Alias if window width is 960-1279px', () => {
+    spyOnProperty(window, 'innerWidth').and.returnValue(960);
+    window.dispatchEvent(new Event('resize'));
+    expect(service.getMQAlias()).toBe('md');
+  });
+
+  it('Should return lg MQ Alias if window width is 1280-1919px', () => {
+    spyOnProperty(window, 'innerWidth').and.returnValue(1280);
+    window.dispatchEvent(new Event('resize'));
+    expect(service.getMQAlias()).toBe('lg');
+  });
+
+  it('Should return xl MQ Alias if window width is above 1919px', () => {
+    spyOnProperty(window, 'innerWidth').and.returnValue(1920);
+    window.dispatchEvent(new Event('resize'));
+    expect(service.getMQAlias()).toBe('xl');
   });
 
 });
