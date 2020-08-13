4782822443ef0511536bec9b97f1f1a874a7ea1f
Warrick Corfe-Tan <warrickct@gmail.com>
2020-08-13 14:24:14 +1200

    Some tests working for articles component.

--- research-hub-web/src/app/components/articles/articles.component.spec.ts
+++ research-hub-web/src/app/components/articles/articles.component.spec.ts
@@ -40,13 +40,231 @@ describe('ArticlesComponent', () => {
   } as ArticleCollection);
 
   const mockArticle$: Observable<Article> = of({
-        '__typename': 'Article',
-        'slug': 'first-article',
-        'title': 'First article',
-        'summary': 'A brief description of the first article. I\'m writing some more stuff here just so that this seems a little more realistic. Sam was here. Have a good day.',
-        'ssoProtected': false,
-        'searchable': true
-  } as Article);
+      'title': 'First article',
+      'summary': 'A brief description of the first article. I\'m writing some more stuff here just so that this seems a little more realistic. Sam was here. Have a good day.',
+      'ssoProtected': false,
+      'body': {
+        'json': {
+          'data': {},
+          'content': [
+            {
+              'data': {},
+              'content': [
+                {
+                  'data': {},
+                  'marks': [],
+                  'value': 'Welcome to the Hub',
+                  'nodeType': 'text'
+                }
+              ],
+              'nodeType': 'heading-2'
+            },
+            {
+              'data': {},
+              'content': [
+                {
+                  'data': {},
+                  'marks': [],
+                  'value': 'This is the first article. Sam\'s writing some random text here to pad out space.',
+                  'nodeType': 'text'
+                }
+              ],
+              'nodeType': 'paragraph'
+            },
+            {
+              'data': {},
+              'content': [
+                {
+                  'data': {},
+                  'marks': [],
+                  'value': 'Smaller Heading',
+                  'nodeType': 'text'
+                }
+              ],
+              'nodeType': 'heading-3'
+            },
+            {
+              'data': {},
+              'content': [
+                {
+                  'data': {},
+                  'marks': [],
+                  'value': 'I\'m just testing how the HTML rendering works.',
+                  'nodeType': 'text'
+                }
+              ],
+              'nodeType': 'paragraph'
+            },
+            {
+              'data': {},
+              'content': [
+                {
+                  'data': {},
+                  'content': [
+                    {
+                      'data': {},
+                      'content': [
+                        {
+                          'data': {},
+                          'marks': [],
+                          'value': 'I\'m just testing how a bullet point list works.',
+                          'nodeType': 'text'
+                        }
+                      ],
+                      'nodeType': 'paragraph'
+                    }
+                  ],
+                  'nodeType': 'list-item'
+                },
+                {
+                  'data': {},
+                  'content': [
+                    {
+                      'data': {},
+                      'content': [
+                        {
+                          'data': {},
+                          'marks': [],
+                          'value': 'Wow, it has bullets.',
+                          'nodeType': 'text'
+                        }
+                      ],
+                      'nodeType': 'paragraph'
+                    }
+                  ],
+                  'nodeType': 'list-item'
+                },
+                {
+                  'data': {},
+                  'content': [
+                    {
+                      'data': {},
+                      'content': [
+                        {
+                          'data': {},
+                          'marks': [],
+                          'value': 'Run out of generic text ',
+                          'nodeType': 'text'
+                        },
+                        {
+                          'data': {},
+                          'marks': [
+                            {
+                              'type': 'italic'
+                            }
+                          ],
+                          'value': 'ideas ',
+                          'nodeType': 'text'
+                        },
+                        {
+                          'data': {},
+                          'marks': [],
+                          'value': 'so I\'ll stop now.',
+                          'nodeType': 'text'
+                        }
+                      ],
+                      'nodeType': 'paragraph'
+                    }
+                  ],
+                  'nodeType': 'list-item'
+                }
+              ],
+              'nodeType': 'unordered-list'
+            },
+            {
+              'data': {},
+              'content': [
+                {
+                  'data': {},
+                  'marks': [],
+                  'value': 'An Even Smaller Heading',
+                  'nodeType': 'text'
+                }
+              ],
+              'nodeType': 'heading-4'
+            },
+            {
+              'data': {},
+              'content': [
+                {
+                  'data': {},
+                  'marks': [],
+                  'value': 'Last thing I\'ll check is how a link works, have you heard of ',
+                  'nodeType': 'text'
+                },
+                {
+                  'data': {
+                    'uri': 'https://google.com/'
+                  },
+                  'content': [
+                    {
+                      'data': {},
+                      'marks': [],
+                      'value': 'Google',
+                      'nodeType': 'text'
+                    }
+                  ],
+                  'nodeType': 'hyperlink'
+                },
+                {
+                  'data': {},
+                  'marks': [],
+                  'value': '?',
+                  'nodeType': 'text'
+                }
+              ],
+              'nodeType': 'paragraph'
+            },
+            {
+              'data': {},
+              'content': [
+                {
+                  'data': {},
+                  'marks': [],
+                  'value': '',
+                  'nodeType': 'text'
+                }
+              ],
+              'nodeType': 'paragraph'
+            }
+          ],
+          'nodeType': 'document'
+        },
+        '__typename': 'ArticleBody'
+      },
+      'keywords': [
+        'article',
+        'informational',
+        'general',
+        'test'
+      ],
+      'slug': 'first-article',
+      'searchable': true,
+      'icon': {
+        'title': 'Pippy',
+        'description': 'Sam\'s dog Pippy',
+        'url': 'https://images.ctfassets.net/vbuxn5csp0ik/014fpqFg0KmnP8NWc9Jyxe/0a1a14a6a0dd438443f70d5f917568fc/Screen_Shot_2020-07-23_at_3.12.46_PM.png',
+        '__typename': 'Asset'
+      },
+      'relatedItemsCollection': {
+        'items': [
+          {
+            'title': 'Super dooper research service',
+            'summary': 'The best service ever. It saved my life.',
+            'slug': 'super-dooper-research-service',
+            '__typename': 'Service'
+          },
+          {
+            'title': 'Death Star',
+            'summary': 'Mobile space station and galactic superweapon.',
+            'slug': 'death-star',
+            '__typename': 'Equipment'
+          }
+        ],
+        '__typename': 'ArticleRelatedItemsCollection'
+      },
+      '__typename': 'Article'
+    } as unknown as Article);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -63,9 +281,9 @@ describe('ArticlesComponent', () => {
         {
           provide: ActivatedRoute,
           useValue: {
-            params: of([{
-              slug: 'first-article'
-            }])
+            params: of({
+              slug: ''
+            })
           }
         }
       ]
@@ -95,8 +313,32 @@ describe('ArticlesComponent', () => {
     expect(de.nativeElement.innerHTML).toEqual('Article Collection');
   });
 
-  xit('should have an specified article\'s data', () => {
-    //sdfksl
+  describe('When a truthy url slug is present', () => {
+    beforeEach(() => {
+      controller = TestBed.get(ApolloTestingController);
+      fixture = TestBed.createComponent(ArticlesComponent);
+      component = fixture.componentInstance;
+      component.slug = 'first-article';
+      TestBed.get(ActivatedRoute).params = of({
+        slug: 'first-article'
+      });
+      fixture.detectChanges();
+    })
+
+    it('Should have a truthy slug', () => {
+      expect(component.slug).toBeTruthy();
+    });
+
+    it('should have an specified article\'s data', () => {
+      console.log(component.slug);
+    });
+
+    it('Should get a single article data', () => {
+      component.getArticleBySlug(component.slug).subscribe(res => {
+        expect(res.slug).toEqual('first-article');
+      })
+    })
+
   });
 
 });
