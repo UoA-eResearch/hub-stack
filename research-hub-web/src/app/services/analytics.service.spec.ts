1d2702e7b17e0e5c14f6a20bc497c548ae40426d
warrickct <warrickct@gmail.com>
2020-08-18 14:06:49 +1200

    Added basic error detection tests for analytics services.

--- research-hub-web/src/app/services/analytics.service.spec.ts
+++ research-hub-web/src/app/services/analytics.service.spec.ts
@@ -1,6 +1,6 @@
 import { AnalyticsService } from './analytics.service';
 
-describe('Analytics service', () => {
+describe('Google Analytics service', () => {
     let analyticsService: AnalyticsService;
 
     const mockUXEventLabel = 'Home page research activities';
@@ -19,14 +19,85 @@ describe('Analytics service', () => {
     });
 
     it('Should throw an error at an invalid UX event', () => {
-        expect(analyticsService.trackUserExperience('blah', '')).toThrowError('This is not a known Google Analytics UX event');
+        expect(() => analyticsService.trackUserExperience('blah', '')).toThrowError();
     });
 
-    xit('Should return if google analytics type is undefined', () => {
-        // expect(analyticsService.trackEvent())
-    })
+    it('Should not throw error when calling track policy valid inputs', () => {
+        expect(() => analyticsService.trackPolicy('test', 'test')).not.toThrowError();
+    });
 
-    xit('Should track page view', () => {
+    it('Should not throw error when calling track join user study with valid input', () => {
+        expect(() => analyticsService.trackJoinUserStudy('test')).not.toThrowError();
+    });
+
+    it('Should not throw error when calling track feedback with valid input', () => {
+        expect(() => analyticsService.trackFeedback('test')).not.toThrowError();
+    });
+
+    it('Should not throw error when calling track outbound link with valid input', () => {
+        expect(() => analyticsService.trackJoinUserStudy('test')).not.toThrowError();
+    });
+
+    it('Should not throw error when calling track no search results with valid input', () => {
+        expect(() => analyticsService.trackNoSearchResults('category', 'text')).not.toThrowError();
+    });
+
+    it('Should not throw error when calling trackActionIntegrated results with valid input', () => {
+        expect(() => analyticsService.trackActionIntegrated('category')).not.toThrowError();
+    });
+
+    describe('trackIntegratedService (function)', () => {
+        it('Should not throw error when called with valid input', () => {
+            expect(() => analyticsService.trackIntegratedService('category', 'text')).not.toThrowError();
+        });
+    });
+
+    describe('trackActionExternal (function)', () => {
+        it('Should not throw error when called with valid input', () => {
+            expect(() => analyticsService.trackActionExternal('event category', 'category', 'text')).not.toThrowError();
+        });
+    });
+
+    describe('trackSearch (function)', () => {
+        it('Should not throw error when called with valid input', () => {
+            expect(() => analyticsService.trackSearch('category', 'text')).not.toThrowError();
+        });
+    });
+
+    describe('trackPerson (function)', () => {
+        it('Should not throw error when called with valid input', () => {
+            expect(() => analyticsService.trackPerson('text', 'text')).not.toThrowError();
+        });
+    });
+
+    describe('trackOrgUnit (function)', () => {
+        it('Should not throw error when called with valid input', () => {
+            expect(() => analyticsService.trackOrgUnit('text', 'text')).not.toThrowError();
+        });
+    });
+
+    describe('trackGuideCategory (function)', () => {
+        it('Should not throw error when called with valid input', () => {
+            expect(() => analyticsService.trackGuideCategory('text', 'text')).not.toThrowError();
+        });
+    });
+
+    describe('trackGuide (function)', () => {
+        it('Should not throw error when called with valid input', () => {
+            expect(() => analyticsService.trackGuide('text', 'text')).not.toThrowError();
+        });
+    });
+
+    describe('trackContent (function)', () => {
+        it('Should not throw error when called with valid input', () => {
+            expect(() => analyticsService.trackContent('text', 'text')).not.toThrowError();
+        });
+    });
+
+    describe('trackPageView (function)', () => {
+        it('Should not throw error when called with valid input', () => {
+            expect(() => analyticsService.trackContent('text', 'text')).not.toThrowError();
+        });
     });
 
 });
