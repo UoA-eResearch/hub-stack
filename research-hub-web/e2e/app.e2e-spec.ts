272dfd52175e7e77707a5c35e15a9b06416eca86
Hganavak <hganavak@gmail.com>
2020-08-25 15:19:00 +1200

    Got tests working reliably on BrowserStack automation and local Selenium

--- research-hub-web/e2e/app.e2e-spec.ts
+++ research-hub-web/e2e/app.e2e-spec.ts
@@ -1,7 +1,48 @@
 import { ResearchHubPage } from './app.po';
-import { browser, by, element, $, $$ } from 'protractor';
+import { browser, by, element, $, $$, ExpectedConditions, ElementFinder, ElementArrayFinder } from 'protractor';
 
 let page: ResearchHubPage;
+const TIMEOUT_PERIOD = 12000;
+
+/**
+ * Wrapper around the standard $() and $$() Protractor functions that add extra waits
+ * required to make the tests work reliably in BrowserStack Automation.
+ * 
+ * @param search CSS element finder
+ */
+export let _$ = (search): ElementFinder => {
+  browser.driver.wait(ExpectedConditions.visibilityOf($(search)), TIMEOUT_PERIOD);
+  browser.waitForAngular();
+  return $(search);
+}
+
+/**
+ * Wrapper around the standard $() and $$() Protractor functions that add extra waits
+ * required to make the tests work reliably in BrowserStack Automation.
+ * 
+ * @param search CSS element finder
+ */
+export let _$$ = (search): ElementArrayFinder => {
+  browser.driver.wait(ExpectedConditions.visibilityOf($$(search).first()), TIMEOUT_PERIOD);
+  browser.waitForAngular();
+  return $$(search);
+}
+
+/**
+ * Wrapper around the standard element.sendKeys() method that fixes sync issues causing
+ * some keys to be dropped.
+ * Bug reference: https://github.com/angular/protractor/issues/698
+ * 
+ * @param element The element the keys are being sent to
+ * @param keys The string of keys to be sent to the element
+ */
+export let sendKeysSafely = (element: ElementFinder, keys: string) => {
+  element.click().then(() => element.clear().then(() => {
+    keys.split('').forEach(c => element.sendKeys(c));
+  }));
+
+  browser.driver.sleep(3000); // Allow 3 seconds after typing input
+}
 
 /**
  * Tests the basic functionality of the ResearchHub, e.g. whether the home page
@@ -18,7 +59,7 @@ describe('ResearchHub\'s Basic Functionality', () => {
    * Visits the home page and checks it contains the heading 'Welcome to the ResearchHub'.
    */
   it('can display welcome message', async () => {
-    expect(await $('app-root h1').getText()).toEqual('Welcome to the ResearchHub');
+    expect(await _$('app-root h1').getText()).toEqual('Welcome to the ResearchHub');
   });
 
   /**
@@ -26,8 +67,8 @@ describe('ResearchHub\'s Basic Functionality', () => {
    * been successfully navigated to by checking the presence of the search results page title 'Results'.
    */
   it('can browse by category', async () => {
-    await $$('.tile-text').first().click();
-    const searchPageTitle = await $('.search-results-title').getText();
+    await _$$('.tile-text').first().click();
+    const searchPageTitle = await _$('.search-results-title').getText();
     expect(searchPageTitle).toEqual('Results');
   });
 
@@ -47,7 +88,8 @@ describe('ResearchHub\'s Search Functionality', () => {
    */
   it('can directly navigate to search results page', async () => {
     await page.navigateTo(browser.baseUrl + '/search');
-    expect(await $('.search-results-title').getText()).toEqual('Results');
+    await browser.driver.wait(ExpectedConditions.visibilityOf(_$('.search-results-title')), TIMEOUT_PERIOD);
+    expect(await _$('.search-results-title').getText()).toEqual('Results');
   });
 
   /**
@@ -56,8 +98,8 @@ describe('ResearchHub\'s Search Functionality', () => {
    */
   it('displays search results after typing in homepage search bar', async () => {
     await page.navigateTo(browser.baseUrl);
-    await $('app-search-bar input').sendKeys('vm');
-    expect(await $('.search-results-title').getText()).toEqual('Results');
+    sendKeysSafely(await _$('app-search-bar input'), 'biblioinformatics');
+    expect(await _$('.search-results-title').getText()).toEqual('Results');
   });
 
   /**
@@ -66,9 +108,10 @@ describe('ResearchHub\'s Search Functionality', () => {
    */
   it('displays correct search results that can be navigated to', async () => {
     await page.navigateTo(browser.baseUrl);
-    await $('app-search-bar input').sendKeys('biblioinformatics');
-    await $('.results-list .mat-list-item').click();
-    expect($('h2').getText()).toEqual('BiblioInformatics');
+    sendKeysSafely(await _$('app-search-bar input'), 'biblioinformatics');
+
+    await _$$('.results-list .mat-list-item').first().click();
+    expect(_$('h2').getText()).toEqual('BiblioInformatics');
   });
 
 });
@@ -92,10 +135,10 @@ describe('ResearchHub\'s Filter Functionality', () => {
     // These two variables store the number of results returned before and after filtering.
     let initialResultCount: number, filteredResultCount: number;
 
-    await $$('.search-results-text').first().getText().then(result => initialResultCount = parseInt(result));
-    await $$('.mat-slide-toggle-thumb').first().click();
+    await _$$('.search-results-text').first().getText().then(result => initialResultCount = parseInt(result));
+    await _$$('.mat-slide-toggle-thumb').first().click();
 
-    await $$('.search-results-text').first().getText().then(result => {
+    await _$$('.search-results-text').first().getText().then(result => {
       filteredResultCount = parseInt(result)
       expect(initialResultCount).toBeGreaterThan(filteredResultCount);
     });
@@ -117,7 +160,7 @@ describe('ResearchHub\'s Research Impact Content', () => {
    * Directly navigates to the Research Impact guide page and checks the presence of the title 'Research Impact Guide'.
    */
   it('can directly navigate to main guide page', async () => {
-    expect(await $('.description h2').getText()).toEqual('Research Impact Guide');
+    expect(await _$('.description h2').getText()).toEqual('Research Impact Guide');
   });
 
   /**
@@ -125,7 +168,7 @@ describe('ResearchHub\'s Research Impact Content', () => {
    * src attribute.
    */
   it('can load an iframe with youtube as src attribute', async () => {
-    expect(await $('.description iframe').getAttribute('src')).toContain('youtube');
+    expect(await _$('.description iframe').getAttribute('src')).toContain('youtube');
   });
 
   /**
@@ -133,8 +176,8 @@ describe('ResearchHub\'s Research Impact Content', () => {
    * its title is 'Planning for Impact'.
    */
   it('can correctly load a sub-page (guideCategory) item', async () => {
-    await $$('mat-grid-list .browse-tile').first().click();
-    expect(await $('.description h1').getText()).toEqual('Planning for Impact');
+    await _$$('mat-grid-list .browse-tile').first().click();
+    expect(await _$('.description h1').getText()).toEqual('Planning for Impact');
   });
 
   /**
@@ -142,8 +185,8 @@ describe('ResearchHub\'s Research Impact Content', () => {
    * the final part of the breadcrumbs is 'Planning for Impact'.
    */
   it('can display the breadcrumbs correctly', async () => {
-    await $$('mat-grid-list .browse-tile').first().click();
-    expect(await $('.description a:nth-of-type(3)').getText()).toEqual('Planning for Impact')
+    await _$$('mat-grid-list .browse-tile').first().click();
+    expect(await _$('.description a:nth-of-type(3)').getText()).toEqual('Planning for Impact')
   });
 
 });
