a21ccc8e8fea1b3ef8ec69bb9922344aa9ccd5e5
Hganavak <hganavak@gmail.com>
2020-08-28 10:33:10 +1200

    Increased timeout period

--- research-hub-web/e2e/app.e2e-spec.ts
+++ research-hub-web/e2e/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { ResearchHubPage } from './app.po';
 import { browser, by, element, $, $$, ExpectedConditions, ElementFinder, ElementArrayFinder } from 'protractor';
 
 let page: ResearchHubPage;
-const TIMEOUT_PERIOD = 12000;
+const TIMEOUT_PERIOD = 20000;
 
 /**
  * Wrapper around the standard $() and $$() Protractor functions that add extra waits
