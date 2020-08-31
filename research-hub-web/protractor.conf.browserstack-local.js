e538e026b5dc7b1fca08a8ec00a8c80af658a541
Hganavak <hganavak@gmail.com>
2020-08-31 14:31:37 +1200

    Updated e2e BrowserStack tsconfig references

--- research-hub-web/protractor.conf.browserstack-local.js
+++ research-hub-web/protractor.conf.browserstack-local.js
@@ -2,11 +2,15 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 // Note that this is the config file for e2e tests using BrowserStack's automation API (against local hosts). See protractor.conf.browserstack-remote.js for remote-test settings.
 
-const { SpecReporter } = require('jasmine-spec-reporter');
-var { 
-  BROWSERSTACK_CREDENTIALS_USER, 
-  BROWSERSTACK_CREDENTIALS_KEY 
-} = require('dotenv').config({path: '../.env'}).parsed;
+const {
+  SpecReporter,
+  StacktraceOption
+} = require('jasmine-spec-reporter');
+
+var {
+  BROWSERSTACK_CREDENTIALS_USER,
+  BROWSERSTACK_CREDENTIALS_KEY
+} = require('dotenv').config({ path: '../.env' }).parsed;
 var browserstack = require('browserstack-local');
 
 exports.config = {
@@ -17,9 +21,15 @@ exports.config = {
   },
   onPrepare: function () {
     require('ts-node').register({
-      project: require('path').join(__dirname, './e2e/tsconfig.json')
+      project: require('path').join(__dirname, 'tsconfig.e2e.json')
     });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    jasmine.getEnv().addReporter(
+      new SpecReporter({
+        spec: {
+          displayStacktrace: StacktraceOption.PRETTY
+        }
+      })
+    );
     browser.driver.manage().window().maximize();
   },
 
