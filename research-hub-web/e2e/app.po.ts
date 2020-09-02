6d1c13c615ddd436e7313aa142dde918471cf27e
Hganavak <hganavak@gmail.com>
2020-09-03 11:05:54 +1200

    testing with non-async navigate

--- research-hub-web/e2e/app.po.ts
+++ research-hub-web/e2e/app.po.ts
@@ -1,7 +1,7 @@
 import { browser } from 'protractor';
 
 export class ResearchHubPage {
-  async navigateTo(url) {
-    return await browser.driver.get(url);
+  navigateTo(url) {
+    return browser.driver.get(url);
   }
 }
