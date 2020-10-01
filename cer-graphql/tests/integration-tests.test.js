736de7090223c1d81eb70b8dab095fb210c0905a
warrickct <warrickct@gmail.com>
2020-10-01 16:30:20 +1300

    Revert "Merge branch 'integration-testing' into sandbox"
    This reverts commit 8a0608cfd8c640820ea0d4d5d7d6e6fc03bc5710, reversing
    changes made to 84580d88500466beb0921a60151b08c57e74676e.

--- cer-graphql/tests/integration-tests.test.js
+++ cer-graphql/tests/integration-tests.test.js
@@ -3,9 +3,6 @@ const { createServer, getCredentials } = require('../index')
 const TQ = require('./test-queries'); // Collection of test queries
 const { gql, introspectSchema, ApolloServer } = require('apollo-server');
 const { JsonWebTokenError } = require('jsonwebtoken');
-const aws = require('aws-sdk');
-const aws4 = require('aws4');
-const fetch = require('node-fetch');
 
 /**
  * This function creates both the ApolloServer and test client
@@ -15,65 +12,6 @@ async function createServerAndTestClient() {
     let server = await createServer(getCredentials(true));
     return createTestClient(new ApolloServer({ ...server, context: () => { } }));
 }
-
-/**
- * Creates as testing server using the real server's configuration 
- * with an injected context so we can add an authorization header to our query requests.
- */
-async function createServerAndTestClientWithAuth() {
-    let server = await createServer(getCredentials(true));
-    let tokens = await getTokens();
-    return createTestClient(new ApolloServer({
-        ...server.config,
-        context: () => server.config.context({
-            req: {
-                headers: {
-                    authorization: `Bearer ${tokens['access_token']}`
-                }
-            }
-        })
-    }));
-}
-
-/**
- * Gets OAuth tokens
- */
-const getTokens = async () => {
-    let awsCreds;
-    try {
-        awsCreds = new aws.SharedIniFileCredentials({
-            profile: 'saml',
-        });
-    } catch (error) {
-        console.log("Could not retrieve AWS credentials. Try re-running the credential python script.");
-    }
-
-    let opts = {
-        host: 'ef54vsv71a.execute-api.ap-southeast-2.amazonaws.com',
-        path: '/sandbox/',
-        region: 'ap-southeast-2',
-        service: 'execute-api',
-        'Accept': '*/*',
-        'Accept-Encoding': 'gzip, deflate, br'
-    };
-    aws4.sign(opts, {
-        accessKeyId: awsCreds.accessKeyId,
-        secretAccessKey: awsCreds.secretAccessKey,
-        sessionToken: awsCreds.sessionToken
-    });
-    let res = await fetch('https://ef54vsv71a.execute-api.ap-southeast-2.amazonaws.com/sandbox/', opts);
-    const resJson = await res.json();
-    try {
-        if (!resJson['access_token']) {
-            throw 'Fetching response for OAuth2.0 tokens does not contain access token.';
-        }
-    }
-    catch (error) {
-        console.error(error);
-    }
-    return resJson;
-}
-
 
 /**
  * Before any of the tests run create the query function and make
@@ -138,12 +76,6 @@ describe('Authorisation resolvers', () => {
         expect(res.errors[0].extensions.code).toEqual('UNAUTHENTICATED');
     });
 
-    test('Requesting an articleCollection non-public field with a valid Authorization header returns an response', async function () {
-        let { query } = await createServerAndTestClientWithAuth();
-        let res = await query({ query: TQ.GET_ARTICLE_COLLECTION_PRIVATE_WITH_SSO });
-        expect(res.data.articleCollection).toBeTruthy();
-    }, 20000);
-
     test('Requesting a article single resource non-public field returns an error', async function () {
         let res = await query({
             query: TQ.GET_ARTICLE_BY_SYS_ID_PRIVATE,
@@ -152,5 +84,4 @@ describe('Authorisation resolvers', () => {
 
         expect(res.errors[0].extensions.code).toEqual('UNAUTHENTICATED');
     });
-
 });
--- serverless-now/test/main.js
+++ serverless-now/test/main.js
@@ -3,46 +3,10 @@
 const mochaPlugin = require("serverless-mocha-plugin");
 const expect = mochaPlugin.chai.expect;
 let wrapped = mochaPlugin.getWrapper("main", "/handler.js", "main");
-const fetch = require('node-fetch');
-const aws = require('aws-sdk');
-const crypto = require('crypto');
-const aws4 = require('aws4');
 
 // Function to return the JSON parsed response body
 let getResBody = async (req) =>
   await wrapped.run(req).then((res) => JSON.parse(res.body));
-
-/**
- * Gets the credentials stored locally on file, 
- * assigns them to aws config and returns the credential object.
- */
-const getAwsCredentials = () => {
-  let credentials = new aws.SharedIniFileCredentials({
-    profile: 'saml',
-  });
-  credentials = credentials;
-  return credentials;
-}
-
-const getTokens = async () => {
-  let awsCreds = getAwsCredentials();
-  let opts = {
-    host: 'ef54vsv71a.execute-api.ap-southeast-2.amazonaws.com',
-    path: '/sandbox/',
-    region: 'ap-southeast-2',
-    service: 'execute-api',
-    'Accept': '*/*',
-    'Accept-Encoding': 'gzip, deflate, br'
-  };
-  aws4.sign(opts, {
-    accessKeyId: awsCreds.accessKeyId,
-    secretAccessKey: awsCreds.secretAccessKey,
-    sessionToken: awsCreds.sessionToken
-  });
-  let res = await fetch('https://ef54vsv71a.execute-api.ap-southeast-2.amazonaws.com/sandbox/', opts);
-  const resJson = await res.json();
-  return resJson;
-}
 
 describe("serverless-now", () => {
   // Example values used for ServiceNow testing
@@ -68,23 +32,17 @@ describe("serverless-now", () => {
     expect(resBody.number.value).to.equal(EXAMPLE_TICKET_ID);
   });
 
-  it("POST request returns a response from service now.", async function () {
-    this.timeout(20000);
-    let authTokens = await getTokens();
-    const resBody = await getResBody({
-      httpMethod: "POST",
-      headers: {
-        'Authorization': `Bearer ${authTokens['access_token']}`,
-        'Content-Type': 'application/json'
-      },
-      body: {
-        upi: EXAMPLE_UPI,
-        comment: "Example ticket comment.",
-      },
-    });
-    expect(resBody.status).to.equal('error');
-    return;
-  });
+  // TODO: commenting out until POST requests are working.
+  // it("responds to POST requests with a valid body", async () => {
+  //   const resBody = await getResBody({
+  //     httpMethod: "POST",
+  //     body: {
+  //       upi: EXAMPLE_UPI,
+  //       comment: "Example ticket comment.",
+  //     },
+  //   });
+  //   expect(resBody.object).deep.to.contain({ upi: EXAMPLE_UPI });
+  // });
 
   it("returns a decrypted example secret from AWS parameter store", async () => {
     const resBody = await getResBody({});
