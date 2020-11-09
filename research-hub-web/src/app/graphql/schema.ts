c989235165da5f72e8686209546c214fe5a4988e
juliuskarl <julius.karl.macrohon@gmail.com>
2020-11-10 09:41:33 +1300

    displaying keywords

--- research-hub-web/src/app/graphql/schema.ts
+++ research-hub-web/src/app/graphql/schema.ts
@@ -2728,6 +2728,7 @@ export type TestContentType = Entry & {
   sys: Sys;
   linkedFrom: Maybe<TestContentTypeLinkingCollections>;
   shortTxt: Maybe<Scalars['String']>;
+  test1: Maybe<Scalars['String']>;
 };
 
 
@@ -2739,6 +2740,12 @@ export type TestContentTypeLinkedFromArgs = {
 
 /** [See type definition](https://app.contentful.com/spaces/vbuxn5csp0ik/content_types/testContentType) */
 export type TestContentTypeShortTxtArgs = {
+  locale: Maybe<Scalars['String']>;
+};
+
+
+/** [See type definition](https://app.contentful.com/spaces/vbuxn5csp0ik/content_types/testContentType) */
+export type TestContentTypeTest1Args = {
   locale: Maybe<Scalars['String']>;
 };
 
@@ -2764,6 +2771,13 @@ export type TestContentTypeFilter = {
   shortTxt_not_in: Maybe<Array<Maybe<Scalars['String']>>>;
   shortTxt_contains: Maybe<Scalars['String']>;
   shortTxt_not_contains: Maybe<Scalars['String']>;
+  test1_exists: Maybe<Scalars['Boolean']>;
+  test1: Maybe<Scalars['String']>;
+  test1_not: Maybe<Scalars['String']>;
+  test1_in: Maybe<Array<Maybe<Scalars['String']>>>;
+  test1_not_in: Maybe<Array<Maybe<Scalars['String']>>>;
+  test1_contains: Maybe<Scalars['String']>;
+  test1_not_contains: Maybe<Scalars['String']>;
   OR: Maybe<Array<Maybe<TestContentTypeFilter>>>;
   AND: Maybe<Array<Maybe<TestContentTypeFilter>>>;
 };
@@ -2771,6 +2785,8 @@ export type TestContentTypeFilter = {
 export enum TestContentTypeOrder {
   ShortTxtAsc = 'shortTxt_ASC',
   ShortTxtDesc = 'shortTxt_DESC',
+  Test1Asc = 'test1_ASC',
+  Test1Desc = 'test1_DESC',
   SysIdAsc = 'sys_id_ASC',
   SysIdDesc = 'sys_id_DESC',
   SysPublishedAtAsc = 'sys_publishedAt_ASC',
@@ -3449,7 +3465,7 @@ export type AllSubHubChildPagesQueryVariables = Exact<{
 }>;
 
 
-export type AllSubHubChildPagesQuery = { __typename?: 'Query', subHubCollection: Maybe<{ __typename?: 'SubHubCollection', items: Array<Maybe<{ __typename?: 'SubHub', slug: Maybe<string>, title: Maybe<string>, viewType: Maybe<string>, summary: Maybe<string>, ssoProtected: Maybe<boolean>, searchable: Maybe<boolean>, body: Maybe<{ __typename?: 'SubHubBody', json: any }>, subhubPagesCollection: Maybe<{ __typename?: 'SubHubSubhubPagesCollection', items: Array<Maybe<{ __typename: 'Article', slug: Maybe<string>, title: Maybe<string>, ssoProtected: Maybe<boolean>, summary: Maybe<string> } | { __typename: 'CaseStudy', slug: Maybe<string>, title: Maybe<string>, ssoProtected: Maybe<boolean>, summary: Maybe<string> } | { __typename: 'Equipment', slug: Maybe<string>, title: Maybe<string>, ssoProtected: Maybe<boolean>, summary: Maybe<string> } | { __typename: 'OfficialDocuments', title: Maybe<string>, summary: Maybe<string> } | { __typename: 'Service', slug: Maybe<string>, title: Maybe<string>, ssoProtected: Maybe<boolean>, summary: Maybe<string> } | { __typename: 'SubHub', slug: Maybe<string>, title: Maybe<string>, ssoProtected: Maybe<boolean>, summary: Maybe<string> }>> }> }>> }> };
+export type AllSubHubChildPagesQuery = { __typename?: 'Query', subHubCollection: Maybe<{ __typename?: 'SubHubCollection', items: Array<Maybe<{ __typename?: 'SubHub', slug: Maybe<string>, title: Maybe<string>, viewType: Maybe<string>, summary: Maybe<string>, keywords: Maybe<Array<Maybe<string>>>, ssoProtected: Maybe<boolean>, searchable: Maybe<boolean>, body: Maybe<{ __typename?: 'SubHubBody', json: any }>, subhubPagesCollection: Maybe<{ __typename?: 'SubHubSubhubPagesCollection', items: Array<Maybe<{ __typename: 'Article', slug: Maybe<string>, title: Maybe<string>, ssoProtected: Maybe<boolean>, summary: Maybe<string> } | { __typename: 'CaseStudy', slug: Maybe<string>, title: Maybe<string>, ssoProtected: Maybe<boolean>, summary: Maybe<string> } | { __typename: 'Equipment', slug: Maybe<string>, title: Maybe<string>, ssoProtected: Maybe<boolean>, summary: Maybe<string> } | { __typename: 'OfficialDocuments', title: Maybe<string>, summary: Maybe<string> } | { __typename: 'Service', slug: Maybe<string>, title: Maybe<string>, ssoProtected: Maybe<boolean>, summary: Maybe<string> } | { __typename: 'SubHub', slug: Maybe<string>, title: Maybe<string>, ssoProtected: Maybe<boolean>, summary: Maybe<string> }>> }> }>> }> };
 
 export type GetArticleByIdQueryVariables = Exact<{
   id: Scalars['String'];
@@ -3683,6 +3699,7 @@ export const AllSubHubChildPagesDocument = gql`
       body {
         json
       }
+      keywords
       ssoProtected
       searchable
       subhubPagesCollection {
