d5ad137f63eca41f7c3d2bc54d3f6596edf2ad3e
juliuskarl <julius.karl.macrohon@gmail.com>
2020-12-02 11:15:16 +1300

    pr change requests 03/12/20

--- research-hub-web/src/app/components/about/about.component.ts
+++ research-hub-web/src/app/components/about/about.component.ts
@@ -10,7 +10,7 @@ import { ResearchHubApiService } from '@services/research-hub-api.service';
 export class AboutComponent {
   public title: String = "About";
   public summary: String = "The Centre for eResearch comprises a team of highly qualified research and technical staff dedicated to the delivery of advanced computational solutions to help power the University's research mission.";
-  public coverImageUrl: String = 'url(../../../assets/images/banner15.jpg)';
+  public coverImageUrl: String = 'url(assets/images/banner15.jpg)';
 
   theDirectorUrl = this.apiService.getAssetUrl('page-elements/mark.jpg');
 
