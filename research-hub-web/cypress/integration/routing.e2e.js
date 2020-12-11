ce43693767a1c32325fea04cb0a96158f9556fac
juliuskarl <julius.karl.macrohon@gmail.com>
2020-12-11 20:54:03 +1300

    deploying

--- research-hub-web/cypress/integration/routing.e2e.js
+++ research-hub-web/cypress/integration/routing.e2e.js
@@ -16,25 +16,25 @@ describe('ResearchHubs Static Routing', () => {
     });
 });
 
-xdescribe('ResearchHubs Dynamic SubHub Routing', () => {
+// describe('ResearchHubs Dynamic SubHub Routing', () => {
 
-    it('can visit /cer and load a SubHub', () => {
-        cy.visit('/cer');
-        cy.contains('Centre for eResearch');
-        cy.contains('CeR. A root level SubHub.');
-    });
+//     it('can visit /cer and load a SubHub', () => {
+//         cy.visit('/cer');
+//         cy.contains('Centre for eResearch');
+//         cy.contains('CeR. A root level SubHub.');
+//     });
 
-    it('can visit /cer/our-services/engagement/first-article and load an Article', () => {
-        cy.visit('/cer/our-services/engagement/first-article');
-        cy.contains('Have a good day.');
-        cy.get('#article-container').should('exist')
-    });
+//     it('can visit /cer/our-services/engagement/first-article and load an Article', () => {
+//         cy.visit('/cer/our-services/engagement/first-article');
+//         cy.contains('Have a good day.');
+//         cy.get('#article-container').should('exist')
+//     });
 
-    it('will update a content item\'s URL when it is visited from outside the SubHub', () => {
-        cy.visit('/articles');
-        cy.get('mat-nav-list > mat-card')
-            .contains('Test - First article').click();
-        cy.url().should('include', '/cer/');
-    })
+//     it('will update a content item\'s URL when it is visited from outside the SubHub', () => {
+//         cy.visit('/articles');
+//         cy.get('mat-nav-list > mat-card')
+//             .contains('Test - First article').click();
+//         cy.url().should('include', '/cer/');
+//     })
 
-}); 
+// }); 
