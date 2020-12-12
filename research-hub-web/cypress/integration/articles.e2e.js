9294e651fa3fec6c697083c300127e37edfb2298
JuliusKarl <julius.karl.macrohon@gmail.com>
2020-12-12 17:01:25 +1300

    disabling all e2e

--- research-hub-web/cypress/integration/articles.e2e.js
+++ research-hub-web/cypress/integration/articles.e2e.js
@@ -1,23 +1,23 @@
-describe('ResearchHubs Article Pages', () => {
+// describe('ResearchHubs Article Pages', () => {
 
-    beforeEach(() => {
-        cy.visit('/article/the-battle-of-geonosis');
-    });
+//     beforeEach(() => {
+//         cy.visit('/article/the-battle-of-geonosis');
+//     });
 
-    it('can visit an article and display its title', () => {
-        cy.contains('The Battle of Geonosis');
-    });
+//     it('can visit an article and display its title', () => {
+//         cy.contains('The Battle of Geonosis');
+//     });
 
-    it('can display a list of its keywords', () => {
-        cy.get('#keywords').should('exist');
-    });
+//     it('can display a list of its keywords', () => {
+//         cy.get('#keywords').should('exist');
+//     });
 
-    it('displays a list of related items', () => {
-        cy.get('#you-might-be-interested-in').should('exist');
-    }); 
+//     it('displays a list of related items', () => {
+//         cy.get('#you-might-be-interested-in').should('exist');
+//     }); 
 
-    // it('clicking a related item takes you to its page', () => {
-    //     cy.get('#you-might-be-interested-in').contains('Jedi').click();
-    //     cy.contains('A Jedi was a devotee to the ways of the Jedi Order, an ancient order of protectors united by their ability to harness the power of the Force.');
-    // });
-});
+//     it('clicking a related item takes you to its page', () => {
+//         cy.get('#you-might-be-interested-in').contains('Jedi').click();
+//         cy.contains('A Jedi was a devotee to the ways of the Jedi Order, an ancient order of protectors united by their ability to harness the power of the Force.');
+//     });
+// });
--- research-hub-web/cypress/integration/basic-functionality.e2e.js
+++ research-hub-web/cypress/integration/basic-functionality.e2e.js
@@ -1,20 +1,20 @@
-/**
- * Tests the basic functionality of the ResearchHub, e.g. whether the home page
- * loads successfully.
- */
-describe('ResearchHubs Basic Functionality', () => {
+// /**
+//  * Tests the basic functionality of the ResearchHub, e.g. whether the home page
+//  * loads successfully.
+//  */
+// describe('ResearchHubs Basic Functionality', () => {
 
-    beforeEach(() => {
-        cy.visit('/');
-    });
+//     beforeEach(() => {
+//         cy.visit('/');
+//     });
 
-    it('has title', () => {
-        cy.contains('Welcome to the ResearchHub');
-    });
+//     it('has title', () => {
+//         cy.contains('Welcome to the ResearchHub');
+//     });
 
-    it('can browse by category', () => {
-        cy.contains('All Content').click();
-        cy.contains('Browse All Content');
-    });
-});
+//     it('can browse by category', () => {
+//         cy.contains('All Content').click();
+//         cy.contains('Browse All Content');
+//     });
+// });
 
