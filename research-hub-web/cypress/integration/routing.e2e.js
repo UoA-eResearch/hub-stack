90136b820b7ad2972d650f80ad65a2b784fc8971
JuliusKarl <julius.karl.macrohon@gmail.com>
2021-01-26 09:52:23 +1300

    updating tests

--- research-hub-web/cypress/integration/routing.e2e.js
+++ research-hub-web/cypress/integration/routing.e2e.js
@@ -21,13 +21,13 @@ describe('ResearchHubs Dynamic SubHub Routing', () => {
 
     it('can visit /Centre-for-eResearch and load a SubHub', () => {
         cy.visit('/Centre-for-eResearch ');
-        cy.contains('Pages in this SubHub');
-        cy.contains('CeR. A root level SubHub.');
+        cy.contains('Demo - Centre for eResearch');
+        cy.contains('The Centre for eResearch is centrally funded by the University to deliver the services to help researchers address challenging computational problems. These services are generally free to use for research staff and students.');
     });
 
     it('can visit /our-services/engagement/first-article and load an Article', () => {
         cy.visit('/our-services/engagement/first-article');
-        cy.contains('The Centre for eResearch is centrally funded by the University to deliver the services to help researchers address challenging computational problems. These services are generally free to use for research staff and students.');
+        cy.contains('Have a good day.');
         cy.get('#article-container').should('exist')
     });
 
