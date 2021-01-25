7ee20781090a852d6fc02cc75c0ab5bee35efdc2
JuliusKarl <julius.karl.macrohon@gmail.com>
2021-01-26 10:03:02 +1300

    hotfix

--- research-hub-web/src/app/components/subhubs/subhubs.component.ts
+++ research-hub-web/src/app/components/subhubs/subhubs.component.ts
@@ -1,16 +1,13 @@
-import { Component, OnInit, Type } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { pluck, map, flatMap, tap, catchError } from 'rxjs/operators';
+import { pluck, flatMap } from 'rxjs/operators';
 import {
-  AllSubHubGQL,
-  GetSubHubByIdGQL,
-  GetSubHubBySlugGQL,
+  AllSubHubChildPagesGQL,
   SubHubCollection,
   SubHub
-} from "@graphql/schema";
-import { CerGraphqlService } from "@services/cer-graphql.service";
-import { AppComponentService } from '../../app.component.service';
+} from '@graphql/schema';
+import { CerGraphqlService } from '@services/cer-graphql.service';
 
 
 @Component({
@@ -21,23 +18,17 @@ import { AppComponentService } from '../../app.component.service';
 export class SubhubsComponent implements OnInit {
 
   public subhub$: Observable<SubHubCollection>;
-  public currentSubHub$: Observable<SubHub>;
   public parentSubHubs;
   public allSubHubs$: Observable<SubHubCollection>;
   public slug: string;
 
   constructor(
     private route: ActivatedRoute,
-    public router: Router,
-    public AllSubHubGQL: AllSubHubGQL,
-    public GetSubHubBySlug: GetSubHubBySlugGQL,
-    public GetSubHubById: GetSubHubByIdGQL,
-    public cerGraphQLService: CerGraphqlService,
-    public appComponentService: AppComponentService,
+    public AllSubHubChildPagesGQL: AllSubHubChildPagesGQL,
+    public cerGraphQLService: CerGraphqlService
   ) { }
 
   async ngOnInit() {
-
     /**
      * Check if there is a slug URL parameter present. If so, this is
      * passed to the getArticleBySlug() method.
@@ -53,15 +44,10 @@ export class SubhubsComponent implements OnInit {
    */
   private async _loadContent() {
     if (!!this.slug) {
-      this.getSubHubBySlug(this.slug).subscribe(data => {
-        this.currentSubHub$ = this.getSubHubById(data.sys.id);
-        this.currentSubHub$.subscribe(res => {
-          this.appComponentService.setTitle(res.title);
-        });
-      });
+      this.subhub$ = this.getSubHub(this.slug);
       this.parentSubHubs = await this.cerGraphQLService.getParentSubHubs(this.slug);
     } else {
-      this.allSubHubs$ = this.getAllSubHubs();
+      this.allSubHubs$ = this.getAllSubHubs(this.slug);
     }
   }
 
@@ -69,36 +55,24 @@ export class SubhubsComponent implements OnInit {
    * Runs the query for the main body of a subhub item as including it's child pages but excluding it's ancestor/parent data.
    * @param slug Page slug
    */
-  public getAllSubHubs(): Observable<SubHubCollection> {
+  public getAllSubHubs(slug: string): Observable<SubHubCollection> {
     try {
-      return this.AllSubHubGQL.fetch()
+      return this.AllSubHubChildPagesGQL
+        .fetch()
         .pipe(pluck('data', 'subHubCollection')) as Observable<SubHubCollection>;
     } catch (e) {
       console.error('Error loading subhub body info and children')
     }
   }
 
-  /**
-   * Get Subhub by passing the slug into the SubhubCollection query
-   * @param slug 
-   */
-  public getSubHubBySlug(slug: string): Observable<SubHub> {
+  public getSubHub(slug: string): Observable<SubHubCollection> {
     try {
-      return this.GetSubHubBySlug.fetch({ slug: this.slug })
-        .pipe(flatMap(x => x.data.subHubCollection.items)) as Observable<SubHub>;
+      return this.AllSubHubChildPagesGQL.fetch({
+        slug
+      }).pipe(pluck('data', 'subHubCollection')) as Observable<SubHubCollection>;
     } catch (e) {
       console.error('Error loading subhub body info and children')
     }
   }
 
-  /**
-   * Get Subhub by passing id into the Subhub query
-   * @param id 
-   */
-  public getSubHubById(id: string): Observable<SubHub> {
-    try {
-      return this.GetSubHubById.fetch({id: id})
-      .pipe(map(x => x.data.subHub), catchError(err => (this.router.navigate(['/error/500'])))) as Observable<SubHub>;
-    } catch (e) { console.error(`Error loading subhub ${id}:`, e); }
-  }
 }
--- research-hub-web/src/app/components/subhubs/subhubs.module.ts
+++ research-hub-web/src/app/components/subhubs/subhubs.module.ts
@@ -4,15 +4,13 @@ import { CommonModule } from '@angular/common';
 import { SubhubsRoutingModule } from './subhubs-routing.module';
 import { SubhubsComponent } from './subhubs.component';
 import { SharedModule } from '@components/shared/app.shared.module';
-import { NgxContentfulRichTextModule } from 'ngx-contentful-rich-text';
 
 @NgModule({
   declarations: [SubhubsComponent],
   imports: [
     CommonModule,
     SubhubsRoutingModule,
-    SharedModule,
-    NgxContentfulRichTextModule
+    SharedModule
   ]
 })
 export class SubhubsModule { }
