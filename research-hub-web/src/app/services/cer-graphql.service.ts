5474fac59995241c0020627889038f0f676dde16
Noel <noel.zeng@auckland.ac.nz>
2021-02-04 16:07:50 +1300

    Further clarify pushsubhubroutes code

--- research-hub-web/src/app/services/cer-graphql.service.ts
+++ research-hub-web/src/app/services/cer-graphql.service.ts
@@ -35,15 +35,15 @@ export class CerGraphqlService {
     this.hasPushedSubhubRoutes = true;
     const routes = this.router.config;
     await this._generateSubHubMapAndRoutes(); // Generate _subHubMap.map and _subHubMap.routes
-    // Check if there's a wildcard route in our configuration.
+    // Check if there's a wildcard route in our configuration...
     const wildcardRouteIdx = routes.findIndex((route) => route.path === "**");
     let  newRoutes;
     if (wildcardRouteIdx > -1) {
-      // If so, insert the new routes before the wildcard route, so the wildcard is not matched 
-      // against a path before they are tested with the new routes. 
+      // ...if so, insert the new routes before the wildcard route, so during routing
+      // the new routes are matched before the wildcard route.
       const routesBeforeWildcard = routes.slice(0, wildcardRouteIdx);
-      const routesAfterWildcard = routes.slice(wildcardRouteIdx, routes.length);
-      newRoutes = routesBeforeWildcard.concat(this._subHubMap.routes, routesAfterWildcard);
+      const wildcardRouteAndAfter = routes.slice(wildcardRouteIdx, routes.length);
+      newRoutes = routesBeforeWildcard.concat(this._subHubMap.routes, wildcardRouteAndAfter);
     } else {
       newRoutes = routes.concat(this._subHubMap.routes);
     }
