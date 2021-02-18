e2231742755b3d6d275899b54809c5b426e58db0
JuliusKarl <julius.karl.macrohon@gmail.com>
2021-02-18 16:25:13 +1300

    prototype one

--- research-hub-web/src/app/components/home/browse/browse.component.ts
+++ research-hub-web/src/app/components/home/browse/browse.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 import { MediaChange, MediaObserver } from '@angular/flex-layout';
 import { LayoutService } from '@services/layout.service';
 import { 
   categoryOptions,
   categoryOptionsGQL 
 } from '@app/global/global-variables';
+import { AllCategoriesGQL, AllEventsGQL, CategoryCollection, EventCollection } from '@app/graphql/schema';
+import { pluck } from 'rxjs/operators';
 
 @Component({
   selector: 'app-browse',
@@ -17,7 +19,8 @@ export class BrowseComponent implements OnInit, OnDestroy {
   public description = "The University of Auckland provides top-quality support to our research community. The ResearchHub is your gateway to research support at the University of Auckland. Here you can explore what's on offer by topic.";
   public categoryOptions = categoryOptions;
   public categoryOptionsGQL = categoryOptionsGQL;
-
+  public allCategories$: Observable<CategoryCollection>;
+  public allEvents$: Observable<EventCollection>;
   private mediaSub: Subscription;
 
   @Input()
@@ -29,16 +32,39 @@ export class BrowseComponent implements OnInit, OnDestroy {
   @Input()
   numCols = 4;
 
-  constructor(private media: MediaObserver,
-    private layoutService: LayoutService) {
+  constructor(
+    private media: MediaObserver,
+    private layoutService: LayoutService,
+    public allCategoriesGQL: AllCategoriesGQL,
+    public allEventsGQL: AllEventsGQL,) {
   }
 
-  ngOnInit() {
+  async ngOnInit() {
     this.updateCols(this.layoutService.getMQAlias());
 
     this.mediaSub = this.media.media$.subscribe((change: MediaChange) => {
       this.updateCols(change.mqAlias);
     });
+
+    // Get all categories
+    this.allCategories$ = this.getAllCategories();
+
+    // Get all Events
+    this.allEvents$ = this.getAllEvents();
+  }
+
+  public getAllCategories(): Observable<CategoryCollection> {
+    try {
+      return this.allCategoriesGQL.fetch()
+        .pipe(pluck('data', 'categoryCollection')) as Observable<CategoryCollection>
+    } catch (e) { console.error('Error loading all Categories:', e) };
+  }
+
+  public getAllEvents(): Observable<EventCollection> {
+    try {
+      return this.allEventsGQL.fetch()
+        .pipe(pluck('data', 'eventCollection')) as Observable<EventCollection>
+    } catch (e) { console.error('Error loading all events:', e) };
   }
 
   updateCols(mqAlias: string) {
