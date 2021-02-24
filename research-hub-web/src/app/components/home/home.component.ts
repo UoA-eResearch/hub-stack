f7c91dc07dde732bdb35a959fe04980708872ea1
JuliusKarl <julius.karl.macrohon@gmail.com>
2021-02-25 12:49:10 +1300

    homepage

--- research-hub-web/src/app/components/home/home.component.ts
+++ research-hub-web/src/app/components/home/home.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchBarService } from '../search-bar/search-bar.service';
-import { Message, feedbackLink, userStudyLink } from '../../global/global-variables';
+import { 
+  Message, 
+  feedbackLink, 
+  userStudyLink 
+} from '../../global/global-variables';
 
 @Component({
   selector: 'app-home',
@@ -8,16 +12,21 @@ import { Message, feedbackLink, userStudyLink } from '../../global/global-variab
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  // About
+  /**
+   * About Section
+   */
   public quote = Message;
   public director = "- Mark Gahegan, Director of Centre for eResearch";
 
-  // Contact
+
+  /**
+   * Contact Section
+   */
   public title = "Contact";
   public description = "If you want to get in touch you can ring us, write to us or even visit us. We'd love to hear from you.";
   public email = "eresearch-support@auckland.ac.nz";
   public phone = "+64 9 373 7599 ext 82231";
-  public  building = "assets/images/cer-level5-20170905.svg";
+  public building = "assets/images/cer-level5-20170905.svg";
   public feedbackLink = feedbackLink;
   public userStudyLink = userStudyLink;
 
@@ -25,20 +34,5 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
-    enum CategoryId {
-      All = 1,
-      Support,
-      Equipment,
-      Training,
-      Software,
-      Facilities,
-      Guide,
-      Person,
-      Policies,
-      Articles,
-      SubHubs
-    }
-    this.searchBarService.setSearchText('');
-    this.searchBarService.setCategory(CategoryId.All);
   }
 }
