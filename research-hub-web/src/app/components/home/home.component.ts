5b3f026696120736dc472e53edfaa0d6a2833cd7
JuliusKarl <julius.karl.macrohon@gmail.com>
2021-02-24 16:01:46 +1300

    homepage design prototype

--- research-hub-web/src/app/components/home/home.component.ts
+++ research-hub-web/src/app/components/home/home.component.ts
@@ -8,8 +8,18 @@ import { Message } from '../../global/global-variables';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  public message = Message;
+  // About
+  public quote = Message;
   public director = "- Mark Gahegan, Director of Centre for eResearch";
+
+  // Contact
+  public title = "Contact";
+  public description = "If you want to get in touch you can ring us, write to us or even visit us. We'd love to hear from you.";
+  public email = "eresearch-support@auckland.ac.nz";
+  public phone = "+64 9 373 7599 ext 82231";
+  public  building = "assets/images/cer-level5-20170905.svg";
+  public userStudyLink = "https://docs.google.com/forms/d/e/1FAIpQLSeXhrPKLqmdAr_r3aUwY9zyPa5REWJs63FQdmNwRbXKBUS8WQ/viewform";
+  public feedbackLink = "https://docs.google.com/forms/d/e/1FAIpQLSdxSyxLBBzexHDgPmjoAukxDzDo3fRHfKi4TmqFHYxa0dB37g/viewform";
 
   constructor(private searchBarService: SearchBarService) {
   }
