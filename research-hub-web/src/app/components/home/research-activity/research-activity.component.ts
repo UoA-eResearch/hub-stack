bd5040d3d40565280e8cdd48f8c4b1206078f5d5
JuliusKarl <julius.karl.macrohon@gmail.com>
2021-02-25 04:25:53 +1300

    refacgtor browse and activity

--- research-hub-web/src/app/components/home/research-activity/research-activity.component.ts
+++ research-hub-web/src/app/components/home/research-activity/research-activity.component.ts
@@ -1,16 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { 
-  ResearchActivityId, 
-  researchActivityOptions,
-  OptionType
-} from '@app/global/global-variables';
+import { researchActivityOptions } from '@app/global/global-variables';
 
 @Component({
   selector: 'app-research-activity',
   templateUrl: './research-activity.component.html',
   styleUrls: ['./research-activity.component.scss']
 })
-export class ResearchActivityComponent implements OnInit {
+export class ResearchActivityComponent {
   public title = 'Research Activities';
   public description = 'The research lifecycle describes the research journey from project inception to completion. It highlights five project stages. Below you can explore what the University of Auckland provides to support you according to where you are in your research journey.';
   public researchActivityOptions = researchActivityOptions;
@@ -18,9 +14,6 @@ export class ResearchActivityComponent implements OnInit {
   constructor() {}
 
   getQueryParams(activity) {
-    return { researchActivityId: [activity.id] };
-  }
-
-  ngOnInit() {
+    return { researchActivityId: [activity.slug] };
   }
 }
