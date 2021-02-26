53e66c83f15b4f7bfb5fe7298dbb3cd5728be923
JuliusKarl <julius.karl.macrohon@gmail.com>
2021-02-27 01:26:19 +1300

    wcag 2

--- research-hub-web/src/app/components/shared/cards/cards.component.ts
+++ research-hub-web/src/app/components/shared/cards/cards.component.ts
@@ -15,15 +15,6 @@ export class CardsComponent implements OnInit {
 
   async ngOnInit() {
 
-    // If card is displaying a Person
-    if (this.contentItem.items[0]?.__typename == 'Person') {
-      this.contentItem.items.forEach(element => {
-        element['title']= element['name'];
-        element['summary'] = element['role'];
-        element['__typename'] = 'Contact'
-      });
-    }
-
     // If you want to hide image when displayed
     if (this.hideImage) {
       this.contentItem.items.forEach(element => {
