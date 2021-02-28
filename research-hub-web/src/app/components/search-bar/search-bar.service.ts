48a380449e5acd1b2b134cee382cdad4991549f0
JuliusKarl <julius.karl.macrohon@gmail.com>
2021-03-01 11:34:16 +1300

    create search page

--- research-hub-web/src/app/components/search-bar/search-bar.service.ts
+++ research-hub-web/src/app/components/search-bar/search-bar.service.ts
@@ -56,6 +56,9 @@ export class SearchBarService {
   }
 
   getSearchParams(): SearchBarParams {
-    return {searchText: this.searchText, category: this.category} as SearchBarParams;
+    return {
+      searchText: this.searchText,
+      category: this.category
+    } as SearchBarParams;
   }
 }
