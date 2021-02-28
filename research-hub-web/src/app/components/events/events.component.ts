87237e49628f019e2c59cfea41794a7827d48d1a
JuliusKarl <julius.karl.macrohon@gmail.com>
2021-03-01 00:41:39 +1300

    event callToAction email

--- research-hub-web/src/app/components/events/events.component.ts
+++ research-hub-web/src/app/components/events/events.component.ts
@@ -70,7 +70,12 @@ export class EventsComponent implements OnInit, OnDestroy {
     if (!!this.slug) {
       this.event = this.getEventBySlug(this.slug);
       this.event$ = this.event.subscribe(data => {
-          this.bodyMediaService.setBodyMedia(data.bodyText.links);
+
+        // If Call To Action is an email address
+        if (data.callToAction.match( /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+          data['callToAction'] = 'mailto:' + data['callToAction'];
+        }
+        this.bodyMediaService.setBodyMedia(data.bodyText.links);
         this.appComponentService.setTitle(data.title);
       });
       this.parentSubHubs = await this.cerGraphQLService.getParentSubHubs(this.slug);
