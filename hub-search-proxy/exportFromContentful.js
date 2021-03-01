f85fe4a370e4fcf4e0afefba09f27c66f4b8fd56
Rose McColl <rosemccoll@hotmail.com>
2021-03-02 09:35:51 +1300

    tidy up bulk loader and filter entries on valid types

--- hub-search-proxy/exportFromContentful.js
+++ hub-search-proxy/exportFromContentful.js
@@ -3,20 +3,17 @@ const contentfulExport = require('contentful-export');
 const token = process.env.CONTENTFUL_ACCESS_TOKEN;
 const spaceId = process.env.CONTENTFUL_SPACE_ID;
 
-
-module.exports.contentfulExporter = () => {
+async function contentfulExporter() {
   const options = {
     spaceId: spaceId,
     managementToken: token,
     contentOnly: true,
-    downloadAssets: false
+    downloadAssets: false,
+    saveFile: false
   };
 
-  contentfulExport(options)
-  .then((result) => {
-    console.log('Your space data:', result)
-  })
-  .catch((err) => {
-    console.log('Oh no! Some errors occurred!', err)
-  });
+  return contentfulExport(options);
 };
+
+export { contentfulExporter };
+
