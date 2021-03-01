c0b291f2be3515db3f6bd51e7ff4abc6b49d1629
JuliusKarl <julius.karl.macrohon@gmail.com>
2021-03-02 11:04:20 +1300

    getting filters

--- research-hub-web/src/app/components/search-page/search-page.component.ts
+++ research-hub-web/src/app/components/search-page/search-page.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { 
+  AllCategoriesGQL,
+  AllStagesGQL,
+  AllOrganisationsGQL,
+  CategoryCollection,
+  OrgUnitCollection,
+  StageCollection
+} from '@app/graphql/schema';
+import { Observable } from 'rxjs';
+import { pluck } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-page',
@@ -6,10 +16,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-page.component.scss']
 })
 export class SearchPageComponent implements OnInit {
+  public allCategories$: Observable<CategoryCollection>;
+  public allStages$: Observable<StageCollection>;
+  public allOrganisations$: Observable<OrgUnitCollection>;
 
-  constructor() { }
+  constructor(
+    public allCategoriesGQL: AllCategoriesGQL,
+    public allStagesGQL: AllStagesGQL,
+    public allOrganisationsGQL: AllOrganisationsGQL
+    ) {}
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.allCategories$ = this.getAllCategories();
+    this.allStages$ = this.getAllStages();
+    this.allOrganisations$ = this.getAllOrganisations();
   }
 
+  // Get all research stages
+  public getAllStages(): Observable<StageCollection> {
+    try {
+      return this.allStagesGQL.fetch()
+        .pipe(pluck('data', 'stageCollection')) as Observable<StageCollection>
+    } catch (e) { console.error('Error loading all stages:', e) };
+  }
+
+  // Get all research categories
+  public getAllCategories(): Observable<CategoryCollection> {
+    try {
+      return this.allCategoriesGQL.fetch()
+        .pipe(pluck('data', 'categoryCollection')) as Observable<CategoryCollection>
+    } catch (e) { console.error('Error loading all Categories:', e) };
+  }
+
+  // Get all organisations
+  public getAllOrganisations(): Observable<OrgUnitCollection> {
+    try {
+      return this.allOrganisationsGQL.fetch()
+        .pipe(pluck('data', 'orgUnitCollection')) as Observable<OrgUnitCollection>
+    } catch (e) { console.error('Error loading all organisations:', e) };
+  }
 }
