88a787635fac76ec6781119ddd7aeb8eaadd2008
JuliusKarl <julius.karl.macrohon@gmail.com>
2021-03-03 11:49:03 +1300

    search page service

--- research-hub-web/src/environments/environment.ts
+++ research-hub-web/src/environments/environment.ts
@@ -18,6 +18,7 @@ export const environment = {
     codeChallengeMethod: 'S256',
     logout_uri: 'http://localhost:4200'
   },
+  searchUrl: 'https://apigw.test.amazon.auckland.ac.nz/hub-search-proxy',
   privateUrlKeyWords: {
     get whoNeedBearerToken() {
       return [
