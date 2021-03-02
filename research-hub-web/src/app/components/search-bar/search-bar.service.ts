901b32c34df31f586e0ba2f5f0cb6ea5196ccdcf
JuliusKarl <julius.karl.macrohon@gmail.com>
2021-03-03 11:54:20 +1300

    search page service

--- research-hub-web/src/app/components/search-bar/search-bar.service.ts
+++ research-hub-web/src/app/components/search-bar/search-bar.service.ts
@@ -1,6 +1,18 @@
 import {Injectable} from '@angular/core';
-import {Subject} from 'rxjs';
-
+import { 
+  AllCategoriesGQL,
+  AllStagesGQL,
+  AllOrganisationsGQL,
+  AllPagesGQL,
+  AllItemsByCategoryGQL,
+  AllItemsByStageGQL,
+  AllItemsByOrganisationGQL,
+  CategoryCollection,
+  OrgUnitCollection,
+  StageCollection
+} from '@app/graphql/schema';
+import { Observable, Subject } from 'rxjs';
+import { pluck } from 'rxjs/operators';
 
 export class SearchBarParams {
   public category: string;
@@ -20,7 +32,15 @@ export class SearchBarService {
   public searchText: string;
   public category: string;
 
-  constructor() { }
+  constructor(
+    public allCategoriesGQL: AllCategoriesGQL,
+    public allStagesGQL: AllStagesGQL,
+    public allOrganisationsGQL: AllOrganisationsGQL,
+    public allPagesGQL: AllPagesGQL,
+    public allItemsByCategoryGQL: AllItemsByCategoryGQL,
+    public allItemsByStageGQL: AllItemsByStageGQL,
+    public allItemsByOrganisationGQL: AllItemsByOrganisationGQL,
+  ) { }
 
   setFilterButtonClicked() {
     this.filterButtonClickChange.next('filter');
@@ -60,5 +80,61 @@ export class SearchBarService {
       searchText: this.searchText,
       category: this.category
     } as SearchBarParams;
+  }
+
+   // Get all research stages
+   public getAllStages(): Observable<StageCollection> {
+    try {
+      return this.allStagesGQL.fetch()
+        .pipe(pluck('data', 'stageCollection')) as Observable<StageCollection>
+    } catch (e) { console.error('Error loading all stages:', e) };
+  }
+
+  // Get all research categories
+  public getAllCategories(): Observable<CategoryCollection> {
+    try {
+      return this.allCategoriesGQL.fetch()
+        .pipe(pluck('data', 'categoryCollection')) as Observable<CategoryCollection>
+    } catch (e) { console.error('Error loading all Categories:', e) };
+  }
+
+  // Get all organisations
+  public getAllOrganisations(): Observable<OrgUnitCollection> {
+    try {
+      return this.allOrganisationsGQL.fetch()
+        .pipe(pluck('data', 'orgUnitCollection')) as Observable<OrgUnitCollection>
+    } catch (e) { console.error('Error loading all organisations:', e) };
+  }
+
+  // Get All Pages
+  public getAllPages() {
+    try {
+      return this.allPagesGQL.fetch()
+        .pipe(pluck('data'));
+    } catch (e) { console.error('Error loading all pages:', e) };
+  }
+
+  // Get All Pages by Category
+  public getAllItemsByCategory(filter) {
+    try {
+      return this.allItemsByCategoryGQL.fetch({ displayOrder: filter })
+        .pipe(pluck('data'));
+    } catch (e) { console.error('Error loading all pages:', e) };
+  }
+
+  // Get All Pages by Stage
+  public getAllItemsByStage(filter) {
+    try {
+      return this.allItemsByStageGQL.fetch({ displayOrder: filter })
+        .pipe(pluck('data'));
+    } catch (e) { console.error('Error loading all pages:', e) };
+  }
+
+  // Get All Pages by Organisation
+  public getAllItemsByOrganisation(filter) {
+    try {
+      return this.allItemsByOrganisationGQL.fetch({ displayOrder: filter })
+        .pipe(pluck('data'));
+    } catch (e) { console.error('Error loading all pages:', e) };
   }
 }
