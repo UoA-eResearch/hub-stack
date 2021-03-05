5060f254b045cf1b836300143b8beb52dfc3dce0
Rose McColl <rosemccoll@hotmail.com>
2021-03-05 16:06:25 +1300

    fixing test

--- hub-search-proxy/test/main.js
+++ hub-search-proxy/test/main.js
@@ -1,7 +1,7 @@
 "use strict";
 const mochaPlugin = require("serverless-mocha-plugin");
 const expect = mochaPlugin.chai.expect;
-let wrapped = mochaPlugin.getWrapper("main", "/handler.js", "main");
+let wrapped = mochaPlugin.getWrapper("main", "/handler.js", "search");
 const fetch = require('node-fetch');
 const aws = require('aws-sdk');
 const aws4 = require('aws4');
@@ -11,18 +11,19 @@ let getResBody = async (req) =>
   await wrapped.run(req).then(res => JSON.parse(res.body));
 
 describe("hub-search-proxy", () => {
-  it("POST request returns a response", async function () {
+  it("POST search request returns a response", async function () {
     this.timeout(TIMEOUT_PERIOD);
+    const query = "Tis better to have tested and lost than never to have tested at all";
     const resBody = await getResBody({
       httpMethod: "POST",
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        query: "Tis better to have tested and lost than never to have tested at all"
+        query: query
       })
     });
     console.log(resBody)
-    expect(resBody.message).to.contain('hub-search-proxy');
+    expect(resBody.query).to.contain(query);
   });
 });
