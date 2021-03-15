70f9f7de3f76fb6cb9e0729041daf086af65a2d8
JuliusKarl <julius.karl.macrohon@gmail.com>
2021-03-15 17:14:48 +1300

    passing tests

--- research-hub-web/src/app/components/search-page/search-page.component.spec.ts
+++ research-hub-web/src/app/components/search-page/search-page.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ApolloTestingModule } from 'apollo-angular/testing';
 import { SearchBarService } from '../search-bar/search-bar.service';
 import { SearchPageComponent } from './search-page.component';
+import { AppComponentService } from '../../app.component.service';
 
 describe('SearchPageComponent', () => {
   let component: SearchPageComponent;
@@ -16,7 +17,8 @@ describe('SearchPageComponent', () => {
         HttpClientModule],
       providers: [
         SearchBarService,
-        Location
+        Location,
+        AppComponentService
       ]
     })
     .compileComponents();
