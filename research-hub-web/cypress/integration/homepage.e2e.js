14fcb87d8f8faf75606d6c5d14cc413c09dbceab
JuliusKarl <julius.karl.macrohon@gmail.com>
2021-03-16 13:10:05 +1300

    writing homepage tests

--- research-hub-web/cypress/integration/homepage.e2e.js
+++ research-hub-web/cypress/integration/homepage.e2e.js
@@ -12,23 +12,33 @@ describe('ResearchHubs Homepage', () => {
         cy.contains('ResearchHub');
     });
 
-    it('Displays featured items', () => {
+    it('displays featured items', () => {
         cy.contains(`The thing you learn about learning is that there's always more to learn! Here are the highlights of what we are all learning together in the research community at the University of Auckland`);
     })
 
-    it('Displays research categories', () => {
+    it('displays research categories', () => {
         cy.contains(`The University of Auckland provides top-quality support to our research community. The ResearchHub is your gateway to research support at the University of Auckland. Here you can explore what's on offer by topic.`);
     })
 
-    it('Displays research activities', () => {
+    it('displays research activities', () => {
         cy.contains(`The research lifecycle describes the research journey from project inception to completion. It highlights five project stages. Below you can explore what the University of Auckland provides to support you according to where you are in your research journey.`);
     })
 
-    it('Displays research activities', () => {
+    it('displays research activities', () => {
         cy.contains(`We are developing the ResearchHub further and would be grateful for your feedback. This will give us pointers for user-led refinement and further development of the interface content displayed in the ResearchHub.`);
     })
 
-    it('Displays footer', () => {
+    it('displays footer', () => {
         cy.contains(`Copyright © 2021 Centre for eResearch`);
+    })
+
+    it('clicking Sign In takes you to SSO', () => {
+        cy.get('#top-bar').contains('Sign In').click();
+        cy.contains('Or sign in with one of the following services');
+    })
+
+    it('clicking Sign In takes you to SSO', () => {
+        cy.get('#featured').contains('Research Impact').click();
+        cy.contains('Impact is the contribution that research and creative practice make to society, the environment and the economy, and the benefits to individuals, whānau, communities, organisations, New Zealand and the world');
     })
 });
