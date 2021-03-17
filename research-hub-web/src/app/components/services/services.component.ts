f762cf3803071a962f1c5651ac7fb495fa145083
JuliusKarl <julius.karl.macrohon@gmail.com>
2021-03-18 10:23:34 +1300

    banners and wcag

--- research-hub-web/src/app/components/services/services.component.ts
+++ research-hub-web/src/app/components/services/services.component.ts
@@ -14,6 +14,7 @@ import { CerGraphqlService } from '@services/cer-graphql.service';
 import { BLOCKS, INLINES } from '@contentful/rich-text-types';
 import { NodeRenderer } from 'ngx-contentful-rich-text';
 import { BodyMediaComponent } from '@components/shared/body-media/body-media.component';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-services',
@@ -37,6 +38,7 @@ export class ServicesComponent implements OnInit, OnDestroy {
   public bodyLinks$: Subscription;
   public allServices$: Observable<ServiceCollection>;
   public parentSubHubs;
+  public isMobile: Boolean;
 
   constructor(
     public route: ActivatedRoute,
@@ -45,8 +47,14 @@ export class ServicesComponent implements OnInit, OnDestroy {
     public cerGraphQLService: CerGraphqlService,
     public appComponentService: AppComponentService,
     public bodyMediaService: BodyMediaService,
-    public router: Router
-  ) { }
+    public router: Router,
+    private deviceService: DeviceDetectorService
+  ) { this.detectDevice(); }
+
+  // Detect if device is Mobile
+  detectDevice() {
+    this.isMobile = this.deviceService.isMobile();
+  }
 
   async ngOnInit() {
     /**
