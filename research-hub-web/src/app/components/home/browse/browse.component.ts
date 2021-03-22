8f4850c342bbcb0751e5092459ef652eb4aed3db
JuliusKarl <julius.karl.macrohon@gmail.com>
2021-03-23 08:42:28 +1300

    getting homepage data from contentful

--- research-hub-web/src/app/components/home/browse/browse.component.ts
+++ research-hub-web/src/app/components/home/browse/browse.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AllCategoriesGQL, CategoryCollection, Category } from '@app/graphql/schema';
-import { pluck } from 'rxjs/operators';
+import { AllCategoriesGQL, CategoryCollection, Category, GetHomepageGQL, Homepage } from '@app/graphql/schema';
+import { flatMap, pluck } from 'rxjs/operators';
 import { SearchBarService } from '@app/components/search-bar/search-bar.service';
 
 @Component({
@@ -11,16 +11,20 @@ import { SearchBarService } from '@app/components/search-bar/search-bar.service'
 })
 export class BrowseComponent implements OnInit {
   public title = 'Research Categories';
-  public description = "The University of Auckland provides top-quality support to our research community. The ResearchHub is your gateway to research support at the University of Auckland. Here you can explore what's on offer by topic.";
+  public description = "";
   public allCategories$: Observable<CategoryCollection>;
   public events;
 
   constructor(
     public allCategoriesGQL: AllCategoriesGQL,
-    public searchBarService: SearchBarService) {}
+    public searchBarService: SearchBarService,
+    public getHomepageGQL: GetHomepageGQL) {}
 
   async ngOnInit() {
     this.allCategories$ = this.getAllCategories();
+    this.getHomepage().subscribe(data => {
+      this.description = data.researchCategories;
+    })
   }
 
   public getAllCategories(): Observable<CategoryCollection> {
@@ -28,6 +32,14 @@ export class BrowseComponent implements OnInit {
       return this.allCategoriesGQL.fetch()
         .pipe(pluck('data', 'categoryCollection')) as Observable<CategoryCollection>
     } catch (e) { console.error('Error loading all Categories:', e) };
+  }
+
+  // Get homepage data
+  public getHomepage(): Observable<Homepage> {
+    try {
+      return this.getHomepageGQL.fetch()
+        .pipe(flatMap(x => x.data.homepageCollection.items)) as Observable<Homepage>
+    } catch (e) { console.error('Error loading homepage:', e) };
   }
 
   getSearchQueryParams(item: any) {
