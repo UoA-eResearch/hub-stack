9cdbb0090840f1f10d50088a9d26c25f794c2d92
Rose McColl <rosemccoll@hotmail.com>
2021-04-15 15:57:51 +1200

    updates for dev & test in nonprod

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   required_version = ">= 0.13.3"
   backend "s3" {
-    key     = "researchhub/terraform.tfstate"
+    key     = "researchhub/${var.lifecycle_state}/terraform.tfstate"
   }
 }
 
