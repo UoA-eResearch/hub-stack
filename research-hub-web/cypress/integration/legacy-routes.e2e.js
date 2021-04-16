3537117205b714f59240a9faaeb0d2736717a2b5
Noel <noel.zeng@auckland.ac.nz>
2021-04-16 17:15:20 +1200

    Add tests for legacy routing

--- research-hub-web/cypress/integration/legacy-routes.e2e.js
+++
@@ -1,7 +0,0 @@
-describe("Legacy route redirects", () => {
-
-    it('can visit an old-style content route and be redirected to right page', () => {
-        cy.visit('/#/content/1');
-        cy.contains("Research Virtual Machines");
-    });
-});
