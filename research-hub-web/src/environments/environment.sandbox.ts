efa15d6a726bc91333fbfcfdd08a97cc5157f959
Rose McColl <rosemccoll@hotmail.com>
2021-04-18 10:30:30 +1200

    search proxy env var

--- research-hub-web/src/environments/environment.sandbox.ts
+++ research-hub-web/src/environments/environment.sandbox.ts
@@ -12,7 +12,7 @@ export const environment = {
     codeChallengeMethod: 'S256',
     logout_uri: 'https://research-hub.sandbox.amazon.auckland.ac.nz'
   },
-  searchUrl: 'https://apigw.test.amazon.auckland.ac.nz/hub-search-proxy',
+  searchUrl: 'https://apigw.test.amazon.auckland.ac.nz/hub-search-proxy-dev',
   privateUrlKeyWords: {
     get whoNeedBearerToken() {
       return [
