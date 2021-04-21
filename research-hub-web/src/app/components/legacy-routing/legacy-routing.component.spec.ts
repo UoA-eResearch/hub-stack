2f1c99230cc965d64dab5e127065421e0c5d925e
Noel <noel.zeng@auckland.ac.nz>
2021-04-21 13:15:40 +1200

    Make unit tests run

--- research-hub-web/src/app/components/legacy-routing/legacy-routing.component.spec.ts
+++ research-hub-web/src/app/components/legacy-routing/legacy-routing.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from "@angular/router/testing";
 import { LegacyRoutingComponent } from './legacy-routing.component';
 
 describe('LegacyRoutingComponent', () => {
   let component: LegacyRoutingComponent;
   let fixture: ComponentFixture<LegacyRoutingComponent>;
-
+  // let controller: ApolloTestingController;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LegacyRoutingComponent ]
+      declarations: [ LegacyRoutingComponent ],
+      imports: [
+        RouterTestingModule.withRoutes([
+          // Set up mock routes to make test pass.
+          {
+            path: "error/:errorCode",
+            redirectTo: "",
+          }
+        ]),
+        // ApolloTestingModule
+      ]
     })
     .compileComponents();
   });
