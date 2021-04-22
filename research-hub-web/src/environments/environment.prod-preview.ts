c9a3c936afe06f1aaaa399f6fde800add259df38
Rose McColl <rosemccoll@hotmail.com>
2021-04-22 17:16:08 +1200

    adding new infra variables

--- research-hub-web/src/environments/environment.prod-preview.ts
+++ research-hub-web/src/environments/environment.prod-preview.ts
@@ -1,8 +1,8 @@
 import { environment } from "./environment.prod";
 
 environment.cerGraphQLUrl = environment.cerGraphQLPreviewUrl;
-environment.auth.redirectUri = "https://research-hub-preview.auckland.ac.nz";
-environment.auth.logout_uri = "https://research-hub-preview.auckland.ac.nz";
+environment.auth.redirectUri = "https://research-hub-preview.connect.amazon.auckland.ac.nz";
+environment.auth.logout_uri = "https://research-hub-preview.connect.amazon.auckland.ac.nz";
 
 environment.privateUrlKeyWords = {
   get whoNeedBearerToken() {
--- research-hub-web/src/environments/environment.prod.ts
+++ research-hub-web/src/environments/environment.prod.ts
@@ -2,17 +2,17 @@
 // Also: Insert values in environment.prod-preview.ts
 export const environment = {
   production: true,
-  cerGraphQLUrl: 'https://rhubcpapi.auckland.ac.nz/cer-graphql-service',
-  cerGraphQLPreviewUrl: 'https://rhubcpapi.auckland.ac.nz/cer-graphql-preview-service/',
+  cerGraphQLUrl: 'https://rhubcpapi.connect.amazon.auckland.ac.nz/cer-graphql-service',
+  cerGraphQLPreviewUrl: 'https://rhubcpapi.connect.amazon.auckland.ac.nz/cer-graphql-preview-service/',
   auth: {
     cognitoAwsRegion: 'ap-southeast-2',
     cognitoUserPoolId: 'ap-southeast-2_B3Lx9B4bL',
     cognitoDomain: 'uoapool',
-    cognitoClientId: 'TBC',
-    redirectUri: 'https://research-hub.auckland.ac.nz',
+    cognitoClientId: '7o2oq232s4qjk93eagnj9c63e0',
+    redirectUri: 'https://research-hub.connect.amazon.auckland.ac.nz',
     scopes: 'openid profile https://research-hub-prod.auckland.ac.nz/spa',
     codeChallengeMethod: 'S256',
-    logout_uri: 'https://research-hub.auckland.ac.nz'
+    logout_uri: 'https://research-hub.connect.amazon.auckland.ac.nz'
   },
   searchUrl: 'https://apigw.prod.amazon.auckland.ac.nz/hub-search-proxy-prod',
   privateUrlKeyWords: {
