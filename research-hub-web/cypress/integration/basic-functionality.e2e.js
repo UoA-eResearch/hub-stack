c78e6dd8eb372cc4fbf5abb8e540f8243e9d4826
Robert Carter <r.carter@auckland.ac.nz>
2021-05-11 11:02:34 +1200

    test: page loads and has title

--- research-hub-web/cypress/integration/basic-functionality.e2e.js
+++ research-hub-web/cypress/integration/basic-functionality.e2e.js
@@ -8,8 +8,19 @@ describe('ResearchHubs Basic Functionality', () => {
         cy.visit('/');
     });
 
-    it('has title', () => {
-        cy.contains('ResearchHub');
+    it('page loads and has title', () => {
+        cy.expect('title').not.to.be.empty;
     });
+    
+    // it('an image asset is displayed', () => {
+    //     cy.get('[alt="delayed image"]')
+    //     .should('be.visible')
+    //     .and(($img) => {
+    //       // "naturalWidth" and "naturalHeight" are set when the image loads
+    //       expect($img[0].naturalWidth).to.be.greaterThan(0)
+    //     })
+    // }
+
+
 });
 
