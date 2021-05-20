0b0a514c63c0fdc7946d9406a63417318cd3d594
Rose McColl <rosemccoll@hotmail.com>
2021-05-20 14:41:35 +1200

    create cf firewall for restricting ip addresses in dev and test

---
+++ terraform/firewall-cloudfront.tf
@@ -0,0 +1,44 @@
+resource "aws_waf_ipset" "ipset" {
+  name = "cloudfront_ipset"
+
+  # the list of IPs we want to whitelist = UoA IP range only
+  ip_set_descriptors {
+    type  = "IPV4"
+    value = "130.216.0.0/16"
+  }
+}
+
+resource "aws_waf_rule" "wafrule" {
+  depends_on  = [aws_waf_ipset.ipset]
+  name        = "cloudfront_waf_rule"
+  metric_name = "cloudfrontwafrule"
+
+  predicates {
+    data_id = aws_waf_ipset.ipset.id
+    negated = false
+    type    = "IPMatch"
+  }
+}
+
+resource "aws_waf_web_acl" "waf_acl" {
+  depends_on  = [
+    aws_waf_ipset.ipset,
+    aws_waf_rule.wafrule,
+  ]
+  name        = "cloudfront_waf_acl"
+  metric_name = "cloudfrontwafacl"
+
+  default_action {
+    type = "BLOCK"
+  }
+
+  rules {
+    action {
+      type = "ALLOW"
+    }
+    
+    priority = 1
+    rule_id  = aws_waf_rule.wafrule.id
+    type     = "REGULAR"
+  }
+}
