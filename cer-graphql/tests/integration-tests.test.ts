cde7070f5d957c15b984ecb512c5918904ef9da2
Noel <noel.zeng@auckland.ac.nz>
2021-05-27 20:03:01 +1200

    Fix tests for typescript

--- cer-graphql/tests/integration-tests.test.ts
+++ cer-graphql/tests/integration-tests.test.ts
@@ -1,11 +1,10 @@
-const { createTestClient } = require('apollo-server-testing');
-const { createServer, getCredentials } = require('../index')
-const TQ = require('./test-queries'); // Collection of test queries
-const { gql, introspectSchema, ApolloServer } = require('apollo-server');
-const { JsonWebTokenError } = require('jsonwebtoken');
-const aws = require('aws-sdk');
+import { createServer, getCredentials } from "../server";
+import * as TQ from './test-queries'; // Collection of test queries
+import aws from 'aws-sdk';
+import fetch from "node-fetch";
+import { AddressInfo } from "net";
+// This package does not have type declarations.
 const aws4 = require('aws4');
-const fetch = require('node-fetch');
 
 const TIMEOUT_PERIOD = 40000;
 let awsProfile = 'saml';
@@ -15,10 +14,26 @@ let awsProfile = 'saml';
  * used to make queries against it.
  */
 async function createServerAndTestClient() {
-    let server = await createServer(getCredentials(true));
-    let testServer = createTestClient(new ApolloServer({ ...server, context: () => { } }));
-    return testServer;
-}
+    let server = (await createServer(getCredentials(true))).listen();
+    const addrInfo = server.address() as AddressInfo;
+    const addr = `http://localhost:${addrInfo.port}`;
+    return {
+        query: (q: object) => (
+            fetch(addr, {
+                method: "POST",
+                body: JSON.stringify(q),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).then(res => {
+                return res.json();
+            })
+        ),
+        close: () => {
+            server.close();
+        }
+    }
+};
 
 /**
  * Creates as testing server using the real server's configuration 
@@ -26,31 +41,38 @@ async function createServerAndTestClient() {
  * If passed false, will create a query with an valid authorization header.
  */
 async function createServerAndTestClientWithAuth(useValidToken = true) {
-    let server = await createServer(getCredentials(true));
+    let server = (await createServer(getCredentials(true))).listen();
+    const addrInfo = server.address() as AddressInfo;
+    const addr = `http://localhost:${addrInfo.port}`;
     let tokens = await getTokens();
     if (!useValidToken) {
         tokens['access_token'] = 'Bearer fake token value';
     }
 
-    // creating a new apollo server with authorization baked into the requests
-    let authorizedServer = createTestClient(new ApolloServer({
-        ...server.config,
-        context: () => server.config.context({
-            req: {
+    return {
+        query: (q: object) => (
+            fetch(addr, {
+                method: "POST",
+                body: JSON.stringify(q),
                 headers: {
-                    authorization: `Bearer ${tokens['access_token']}`
+                    authorization: `Bearer ${tokens["access_token"]}`,
+                    "Content-Type": "application/json"
                 }
-            }
-        })
-    }));
-    return authorizedServer;
+            }).then(res => {
+                return res.json();
+            })
+        ),
+        close: () => {
+            server.close();
+        }
+    };
 }
 
 /**
  * Gets OAuth tokens from 2FAB Lambda
  */
 const getTokens = async () => {
-    let deployStage = process.env.stage.trim();
+    let deployStage = process.env.stage?.trim();
     console.log('Getting AWS credentials for profile ' + awsProfile)
     let awsCreds = new aws.SharedIniFileCredentials({
         profile: awsProfile
@@ -91,14 +113,14 @@ const getTokens = async () => {
         'Accept': '*/*',
         'Accept-Encoding': 'gzip, deflate, br'
     };
-    aws4.sign(opts, {
+    const fetchOpts = aws4.sign(opts, {
         accessKeyId: awsCreds.accessKeyId,
         secretAccessKey: awsCreds.secretAccessKey,
         sessionToken: awsCreds.sessionToken
     });
 
     // Making request to 2FAB Lambda using 
-    let res = await fetch(`https://${opts.host}${opts.path}`, opts);
+    let res = await fetch(`https://${opts.host}${opts.path}`, fetchOpts);
     const resJson = await res.json();
     console.warn({resJson});
     try {
@@ -112,101 +134,112 @@ const getTokens = async () => {
     return resJson;
 }
 
-/**
- * Before any of the tests run, set the aws-profile, and create the query function and make
- * it available within all tests.
- */
-beforeAll(async () => {
-    // set the aws profile based on the argument passed in
-    process.argv.forEach(arg => {
-        if (arg.indexOf('--aws-profile') > -1) {
-            awsProfile = arg.split('=')[1];
-        }
-    });
-
-    console.log('AWS Profile set to ' + awsProfile);
-
-    try {
-        return { query } = await createServerAndTestClient();
-    } catch (error) {
-        fail("An error occurred when trying to setup the server. Have you filled in credentials in the .env file?");
-    }
-});
-
-describe('Basic collection queries', () => {
-
-    test('Querying the articleCollection searchable fields returns them', async function () {
-        let res = await query({ query: TQ.GET_ARTICLE_COLLECTION });
-
-        // Get the fields returned from the first item
-        let returned_fields = Object.keys(res.data.articleCollection.items[0]);
-        expect(returned_fields).toEqual(TQ.SEARCHABLE_FIELDS)
-    });
-
-    test('Querying the equipment collection returns an item with a title', async function () {
-        let res = await query({ query: TQ.GET_EQUIPMENT_COLLECTION });
-        expect(res.data.equipmentCollection.items[0].title.length).toBeGreaterThan(0)
-    });
-});
-
-describe('Basic single resource queries', () => {
-
-    test('Can return an individual article by its sys id', async function () {
-        let res = await query({
-            query: TQ.GET_ARTICLE_BY_SYS_ID,
-            variables: { id: '7y4NmxQyIKrsSVmqSew0KJ' }
+describe("Tests for cer-graphql", () => {
+    /**
+     * Before any of the tests run, set the aws-profile, and create the query function and make
+     * it available within all tests.
+     */
+    let query : Function;
+    let closeServer: Function;
+    beforeAll(async () => {
+        // set the aws profile based on the argument passed in
+        process.argv.forEach(arg => {
+            if (arg.indexOf('--aws-profile') > -1) {
+                awsProfile = arg.split('=')[1];
+            }
         });
 
-        expect(res.data.article.title).toEqual('Large Scale Visualisation Facility')
-    });
+        console.log('AWS Profile set to ' + awsProfile);
 
-});
-
-describe('Contentful filters (conditionals)', () => {
-
-    test('Can return an article from the articleCollection with a where clause', async function () {
-        const ARTICLE_TITLE = 'Large Scale Visualisation Facility';
-
-        let res = await query({
-            query: TQ.GET_ARTICLE_BY_WHERE,
-            variables: { title: ARTICLE_TITLE }
-        });
-
-        expect(res.data.articleCollection.items[0].title).toEqual(ARTICLE_TITLE)
-    });
-
-});
-
-describe('Authorization resolvers', () => {
-
-    test('Requesting an articleCollection private field with an invalid Authorization header fails', async function () {
-        let { query } = await createServerAndTestClientWithAuth(false);
-        let message = false;
         try {
-            await query({ query: TQ.GET_ARTICLE_COLLECTION_PRIVATE_WITH_SSO });
-        } catch (err) {
-            message = err.message;
+            const client = await createServerAndTestClient();
+            query = client.query;
+            closeServer = client.close;
+        } catch (error) {
+            fail("An error occurred when trying to setup the server. Have you filled in credentials in the .env file?");
         }
-        expect(message).toBeTruthy();
-    }, TIMEOUT_PERIOD);
-
-    test('Requesting an articleCollection private field w/o a header returns an error', async function () {
-        let res = await query({ query: TQ.GET_ARTICLE_COLLECTION_PRIVATE });
-        expect(res.errors[0].extensions.code).toEqual('UNAUTHENTICATED');
     });
 
-    test('Requesting an articleCollection private field with a valid Authorization header returns data', async function () {
-        let { query } = await createServerAndTestClientWithAuth();
-        let res = await query({ query: TQ.GET_ARTICLE_COLLECTION_PRIVATE_WITH_SSO });
-        expect(res.data.articleCollection).toBeTruthy();
-    }, TIMEOUT_PERIOD);
+    afterAll(() => {
+        // End the server being tested.
+        closeServer();
+    });
 
-    test('Requesting a article single resource private field returns an error', async function () {
-        let res = await query({
-            query: TQ.GET_ARTICLE_BY_SYS_ID_PRIVATE,
-            variables: { id: 'fRd5opeuTFTvdS12aPjI2' }
+    describe('Basic collection queries', () => {
+
+        test('Querying the articleCollection searchable fields returns them', async function () {
+            let res = await query({ query: TQ.GET_ARTICLE_COLLECTION });
+            // Get the fields returned from the first item
+            let returned_fields = Object.keys(res.data.articleCollection.items[0]);
+            expect(returned_fields).toEqual(TQ.SEARCHABLE_FIELDS)
         });
-        expect(res.errors[0].extensions.code).toEqual('UNAUTHENTICATED');
+
+        test('Querying the equipment collection returns an item with a title', async function () {
+            let res = await query({ query: TQ.GET_EQUIPMENT_COLLECTION });
+            expect(res.data.equipmentCollection.items[0].title.length).toBeGreaterThan(0)
+        });
     });
 
+    describe('Basic single resource queries', () => {
+
+        test('Can return an individual article by its sys id', async function () {
+            let res = await query({
+                query: TQ.GET_ARTICLE_BY_SYS_ID,
+                variables: { id: '7y4NmxQyIKrsSVmqSew0KJ' }
+            });
+
+            expect(res.data.article.title).toEqual('Large Scale Visualisation Facility')
+        });
+
+    });
+
+    describe('Contentful filters (conditionals)', () => {
+
+        test('Can return an article from the articleCollection with a where clause', async function () {
+            const ARTICLE_TITLE = 'Large Scale Visualisation Facility';
+
+            let res = await query({
+                query: TQ.GET_ARTICLE_BY_WHERE,
+                variables: { title: ARTICLE_TITLE }
+            });
+
+            expect(res.data.articleCollection.items[0].title).toEqual(ARTICLE_TITLE)
+        });
+
+    });
+
+    describe('Authorization resolvers', () => {
+
+        test('Requesting an articleCollection private field with an invalid Authorization header fails', async function () {
+            let { query, close } = await createServerAndTestClientWithAuth(false);
+            let message = false;
+            let res = await query({ query: TQ.GET_ARTICLE_COLLECTION_PRIVATE_WITH_SSO });
+            message = res.errors[0];
+            expect(message).toBeTruthy();
+            // stop the server
+            close();
+        }, TIMEOUT_PERIOD);
+
+        test('Requesting an articleCollection private field w/o a header returns an error', async function () {
+            let res = await query({ query: TQ.GET_ARTICLE_COLLECTION_PRIVATE });
+            expect(res.errors[0].extensions.code).toEqual('UNAUTHENTICATED');
+        });
+
+        test('Requesting an articleCollection private field with a valid Authorization header returns data', async function () {
+            let { query, close } = await createServerAndTestClientWithAuth();
+            let res = await query({ query: TQ.GET_ARTICLE_COLLECTION_PRIVATE_WITH_SSO });
+            expect((res as any).data.articleCollection).toBeTruthy();
+            // stop the server
+            close();
+        }, TIMEOUT_PERIOD);
+
+        test('Requesting a article single resource private field returns an error', async function () {
+            let res = await query({
+                query: TQ.GET_ARTICLE_BY_SYS_ID_PRIVATE,
+                variables: { id: 'fRd5opeuTFTvdS12aPjI2' }
+            });
+            expect(res.errors[0].extensions.code).toEqual('UNAUTHENTICATED');
+        });
+
+    });
 });
