33839a7a548f137dd89c6c511411662a6cb97d96
Rose McColl <rosemccoll@hotmail.com>
2021-06-02 13:42:08 +1200

    funding page fixes and case study references

--- research-hub-web/src/app/components/case-study/case-study.module.ts
+++ research-hub-web/src/app/components/case-study/case-study.module.ts
@@ -4,11 +4,13 @@ import { CaseStudyRoutingModule } from './case-study-routing.module';
 import { CaseStudyComponent } from './case-study.component';
 import { SharedModule } from '@components/shared/app.shared.module';
 import { NgxContentfulRichTextModule } from 'ngx-contentful-rich-text';
+import { CaseStudyReferencesComponent } from './case-study-references/case-study-references.component';
 
 
 @NgModule({
   declarations: [
-    CaseStudyComponent
+    CaseStudyComponent,
+    CaseStudyReferencesComponent
   ],
   imports: [
     CommonModule,
