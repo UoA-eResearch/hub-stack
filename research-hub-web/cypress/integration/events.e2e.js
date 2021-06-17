92528388fb3361935d6418aa5bda3f41e9c29af5
Robert Carter <r.carter@auckland.ac.nz>
2021-06-17 15:49:00 +1200

    events tests

--- research-hub-web/cypress/integration/events.e2e.js
+++ research-hub-web/cypress/integration/events.e2e.js
@@ -5,15 +5,15 @@ describe('ResearchHubs Events Pages', () => {
     });
 
     it('can visit an event and display its title', () => {
-        cy.contains('Orientation and Welcome Events');
+        cy.expect('h1.content-title').not.to.be.empty;
     });
 
     it('can visit an event and display its subtitle', () => {
-        cy.contains('The Orientation and Welcome Events curriculum is a package of events that are designed to help welcome new staff to the University');
+        cy.expect('.content-summary').not.to.be.empty;
     });
 
     it('event displays body text', () => {
-        cy.contains('The Orientation and Welcome Events curriculum is a package of events that are designed to help welcome new staff to the University.');
+        cy.expect('#event-container p .ng-star-inserted').not.to.be.empty;
     });
 
     it('event displays specifications table', () => {
@@ -21,38 +21,12 @@ describe('ResearchHubs Events Pages', () => {
         cy.get('#specifications-table').contains('Description').should('exist');
     });
 
-    // it('displays a list of related items', () => {
-    //     cy.get('#you-might-be-interested-in').should('exist');
-    // });
-
-    // it('clicking a related item takes you to its page', () => {
-    //     cy.get('#you-might-be-interested-in').contains('Data Carpentry').click();
-    //     cy.contains('Data Carpentry workshops teach core skills for working with data effectively and reproducibly');
-    // });
-
     it('displays a list of contacts', () => {
         cy.get('#contacts').contains('Organisational Development (OD)').should('exist');
     });
 
-    // it('displays a list of documents', () => {
-    //     cy.get('#documents').contains('Code of Conduct').should('exist');
-    // });
-
-    // Cypress doesn't dupport multi-tab testing
-    // 
-    // it('clicking on a documents takes you to the documents', () => {
-    //     cy.get('#documents').contains('Health Research Council – Research Impact Guidance').click();
-    //     cy.contains('The Impact of Research');
-    // });
-
     it('displays a list of organisations', () => {
-        cy.get('#organisations').contains('Organisational Development (OD)').should('exist');
+        cy.expect('#organisations mat-card:first-child h4').not.to.be.empty;
     });
 
-    // Cypress doesn't dupport multi-tab testing
-    // 
-    // it('clicking on an organisation takes you to the organisation', () => {
-    //     cy.get('#organisations').contains('Office of Research Strategy and Integrity').click();
-    //     cy.contains("The Office of Research Strategy and Integrity (ORSI) was established in 2018 to support the work of the Deputy Vice Chancellor Research and the University's Research Committee in: ");
-    // });
 });
