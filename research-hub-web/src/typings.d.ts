2a1c47bc0e954d65dc0a852f37592c129ab53211
Luke <lukas.trombach@auckland.ac.nz>
2021-06-24 12:29:14 +1200

    fixed build

--- research-hub-web/src/typings.d.ts
+++ research-hub-web/src/typings.d.ts
@@ -1,7 +1,9 @@
 /* SystemJS module definition */
-declare let module: NodeModule;
+// eslint-disable-next-line no-var
+declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 interface Window { dataLayer: any; }
