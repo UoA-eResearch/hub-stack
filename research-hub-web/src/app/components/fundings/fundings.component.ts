ca08f13a727fc6ee46ce1df663d8c42403b5acc4
Rose McColl <rosemccoll@hotmail.com>
2021-07-05 15:34:39 +1200

    add more image widths

--- research-hub-web/src/app/components/fundings/fundings.component.ts
+++ research-hub-web/src/app/components/fundings/fundings.component.ts
@@ -114,7 +114,7 @@ export class FundingsComponent implements OnInit, OnDestroy {
         
         // Set banner image URL for webp format if webp is supported
         if (data.banner?.url) {
-          this.bannerImageUrl = this.supportsWebp ? data.banner?.url + '?fm=webp' : data.banner?.url;
+          this.bannerImageUrl = this.supportsWebp ? data.banner?.url + '?w=1900&fm=webp' : data.banner?.url + '?w=1900';
         }
 
         this.bodyMediaService.setBodyMedia(data.bodyText?.links);
