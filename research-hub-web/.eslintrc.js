fa0283f59373dfe97b8542e67754b87470af3454
Luke <lukas.trombach@auckland.ac.nz>
2021-07-09 12:37:33 +1200

    converted eslint config to js module and fixed vscode error

--- research-hub-web/.eslintrc.js
+++ research-hub-web/.eslintrc.js
@@ -1,53 +1,54 @@
-{
-  "root": true,
-  "ignorePatterns": [
+module.exports = {
+  root: true,
+  ignorePatterns: [
     "projects/**/*",
     "cypress/**/*",
     "**/*.spec.ts"
   ],
-  "overrides": [
+  overrides: [
     {
-      "files": [
+      files: [
         "*.ts"
       ],
-      "parserOptions": {
-        "project": [
+      parserOptions: {
+        project: [
           "tsconfig.json",
           "tsconfig.e2e.json"
         ],
-        "createDefaultProgram": true
+        createDefaultProgram: true,
+        tsconfigRootDir: __dirname // this line fixes linting in vscode as it tries to load tsconfig.json from the root folder of the mono repo
       },
-      "extends": [
+      extends: [
         "plugin:@angular-eslint/recommended",
         "plugin:@angular-eslint/template/process-inline-templates"
       ],
-      "rules": {
+      rules: {
         "@angular-eslint/component-selector": [
           "error",
           {
-            "prefix": "app",
-            "style": "kebab-case",
-            "type": "element"
+            prefix: "app",
+            style: "kebab-case",
+            type: "element"
           }
         ],
         "@angular-eslint/directive-selector": [
           "error",
           {
-            "prefix": "app",
-            "style": "camelCase",
-            "type": "attribute"
+            prefix: "app",
+            style: "camelCase",
+            type: "attribute"
           }
         ]
       }
     },
     {
-      "files": [
+      files: [
         "*.html"
       ],
-      "extends": [
+      extends: [
         "plugin:@angular-eslint/template/recommended"
       ],
-      "rules": {}
+      rules: {}
     }
   ]
 }
