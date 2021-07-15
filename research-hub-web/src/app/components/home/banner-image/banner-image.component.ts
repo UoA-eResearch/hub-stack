542da33ef91bb2f54cecf80420031b3f874a4dfe
Luke <lukas.trombach@auckland.ac.nz>
2021-07-16 11:57:22 +1200

    changed banner image loading

--- research-hub-web/src/app/components/home/banner-image/banner-image.component.ts
+++ research-hub-web/src/app/components/home/banner-image/banner-image.component.ts
@@ -1,36 +1,46 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SearchBarService } from '@app/components/search-bar/search-bar.service';
+import { GetBannerImageGQL } from '@app/graphql/schema';
 import { HomeScrollService } from '@services/home-scroll.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-banner-image',
   templateUrl: './banner-image.component.html',
   styleUrls: ['./banner-image.component.scss']
 })
-export class BannerImageComponent {
-  @Input() url: string;
+export class BannerImageComponent implements OnInit {
+  public bannerImageUrl$: Observable<string>;
 
-  searchText = '';
-  title = "Welcome to the ResearchHub";
-  summary = "The ResearchHub connects you with people, resources, and services from across the University to enhance and accelerate your research.";
+  public searchText = '';
+  public title = "Welcome to the ResearchHub";
+  public summary = "The ResearchHub connects you with people, resources, and services from across the University to enhance and accelerate your research.";
 
 
   constructor(
     public homeScrollService: HomeScrollService,
     public searchBarService: SearchBarService,
-    private router: Router
+    private router: Router,
+    private getBannerImageGQL: GetBannerImageGQL
   ) { }
+
+  ngOnInit(): void {
+    this.bannerImageUrl$ = this.getBannerImageGQL.fetch().pipe(
+      map(x => x.data.homepageCollection.items[0].image.url)
+    )
+  }
 
   // Adding search bar in here for now
   // TODO: refactor search bar
-  search() {
+  search(): void {
     this.searchBarService.setSearchText(this.searchText);
     this.searchBarService.setCurrentPage(1);
     this.router.navigate(['/search']);
   }
 
-  clearSearchText() {
+  clearSearchText(): void {
     this.searchText = '';
     this.searchBarService.setSearchText('');
   }
