0c9304345a4c611169bdb9dcc71e4d5f251bf622
Luke <lukas.trombach@auckland.ac.nz>
2021-07-22 14:12:27 +1200

    removed old code from app.component

--- research-hub-web/src/app/components/layout/navbar/navbar.component.ts
+++ research-hub-web/src/app/components/layout/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import { NavigationEnd, Router, RouterEvent } from '@angular/router';
 import { SearchBarService } from '@app/components/search-bar/search-bar.service';
 import { AllCategoriesGQL, AllStagesGQL, Category, Stage } from '@app/graphql/schema';
 import { HomeScrollService } from '@services/home-scroll.service';
-import { LoginService } from '@uoa/auth';
+import { LoginService, UserInfoDto } from '@uoa/auth';
 import { Observable, Subscription } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 
@@ -17,6 +17,8 @@ export class NavbarComponent implements OnInit, OnDestroy {
   public currentUrl = '/';
   public allCategories: Category[] = [];
   public allStages: Stage[] = [];
+
+  public userInfo: UserInfoDto | null = null;
 
   private subscriptions: Subscription = new Subscription();
 
@@ -41,8 +43,38 @@ export class NavbarComponent implements OnInit, OnDestroy {
       })
     );
 
+
+    this.subscriptions.add(
+      this.loginService.userInfo$.pipe(
+        filter(userInfo => userInfo !== null && userInfo !== undefined)
+      ).subscribe(userInfo => {
+        console.log('userInfo ', userInfo)
+        this.userInfo = userInfo;
+        this.sendGoogleAnalyticsUserInfo(userInfo)
+      })
+    );
+
     this.subscriptions.add(this.getAllCategories().subscribe((allCategories) => this.allCategories = allCategories));
     this.subscriptions.add(this.getAllStages().subscribe((allStages) => this.allStages = allStages));
+  }
+
+  private sendGoogleAnalyticsUserInfo(userInfo: UserInfoDto): void {
+    window.dataLayer.push({
+      'user': JSON.stringify(userInfo),
+      ...userInfo,
+    });
+
+    // groups disabled for now, as there is a bug in the UoA auth library
+    // if (userInfo.groups) {
+    //   console.log(userInfo.groups);
+    //   JSON.parse(`${userInfo.groups}`).map(group => {
+    //     const groupObj = {};
+    //     group = group.trim().split('.')[0];
+    //     groupObj[group] = group;
+    //     console.log(groupObj);
+    //     window.dataLayer.push(groupObj);
+    //   })
+    // }
   }
 
   private getAllCategories(): Observable<Category[]> {
