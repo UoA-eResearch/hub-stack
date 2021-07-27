cc81e57e38d1a70dc9810ab283865e6a263251aa
Luke <lukas.trombach@auckland.ac.nz>
2021-07-27 15:48:40 +1200

    update @uoa/auth to 0.6.0

--- research-hub-web/src/app/services/app-storage.service.ts
+++ research-hub-web/src/app/services/app-storage.service.ts
@@ -4,27 +4,27 @@ import { StorageService } from '@uoa/auth';
 import * as localforage from 'localforage';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class AppStorageService implements StorageService {
 
-    constructor() {
-        /**
-         * Due to a bug in Firefox with the IndexedDB driver (see https://github.com/localForage/localForage/issues/920),
-         * change driver to use localstorage instead.
-         */
-        localforage.setDriver(localforage.LOCALSTORAGE);
-    }
+  constructor() {
+    /**
+     * Due to a bug in Firefox with the IndexedDB driver (see https://github.com/localForage/localForage/issues/920),
+     * change driver to use localstorage instead.
+     */
+    localforage.setDriver(localforage.LOCALSTORAGE);
+  }
 
-    getItem(key: string): Promise<any> {
-        return localforage.getItem(key);
-    }
+  getItem(key: string): Promise<any> {
+    return localforage.getItem(key);
+  }
 
-    setItem(key: string, val: any): void {
-        localforage.setItem(key, val);
-    }
+  setItem(key: string, val: any): Promise<void> {
+    return localforage.setItem(key, val);
+  }
 
-    removeItem(key: string): void {
-        localforage.removeItem(key);
-    }
+  removeItem(key: string): void {
+    localforage.removeItem(key);
+  }
 }
