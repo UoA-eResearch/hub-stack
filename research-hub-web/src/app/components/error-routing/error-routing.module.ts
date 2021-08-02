8a1632bcf34715acea43e2d0730224f7bb66b96a
Rose McColl <rosemccoll@hotmail.com>
2021-08-02 15:40:12 +1200

    error page wording tweaks

--- research-hub-web/src/app/components/error-routing/error-routing.module.ts
+++ research-hub-web/src/app/components/error-routing/error-routing.module.ts
@@ -25,7 +25,7 @@ export class ErrorRoutingModule extends UoaErrorsConfig {
             title: 'Unexpected Error',
             content: `<p>
             Sorry, we seem to have encountered an unexpected error. To report this error or if you require help, please contact  
-            <a href=mailto:${this.contactEmail} target="_blank">${this.contactEmail}</a>
+            <a href=mailto:${this.contactEmail} target="_blank">${this.contactEmail}</a>.
           </p>`
         };
 
@@ -34,30 +34,31 @@ export class ErrorRoutingModule extends UoaErrorsConfig {
             content: `<p>Sorry, You do not have permission to view this page. Please ensure that you are logged in and try again.</p>
             <p>
               If you still do not have access and believe you are seeing this page in error, please contact  
-              <a href=mailto:${this.contactEmail} target="_blank">${this.contactEmail}</a>
+              <a href=mailto:${this.contactEmail} target="_blank">${this.contactEmail}</a>.
             </p>`
         };
 
         this.ErrorPageContent['ErrorCode404'] = {
             title: 'Page Not Found',
-            content: `Sorry, it seems you are trying to access a page that doesn't exist. We are in the process of adding content to this website.
-            In the mean time, please try <a href=search>searching for content</a>, or <a href=mailto:${this.contactEmail} target="_blank">contact us</a> to request the information you seek. 
-            Thank you for your patience. Ngā mihi nui.`
+            content: `<p>Sorry, it seems you are trying to access a page that doesn't exist.</p>
+            <p>We are in the process of adding content to this website.
+            In the mean time, please try using the <a href=search>search</a> tool in the top menu to find content, or contact <a href=mailto:${this.contactEmail} target="_blank">${this.contactEmail}</a> to request the information you seek. 
+            Thank you for your patience. Ngā mihi nui.</p>`
         };     
 
         this.ErrorPageContent['ErrorCode501'] = {
             title: 'Cannot process request',
             content: `<p>Sorry, we cannot connect you to the system. Try reloading the page, or going back and following the link again.</p>
-            <p><a href=mailto:${this.contactEmail} target="_blank">
-            Please contact us if you need help or would like to report this issue</a>.</p>`
+            <p> To report this error or if you require help, please contact  
+            <a href=mailto:${this.contactEmail} target="_blank">${this.contactEmail}</a>.</p>`
         };
 
         this.ErrorPageContent['ErrorCode505'] = {
             title: 'Version Not Supported',
             content: `<p>Sorry, it looks like you are using a browser or connection that is not supported.
             Check your connection settings or use an alternative browser such as Google Chrome, Firefox or Edge.</p>
-            <p><a href=mailto:${this.contactEmail} target="_blank">
-            Please contact us if you need help or would like to report this issue.</a></p>`
+            <p>If you require help, please contact  
+            <a href=mailto:${this.contactEmail} target="_blank">${this.contactEmail}</a>.</p>`
         };
             
         for (let [key, value] of Object.entries(this.ErrorPageContent)) {
