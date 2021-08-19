b0971c889d95f682d24809f4bdeefc45ec146b59
Luke <lukas.trombach@auckland.ac.nz>
2021-08-19 14:25:25 +1200

    added search function

---
+++ research-hub-web/src/app/global/searchTypes.ts
@@ -0,0 +1,26 @@
+interface SearchQuery {
+  query: string,
+  size: number,
+  from: number,
+  sort: SortOrder,
+  filters: SearchFilters,
+  includeContentTypes : ContentType
+}
+
+interface SearchFilters {
+  relatedOrgs: string[],
+  stage: string[],
+  category: string[]
+}
+
+interface SearchResult {
+  title: string;
+  summary: string;
+  slug: string;
+  ssoProtected: boolean;
+  contentType: ContentType;
+  icon: string;
+}
+
+type SortOrder = 'A-Z' | 'Z-A' | 'relevance'
+type ContentType = 'Article' | 'SubHub'
--- research-hub-web/src/app/services/search-service.service.ts
+++ research-hub-web/src/app/services/search-service.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'environments/environment'
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchServiceService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public search(query: SearchQuery): Observable<SearchResult[]> {
+    return this.http.post(
+      environment.searchUrl,
+      query
+    ).pipe(
+      map(data => {
+        const results: SearchResult[] = [];
+        data["result"]["hits"]["hits"].forEach(element => {
+          const result: SearchResult = {
+            title: element._source.fields.title["en-US"],
+            summary: element._source.fields.summary["en-US"],
+            slug: element._source.fields.slug["en-US"],
+            ssoProtected: element._source.fields.ssoProtected["en-US"],
+            contentType : element._source.sys.contentType.sys.id,
+            icon: element._source.fields.icon?.["en-US"]["url"]
+          };
+
+          results.push(result);
+        });
+
+        return results;
+      })
+    );
+  }
 }
