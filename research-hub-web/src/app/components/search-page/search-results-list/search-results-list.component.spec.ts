0c4f37834e05c87eb73b5c9c5b3532bb46fd7cb4
Rose McColl <rosemccoll@hotmail.com>
2021-08-24 17:02:35 +1200

    fix unit tests

--- research-hub-web/src/app/components/search-page/search-results-list/search-results-list.component.spec.ts
+++ research-hub-web/src/app/components/search-page/search-results-list/search-results-list.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MockProvider, MockModule } from 'ng-mocks';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { SearchBarService } from '../../search-bar/search-bar.service';
+import { SharedModule } from '../../shared/app.shared.module';
 import { SearchResultsListComponent } from './search-results-list.component';
 
 describe('SearchResultsListComponent', () => {
@@ -8,7 +11,14 @@ describe('SearchResultsListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SearchResultsListComponent ]
+      declarations: [ SearchResultsListComponent ],
+      imports: [
+        HttpClientTestingModule,
+        MockModule(SharedModule)
+      ],
+      providers: [
+        MockProvider(SearchBarService),
+      ]
     })
     .compileComponents();
   });
