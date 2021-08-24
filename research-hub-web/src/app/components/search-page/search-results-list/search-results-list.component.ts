a1cb22d2e7a76ee114b30f0b5f32d773f4ad5597
Rose McColl <rosemccoll@hotmail.com>
2021-08-24 14:37:52 +1200

    generate search-results-list component

---
+++ research-hub-web/src/app/components/search-page/search-results-list/search-results-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-results-list',
+  templateUrl: './search-results-list.component.html',
+  styleUrls: ['./search-results-list.component.scss']
+})
+export class SearchResultsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
