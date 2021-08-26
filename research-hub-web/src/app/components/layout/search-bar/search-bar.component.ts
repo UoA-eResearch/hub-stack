f61a124a13c30cadb9edb75f00e73ab187cad2fa
Luke <lukas.trombach@auckland.ac.nz>
2021-08-26 13:05:02 +1200

    change filters open and close behaviour

--- research-hub-web/src/app/components/layout/search-bar/search-bar.component.ts
+++ research-hub-web/src/app/components/layout/search-bar/search-bar.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit, Renderer2, ViewChild, ViewChildren } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 import { SearchFilters } from '@app/global/searchTypes';
 import { SearchService } from '@services/search.service';
 import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-bar',
@@ -11,18 +13,35 @@ import { Subscription } from 'rxjs';
 export class SearchBarComponent implements OnInit, OnDestroy {
   @Input() public showFilters = false;
 
+  @ViewChild('searchBar') searchBar: ElementRef;
+  @ViewChild('filterContent') filterContent: ElementRef;
+
   public searchText: string;
   public activeFilters: SearchFilters;
 
   private subscriptions = new Subscription();
 
   constructor(
-    public searchService: SearchService
-  ) { }
+    public searchService: SearchService,
+    private renderer: Renderer2,
+    private router: Router
+  ) {
+    this.renderer.listen('window', 'click', (e: Event) => {
+      if (e.target !== this.searchBar.nativeElement
+        && e.target !== this.filterContent.nativeElement
+        && !this.searchBar.nativeElement.contains(e.target)
+        && !this.filterContent.nativeElement.contains(e.target)) {
+          this.showFilters = false;
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.subscriptions.add(this.searchService.searchText.subscribe(text => this.searchText = text));
     this.subscriptions.add(this.searchService.searchFilters.subscribe(filters => this.activeFilters = filters));
+    this.subscriptions.add(this.router.events.pipe(
+      filter(event => event instanceof NavigationStart)
+    ).subscribe(() => this.showFilters = false));
   }
 
   ngOnDestroy(): void {
