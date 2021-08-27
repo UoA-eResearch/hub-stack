cf5deea51396a513e43f53fd1cdbe1ac7e89cc45
Rose McColl <rosemccoll@hotmail.com>
2021-08-27 16:46:21 +1200

    content type display name pipe

---
+++ research-hub-web/src/app/pipes/content-type-display-name.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ContentTypeDisplayNamePipe } from './content-type-display-name.pipe';
+
+describe('ContentTypeDisplayNamePipe', () => {
+  it('create an instance', () => {
+    const pipe = new ContentTypeDisplayNamePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
