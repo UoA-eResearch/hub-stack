ad731e91c2b257aabba36e5eb4e54f280e487a86
Luke <lukas.trombach@auckland.ac.nz>
2021-09-03 09:59:41 +1200

    disabled some e2e tests

--- research-hub-web/cypress/integration/search.e2e.js
+++ research-hub-web/cypress/integration/search.e2e.js
@@ -8,10 +8,13 @@ describe('ResearchHubs Search Page from Homepage', () => {
         cy.get('.initial-search-outer input').invoke('attr', 'data-placeholder').should('eq', 'Search');
     });
 
-    it('entering in the search bar navigates to the search page', () => {
-        cy.get('.initial-search-outer input').type('{enter}');
-        cy.get('#search-filters').should('exist');
-    });
+    /**
+     * Disabled for search redesign
+     */
+    // it('entering in the search bar navigates to the search page', () => {
+    //     cy.get('.initial-search-outer input').type('{enter}');
+    //     cy.get('#search-filters').should('exist');
+    // });
 
     it('displays search content', () => {
         cy.get('.initial-search-outer input').type('{enter}');
@@ -24,16 +27,22 @@ describe('ResearchHubs Search Page from Homepage', () => {
         cy.contains('Results');
     });
 
-    it('displays no results', () => {
-        cy.get('.initial-search-outer input').type('fffffffff');
-        cy.get('.initial-search-outer input').type('{enter}');
-        cy.contains('Sorry, your search for "fffffffff"');
-    });
+    /**
+     * Disabled for search redesign
+     */
+    // it('displays no results', () => {
+    //     cy.get('.initial-search-outer input').type('fffffffff');
+    //     cy.get('.initial-search-outer input').type('{enter}');
+    //     cy.contains('Sorry, your search for "fffffffff"');
+    // });
 
-    it('clicking a research category navigates to the search page', () => {
-        cy.get('#research-categories').contains('View Category').click();
-        cy.get('mat-chip').contains('Clear All').should('exist');
-    });
+    /**
+     * Disabled for search redesign
+     */
+    // it('clicking a research category navigates to the search page', () => {
+    //     cy.get('#research-categories').contains('View Category').click();
+    //     cy.get('mat-chip').contains('Clear All').should('exist');
+    // });
 
 });
 
@@ -42,15 +51,24 @@ describe('ResearchHubs Search Page', () => {
         cy.visit('/search');
     });
 
-    it('search filters should exist', () => {
-        cy.get('#search-filters').should('exist');
-    });
+    /**
+     * Disabled for search redesign
+     */
+    // it('search filters should exist', () => {
+    //     cy.get('#search-filters').should('exist');
+    // });
 
-    it('displays order by filtering', () => {
-        cy.get('#filtering-options').should('exist');
-    });
+    /**
+     * Disabled for search redesign
+     */
+    // it('displays order by filtering', () => {
+    //     cy.get('#filtering-options').should('exist');
+    // });
 
-    it('displays pagination', () => {
-        cy.get('#pagination').should('exist');
-    });
+    /**
+     * Disabled for search redesign
+     */
+    // it('displays pagination', () => {
+    //     cy.get('#pagination').should('exist');
+    // });
 })
