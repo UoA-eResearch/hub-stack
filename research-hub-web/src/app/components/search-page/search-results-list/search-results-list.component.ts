04a8da69fcd82f7b6dc07f8c8ec905be93fd4708
Luke <lukas.trombach@auckland.ac.nz>
2021-09-07 08:59:49 +1200

    added infinite loading on bottom scroll, moved skeleton loader into results list component (not working currently)

--- research-hub-web/src/app/components/search-page/search-results-list/search-results-list.component.ts
+++ research-hub-web/src/app/components/search-page/search-results-list/search-results-list.component.ts
@@ -1,21 +1,12 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, Input } from '@angular/core';
+import { SearchResults } from '@app/global/searchTypes';
 
 @Component({
   selector: 'app-search-results-list',
   templateUrl: './search-results-list.component.html',
   styleUrls: ['./search-results-list.component.scss']
 })
-export class SearchResultsListComponent implements OnChanges {
-  public pageNumber;
-
-  @Input() results;
-
-  ngOnChanges(changes: SimpleChanges) {
-    try { 
-      this.results = changes['results'].currentValue;
-    } catch(e) {
-      console.debug(`Error: ${e}`)
-    }
-  }
+export class SearchResultsListComponent {
+  @Input() loading = false;
+  @Input() results: SearchResults;
 }
