b40d4aafeaea0ff2871a67d14230e2e743cb53e9
Rose McColl <rosemccoll@hotmail.com>
2021-09-07 16:08:35 +1200

    generate cateories and activities pages components

---
+++ research-hub-web/src/app/components/activities-page/activities-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-activities-page',
+  templateUrl: './activities-page.component.html',
+  styleUrls: ['./activities-page.component.scss']
+})
+export class ActivitiesPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ research-hub-web/src/app/components/activities-page/activities-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivitiesPageComponent } from './activities-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    ActivitiesPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ActivitiesPageModule { }
