8bb39ca3fb717508e91dcbe586399ffba993d4e0
Rose McColl <rosemccoll@hotmail.com>
2021-09-07 14:34:56 +1200

    sentry.io setup

--- research-hub-web/src/environments/environment.ts
+++ research-hub-web/src/environments/environment.ts
@@ -3,6 +3,7 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
+  env: 'dev',
   production: false,
   cerGraphQLUrl: 'http://localhost:4000/',
   auth: {
--- research-hub-web/src/main.ts
+++ research-hub-web/src/main.ts
@@ -1,8 +1,31 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import * as Sentry from "@sentry/angular";
+import { Integrations } from "@sentry/tracing";
+import { AppModule } from './app/app.module';
+
+
+Sentry.init({
+  // https://docs.sentry.io/product/sentry-basics/dsn-explainer/
+  dsn: "https://eb04735190794f63abc9c1ddd3d73f64@o991241.ingest.sentry.io/5948230",
+  // https://docs.sentry.io/platforms/javascript/guides/angular/performance/instrumentation/automatic-instrumentation/#tracingorigins
+  integrations: [
+    new Integrations.BrowserTracing({
+      tracingOrigins: ["localhost", environment.cerGraphQLUrl],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    })
+  ],
+  environment: environment.env,
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 0.1,
+  ignoreErrors: [
+    // 'Authentication required to view protected content.'
+  ]
+});
+
 
 if (environment.production) {
   enableProdMode();
