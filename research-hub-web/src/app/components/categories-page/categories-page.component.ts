60c99de56c998972ef1c320b40e3c18c5c827c82
Rose McColl <rosemccoll@hotmail.com>
2021-09-09 14:25:29 +1200

    hook up page descriptions to contentful

--- research-hub-web/src/app/components/categories-page/categories-page.component.ts
+++ research-hub-web/src/app/components/categories-page/categories-page.component.ts
@@ -1,27 +1,39 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { SearchBarService } from '@app/components/search-bar/search-bar.service';
-import { AllCategoriesGQL, CategoryCollection } from '@app/graphql/schema';
-import { Observable } from 'rxjs';
-import { pluck } from 'rxjs/operators';
+import {
+  AllCategoriesGQL,
+  CategoryCollection,
+  GetHomepageGQL
+} from '@app/graphql/schema';
+import { Observable, Subscription } from 'rxjs';
+import { map, pluck } from 'rxjs/operators';
 
 @Component({
   selector: 'app-categories-page',
   templateUrl: './categories-page.component.html',
   styleUrls: ['./categories-page.component.scss']
 })
-export class CategoriesPageComponent implements OnInit {
+export class CategoriesPageComponent implements OnInit, OnDestroy {
   public title: string = 'Research Categories';
-  public description: string = 'The ResearchHub is your gateway to research support at the University of Auckland. Explore by topic.';
+  public description: string;
   public allCategories$: Observable<CategoryCollection>;
-  public events;
+  private subscriptions: Subscription = new Subscription();
 
   constructor(
     public allCategoriesGQL: AllCategoriesGQL,
+    private getHomepageGQL: GetHomepageGQL,
     public searchBarService: SearchBarService
   ) { }
 
   async ngOnInit() {
     this.allCategories$ = this.getAllCategories();
+    this.subscriptions.add(
+      this.getHomepageGQL.fetch().pipe(
+        map(x => x.data.homepageCollection.items[0])
+      ).subscribe(result => {
+        this.description = result.researchCategories;
+      })
+    )
   }
 
   public getAllCategories(): Observable<CategoryCollection> {
@@ -35,4 +47,7 @@ export class CategoriesPageComponent implements OnInit {
     return { researchCategories: [item.displayOrder] };
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
