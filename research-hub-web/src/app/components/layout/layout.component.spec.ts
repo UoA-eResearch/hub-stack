a479ef328380fde1b8bcc091fd2c19a20befa6e6
Rose McColl <rosemccoll@hotmail.com>
2021-10-06 16:11:25 +1300

    unit tests update

--- research-hub-web/src/app/components/layout/layout.component.spec.ts
+++ research-hub-web/src/app/components/layout/layout.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ApolloTestingModule } from 'apollo-angular/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { LayoutComponent } from './layout.component';
+import { MockComponent } from 'ng-mocks';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotificationComponent } from './notification/notification.component';
+import { FooterComponent } from './footer/footer.component';
 
 describe('LayoutComponent', () => {
   let component: LayoutComponent;
@@ -8,7 +14,17 @@ describe('LayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LayoutComponent ]
+      declarations: [ 
+        LayoutComponent,
+        MockComponent(NavbarComponent),
+        MockComponent(NotificationComponent),
+        MockComponent(FooterComponent)
+      ],
+      imports: [
+        HttpClientTestingModule,
+        ApolloTestingModule,
+        RouterTestingModule.withRoutes([])
+      ]
     })
     .compileComponents();
   });
