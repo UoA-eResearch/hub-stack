4d82dcd7a5cf1f7cf312c3db77b558a36fbf7300
Rose McColl <rosemccoll@hotmail.com>
2021-10-19 18:05:44 +1300

    naming

--- research-hub-web/src/app/services/body-media.service.ts
+++ research-hub-web/src/app/services/body-media.service.ts
@@ -17,43 +17,43 @@ export class BodyMediaService {
    * This subsequently enables the use of the ngx-contentful-rich-text library to render the rich text with our
    * custom components.
    * 
-   * @param bodyText 
-   * @returns the modified bodyText
+   * @param richText 
+   * @returns the modified richText
    */
-  resolveNodeData(bodyText): any {    
+  resolveNodeData(richText): any {    
     // create maps for each link type
     const assetBlockMap = new Map();
     // loop through the assets blocks and add them to the map
-    for (const asset of bodyText.links.assets.block) {
+    for (const asset of richText.links.assets.block) {
       assetBlockMap.set(asset.sys.id, asset);
     }
 
     const entryBlockMap = new Map();
     // loop through the entries blocks and add them to the map
-    for (const entry of bodyText.links.entries.block) {
+    for (const entry of richText.links.entries.block) {
       entryBlockMap.set(entry.sys.id, entry);
     }
 
     const entryInlineMap = new Map();
     // loop through the entry inlines and add them to the map
-    for (const entry of bodyText.links.entries.inline) {
+    for (const entry of richText.links.entries.inline) {
       entryInlineMap.set(entry.sys.id, entry);
     }
 
     const entryHyperlinkMap = new Map();
     // loop through the entry hyperlinks and add them to the map
-    for (const entry of bodyText.links.entries.hyperlink) {
+    for (const entry of richText.links.entries.hyperlink) {
       entryHyperlinkMap.set(entry.sys.id, entry);
     }
 
     const assetHyperlinkMap = new Map();
     // loop through the asset hyperlinks and add them to the map
-    for (const asset of bodyText.links.assets.hyperlink) {
+    for (const asset of richText.links.assets.hyperlink) {
       assetHyperlinkMap.set(asset.sys.id, asset);
     }
 
     //populate the rich text nodes with the entries and assets data
-    bodyText.json.content.forEach(node => {
+    richText.json.content.forEach(node => {
       switch(node.nodeType) {
         // For each type of node, first filter out null values, then find matching node.
         case 'embedded-asset-block':
@@ -72,11 +72,11 @@ export class BodyMediaService {
           node.data.target.contentItem = assetHyperlinkMap.get(node.data.target.sys.id);
           break;
         case 'blockquote':
-          node.data.target.contentItem = node.content[0];
+          break;
       }
     });
 
-    return bodyText;
+    return richText;
   }
 }
 
