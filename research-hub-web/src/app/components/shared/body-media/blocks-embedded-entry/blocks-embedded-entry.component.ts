ee9a563f040c47041784429d5fccbe9b8f7ab436
Rose McColl <rosemccoll@hotmail.com>
2021-10-20 20:39:30 +1300

    tidying

--- research-hub-web/src/app/components/shared/body-media/blocks-embedded-entry/blocks-embedded-entry.component.ts
+++ research-hub-web/src/app/components/shared/body-media/blocks-embedded-entry/blocks-embedded-entry.component.ts
@@ -13,7 +13,6 @@ export class BlocksEmbeddedEntryComponent extends NodeRenderer implements OnInit
   constructor(public bodyMediaService: BodyMediaService) { super(); }
 
   ngOnInit(): void {
-    console.log(this.node);
     this.contentItem = this.bodyMediaService.getContentItem(this.node);
   }
 }
--- research-hub-web/src/app/services/body-media.service.ts
+++ research-hub-web/src/app/services/body-media.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,58 +9,49 @@ export class BodyMediaService {
   private entryBlockMap = new Map();
   private entryHyperlinkMap = new Map();
   private entryInlineMap = new Map();
-
   
   constructor() { }
   
+  /**
+   * create maps for each link type so it can be used by our custom rich text rendering components
+   * (components that extend NodeRenderer) to lookup the corresponding content for a rich text node.
+   * @param links 
+   */
   buildLinkMaps(links) {
-    console.log(links)
-
-    // create maps for each link type
     if(links.assets) {
       for (const asset of links.assets.block) {
         this.assetBlockMap.set(asset.sys.id, asset);
       }
-      console.log(this.assetBlockMap)
       for (const asset of links.assets.hyperlink) {
         this.assetHyperlinkMap.set(asset.sys.id, asset);
       }
-      console.log(this.assetHyperlinkMap)
     }
 
     if(links.entries) {
       for (const entry of links.entries.block) {
         this.entryBlockMap.set(entry.sys.id, entry);
       }
-      console.log(this.entryBlockMap)
       for (const entry of links.entries.inline) {
         this.entryInlineMap.set(entry.sys.id, entry);
       }
-      console.log(this.entryInlineMap)
       for (const entry of links.entries.hyperlink) {
         this.entryHyperlinkMap.set(entry.sys.id, entry);
       }
-      console.log(this.entryHyperlinkMap)
-    }    
+    }
   }
 
   getContentItem(node): any {
     switch(node.nodeType) {
       // For each type of node find matching contentItem
       case 'embedded-asset-block':
-        console.log(this.assetBlockMap)
         return this.assetBlockMap.get(node.data.target.sys.id);
       case 'embedded-entry-block':
-        console.log(this.entryBlockMap)
         return this.entryBlockMap.get(node.data.target.sys.id);
       case 'embedded-entry-inline':
-        console.log(this.entryInlineMap)
         return this.entryInlineMap.get(node.data.target.sys.id);
       case 'entry-hyperlink':
-        console.log(this.entryHyperlinkMap)
         return this.entryHyperlinkMap.get(node.data.target.sys.id);
       case 'asset-hyperlink':
-        console.log(this.assetHyperlinkMap)
         return this.assetHyperlinkMap.get(node.data.target.sys.id);
     }
   }
