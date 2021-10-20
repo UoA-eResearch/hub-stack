0d171c8fb1d027b39b0dd27492ce0a5559415bb8
Rose McColl <rosemccoll@hotmail.com>
2021-10-20 20:54:54 +1300

    remove options from rich text pipe

--- research-hub-web/src/app/pipes/rich-text.pipe.ts
+++ research-hub-web/src/app/pipes/rich-text.pipe.ts
@@ -2,7 +2,6 @@
  * Transforms Contentful rich text to HTML.
  */
 import { Pipe, PipeTransform } from '@angular/core';
-import { BLOCKS, INLINES } from '@contentful/rich-text-types';
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 
 @Pipe({
@@ -10,14 +9,6 @@ import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 })
 export class RichTextToHTML implements PipeTransform {
     transform(value: any): string {
-        const options = {
-            renderNode: {
-                [BLOCKS.EMBEDDED_ENTRY]: (node) => `<p>Embedded Entry Block: ${node.data.target.sys.id} | ${node.nodeType}</p>`,
-                [BLOCKS.EMBEDDED_ASSET]: (node) => `<app-blocks-embedded-asset>${node}</app-blocks-embedded-asset>`,
-                [INLINES.EMBEDDED_ENTRY]: (node) => `<p>Embedded Entry Inline: ${node.data.target.sys.id} | ${node.nodeType}</p>`,
-                [INLINES.ENTRY_HYPERLINK]: (node) => `<p>Entry Hyperlink: ${node.data.target.sys.id} | ${node.nodeType}</p>`,
-            }
-        }
-        return documentToHtmlString(value, options);
+        return documentToHtmlString(value);
     }
 }
