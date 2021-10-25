fc704c32ea3ecf9d21d2b3f3ae929fc4a01705d4
Luke <lukas.trombach@auckland.ac.nz>
2021-10-26 12:37:56 +1300

    first version of org unit card

--- research-hub-web/src/app/components/shared/cards/org-unit-card/org-unit-card.component.ts
+++ research-hub-web/src/app/components/shared/cards/org-unit-card/org-unit-card.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { OrgUnit } from '@app/graphql/schema';
 
 @Component({
   selector: 'app-org-unit-card',
   templateUrl: './org-unit-card.component.html',
-  styleUrls: ['./org-unit-card.component.scss']
+  styleUrls: [
+    './org-unit-card.component.scss',
+    '../cards-common.scss'
+  ]
 })
-export class OrgUnitCardComponent implements OnInit {
+export class OrgUnitCardComponent {
+  @Input() orgUnit: OrgUnit;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
