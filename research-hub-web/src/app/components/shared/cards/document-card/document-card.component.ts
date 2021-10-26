87239df9f8cb6f6334f9f037f6c63837633a58c4
Luke <lukas.trombach@auckland.ac.nz>
2021-10-26 13:03:13 +1300

    first version of document card

--- research-hub-web/src/app/components/shared/cards/document-card/document-card.component.ts
+++ research-hub-web/src/app/components/shared/cards/document-card/document-card.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { OfficialDocuments } from '@app/graphql/schema';
 
 @Component({
   selector: 'app-document-card',
   templateUrl: './document-card.component.html',
-  styleUrls: ['./document-card.component.scss']
+  styleUrls: [
+    './document-card.component.scss',
+    '../cards-common.scss'
+  ]
 })
-export class DocumentCardComponent implements OnInit {
+export class DocumentCardComponent {
+  @Input() document: OfficialDocuments;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
