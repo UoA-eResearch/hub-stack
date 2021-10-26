c6336e9049d97c90efa345aec4d6cd458a1c7a2e
Rose McColl <rosemccoll@hotmail.com>
2021-10-27 11:49:06 +1300

    document rich text pipe usage

--- research-hub-web/src/app/pipes/rich-text.pipe.ts
+++ research-hub-web/src/app/pipes/rich-text.pipe.ts
@@ -8,6 +8,13 @@ import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
     name: 'richTextToHTML'
 })
 export class RichTextToHTML implements PipeTransform {
+    /**
+     * Use this pipe for rich text fields that do not have linked assets or entries,
+     * for example the location field on Event
+     * 
+     * @param value - the json document from a rich text field
+     * @returns the rich text document serialized into an html string.
+     */
     transform(value: any): string {
         return documentToHtmlString(value);
     }
