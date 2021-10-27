8e6f2a10d83944d195da1128b2c4ca16f39aff57
Rose McColl <rosemccoll@hotmail.com>
2021-10-27 14:59:46 +1300

    remove rich text pipe

--- research-hub-web/src/app/components/shared/app.shared.module.ts
+++ research-hub-web/src/app/components/shared/app.shared.module.ts
@@ -6,7 +6,6 @@ import { MaterialModule } from '@app/app.material.module';
 import { ErrorDialogComponent } from './error-dialog/error-dialog.component';
 import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 import { HumanCasePipe } from '@pipes/human-case.pipe';
-import { RichTextToHTML } from '@pipes/rich-text.pipe';
 import { ContentTypeDisplayNamePipe } from '@pipes/content-type-display-name.pipe';
 import { CollectionListComponent } from './collection-list/collection-list.component';
 import { RouterModule } from '@angular/router';
@@ -45,7 +44,6 @@ import { InlinesEntryHyperlinkComponent } from './body-media/inlines-entry-hyper
     ErrorDialogComponent,
     ConfirmDialogComponent,
     HumanCasePipe,
-    RichTextToHTML,
     ContentTypeDisplayNamePipe,
     CollectionListComponent,
     CardsComponent,
@@ -66,7 +64,6 @@ import { InlinesEntryHyperlinkComponent } from './body-media/inlines-entry-hyper
     MaterialModule,
     ErrorDialogComponent,
     HumanCasePipe,
-    RichTextToHTML,
     ContentTypeDisplayNamePipe,
     CollectionListComponent,
     CardsComponent,
@@ -76,7 +73,8 @@ import { InlinesEntryHyperlinkComponent } from './body-media/inlines-entry-hyper
     NgxPaginationModule,
     MarkdownToHtmlModule,
     NgxSkeletonLoaderModule,
-    ScrollingModule
+    ScrollingModule,
+    NgxContentfulRichTextModule
   ],
   providers: [],
 })
