e4d2a9fdec1b90e06c2ad457ef881f50b8f82737
Rose McColl <rosemccoll@hotmail.com>
2021-11-05 13:24:23 +1300

    lazy render, create summary text, styling

--- research-hub-web/src/app/components/shared/body-media/expandable-page-part/expandable-page-part.component.ts
+++ research-hub-web/src/app/components/shared/body-media/expandable-page-part/expandable-page-part.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnDestroy, OnInit, Type } from '@angular/core';
-import { Expand, GetExpandPartByIdGQL } from '@app/graphql/schema';
+import { Expand, GetExpandPartByIdGQL, Scalars } from '@app/graphql/schema';
 import { BodyMediaService } from '@services/body-media.service';
 import { NodeRenderer, MarkRenderer } from 'ngx-contentful-rich-text';
 import { Observable, Subscription } from 'rxjs';
@@ -52,6 +52,33 @@ export class ExpandablePagePartComponent implements OnInit, OnDestroy {
     } catch (e) {
       console.error(`Error loading expandable page part ${this.contentItem.sys.id}:`, e);
     }
+  }
+
+  public getSummaryText(json: Scalars['JSON']): string {
+    let summary: string = '';
+    const maxLength = 50;
+    const content: any[] = json.content;
+    try {
+      for (let i = 0; i < content.length; i++) {
+        if (content[i].nodeType === 'paragraph') {
+          for (let j = 0; j < content[i].content.length; j++) {
+            if (content[i].content[j].nodeType === 'text') {
+              if (summary.length <= maxLength) {
+                summary = summary + (content[i].content[j]?.value ? content[i].content[j].value : '');
+              } else {
+                break;
+              }
+            }
+          }
+        }
+      }
+      summary = summary.length > maxLength ?
+      summary.substring(0, maxLength - 3) + "..." :
+      summary.substring(0, maxLength);
+    } catch (e) {
+      console.error(`Error creating expandable page part summary text for ${this.contentItem.sys.id}:`, e);
+    }
+    return summary;
   }
 
   ngOnDestroy(): void {
