e5f22a732029b1637460135b852ab27c0223e7f3
Luke <lukas.trombach@auckland.ac.nz>
2021-11-11 11:21:25 +1300

    breadcrumb refactor

--- research-hub-web/src/app/components/shared/breadcrumbs/breadcrumbs.component.ts
+++ research-hub-web/src/app/components/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CerGraphqlService, SubHubTitleAndSlug } from '@services/cer-graphql.service';
+import { from } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html',
   styleUrls: ['./breadcrumbs.component.scss']
 })
-export class BreadcrumbsComponent {
-  @Input() contentItem;
-  @Input() title;
+export class BreadcrumbsComponent implements OnInit {
+  @Input() title: string;
+  public parentSubHubs: SubHubTitleAndSlug[] = [];
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private cerGraphQLService: CerGraphqlService
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.pipe(
+      map((paramMap) => paramMap.get('slug')),
+      mergeMap((slug: string) => from(this.cerGraphQLService.getParentSubHubs(slug)))
+    ).subscribe((result) => this.parentSubHubs = result);
+  }
 }
