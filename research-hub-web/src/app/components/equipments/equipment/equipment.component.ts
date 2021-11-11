af1fdf37db033ae2b703069b10f96048fdc99fe5
Luke <lukas.trombach@auckland.ac.nz>
2021-11-11 13:22:05 +1300

    final changes to equipment

--- research-hub-web/src/app/components/equipments/equipment/equipment.component.ts
+++ research-hub-web/src/app/components/equipments/equipment/equipment.component.ts
@@ -1,20 +1,11 @@
-import { Location } from '@angular/common';
 import { Component, OnDestroy, OnInit, Type } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import {
-  AllEquipmentGQL,
-  AllEquipmentSlugsGQL,
-  Equipment,
-  EquipmentCollection,
-  GetEquipmentBySlugGQL
-} from '@graphql/schema';
+import { Equipment, GetEquipmentBySlugGQL } from '@graphql/schema';
 import { BodyMediaService } from '@services/body-media.service';
-import { CerGraphqlService } from '@services/cer-graphql.service';
 import { PageTitleService } from '@services/page-title.service';
 import { MarkRenderer, NodeRenderer } from 'ngx-contentful-rich-text';
-import { DeviceDetectorService } from 'ngx-device-detector';
-import { Observable, Subscription } from 'rxjs';
-import { flatMap, pluck } from 'rxjs/operators';
+import { Observable, of, Subscription, throwError } from 'rxjs';
+import { map, mergeMap, switchMap } from 'rxjs/operators';
 import supportsWebP from 'supports-webp';
 
 @Component({
@@ -25,39 +16,25 @@ import supportsWebP from 'supports-webp';
 export class EquipmentComponent implements OnInit, OnDestroy {
   public nodeRenderers: Record<string, Type<NodeRenderer>>;
   public markRenderers: Record<string, Type<MarkRenderer>>;
-  public slug: string;
-  public equipment;
-  public equipment$: Subscription;
-  public route$: Subscription;
-  public bodyLinks$: Subscription;
-  public allEquipment$: Observable<EquipmentCollection>;
-  public parentSubHubs;
+
+  private subscriptions = new Subscription();
+
+  public equipment: Equipment;
   public isMobile: Boolean;
   public supportsWebp: Boolean;
   public bannerImageUrl: string;
 
   constructor(
     public route: ActivatedRoute,
-    public allEquipmentGQL: AllEquipmentGQL,
-    public allEquipmentSlugsGQL: AllEquipmentSlugsGQL,
     public getEquipmentBySlugGQL: GetEquipmentBySlugGQL,
-    public cerGraphQLService: CerGraphqlService,
     public pageTitleService: PageTitleService,
     public bodyMediaService: BodyMediaService,
     public router: Router,
-    private deviceService: DeviceDetectorService,
-    public location: Location
   ) {
-    this.detectDevice();
     this.detectWebP();
 
     this.nodeRenderers = this.bodyMediaService.nodeRenderers;
     this.markRenderers = this.bodyMediaService.markRenderers;
-  }
-
-  // Detect if device is Mobile
-  detectDevice() {
-    this.isMobile = this.deviceService.isMobile();
   }
 
   detectWebP() {
@@ -66,97 +43,69 @@ export class EquipmentComponent implements OnInit, OnDestroy {
     });
   }
 
-  async ngOnInit() {
-    /**
-     * Check if there is a slug URL parameter present. If so, this is
-     * passed to the getEquipmentBySlug() method.
-     */
-    this.route$ = this.route.params.subscribe(params => {
-      this.slug = params.slug || this.route.snapshot.data.slug;
-      this._loadContent();
-    });
+  ngOnInit() {
+    this.subscriptions.add(this.route.params.pipe(
+      map((params) => {
+        return (params.slug || this.route.snapshot.data.slug) as string;
+      }),
+      switchMap((slug) => this.loadEquipment(slug))
+    ).subscribe({
+      next: (equipment: Equipment) => this.equipment = equipment,
+      error: (error) => {
+        console.error(error);
+        this.router.navigate(['error', 404]);
+      }
+    }));
   }
 
-  /**
-   * Function that loads the Equipment/collection depending on if a slug is present.
-   */
-  private async _loadContent() {
-    /**
-     * If this.slug is defined, we're loading an individual Equipment,
-     * therefore run the corresponding query. If not, return all Equipment.
-     */
-    // Check if the equipment slug is valid otherwise redirect to 404
-    this.getAllEquipmentSlugs().subscribe(data => {
-      let slugs = [];
-      data.items.forEach(data => {
-        slugs.push(data.slug)
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  private loadEquipment(slug: string): Observable<Equipment> {
+    return this.getEquipmentBySlug(slug).pipe(
+      map((data) => {
+        // Strip nulls from related collection data.
+        data.relatedContactsCollection.items = data.relatedContactsCollection.items.filter(item => item);
+        data.relatedDocsCollection.items = data.relatedDocsCollection.items.filter(item => item);
+        data.relatedItemsCollection.items = data.relatedItemsCollection.items.filter(item => item);
+        data.relatedOrgsCollection.items = data.relatedOrgsCollection.items.filter(item => item);
+
+        // If Call To Action is an email address
+        if (data.callToAction?.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+          data['callToAction'] = 'mailto:' + data['callToAction'];
+        }
+
+        // Set banner image URL for webp format if webp is supported
+        if (data.banner?.url) {
+          this.bannerImageUrl = this.supportsWebp ? data.banner?.url + '?w=1900&fm=webp' : data.banner?.url + '?w=1900';
+        } else {
+          this.bannerImageUrl = undefined;
+        }
+
+        // For each rich text field add the links to the link maps in the body media service to enable rich text rendering
+        this.bodyMediaService.buildLinkMaps(data.bodyText?.links);
+
+        this.pageTitleService.title = data.title;
+
+        return data;
       })
-      if (!slugs.includes(this.slug)) { this.router.navigate(['error/404']) }
-    });
-    this.equipment = this.getEquipmentBySlug(this.slug);
-    this.equipment$ = this.getEquipmentBySlug(this.slug).subscribe(data => {
-      // Strip nulls from related collection data.
-      data.relatedContactsCollection.items = data.relatedContactsCollection.items.filter(item => item);
-      data.relatedDocsCollection.items = data.relatedDocsCollection.items.filter(item => item);
-      data.relatedItemsCollection.items = data.relatedItemsCollection.items.filter(item => item);
-      data.relatedOrgsCollection.items = data.relatedOrgsCollection.items.filter(item => item);
-
-      // If Call To Action is an email address
-      if (data.callToAction?.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
-        data['callToAction'] = 'mailto:' + data['callToAction'];
-      }
-
-      // Set banner image URL for webp format if webp is supported
-      if (data.banner?.url) {
-        this.bannerImageUrl = this.supportsWebp ? data.banner?.url + '?w=1900&fm=webp' : data.banner?.url + '?w=1900';
-      } else {
-        this.bannerImageUrl = undefined;
-      }
-
-      // For each rich text field add the links to the link maps in the body media service to enable rich text rendering
-      this.bodyMediaService.buildLinkMaps(data.bodyText?.links);
-
-      this.pageTitleService.title = data.title;
-    });
-    this.parentSubHubs = await this.cerGraphQLService.getParentSubHubs(this.slug);
-
+    );
   }
 
   /**
-   * Function that returns all equipment slugs from the EquipmentCollection as an observable
-   * of type EquipmentCollection. This is then unwrapped with the async pipe.
+   * Function that returns an individual article from the ArticleCollection by it's slug
+   * as an observable of type Article.
    *
-   * This function called to determine if a valid slug has been searched otherwise redirect
-   *
-   */
-  public getAllEquipmentSlugs(): Observable<EquipmentCollection> {
-    try {
-      return this.allEquipmentSlugsGQL.fetch()
-        .pipe(pluck('data', 'equipmentCollection')) as Observable<EquipmentCollection>
-    } catch (e) { console.error('Error loading all equipment', e) };
-  }
-
-  /**
-   * Function that returns an individual Equipment from the EquipmentCollection by it's slug
-   * as an observable of type Equipment. This is then unwrapped with the async pipe.
-   *
-   * This function is only called if no slug parameter is present in the URL, i.e.
-   * the user is visiting /Equipment.
-   *
-   * @param slug The Equipment's slug. Retrieved from the route parameter of the same name.
+   * @param slug The article's slug. Retrieved from the route parameter of the same name.
    */
   public getEquipmentBySlug(slug: string): Observable<Equipment> {
-    try {
-      return this.getEquipmentBySlugGQL.fetch({ slug: this.slug })
-        .pipe(flatMap(x => x.data.equipmentCollection.items)) as Observable<Equipment>;
-    } catch (e) { console.error(`Error loading equipment ${slug}:`, e); }
-  }
-
-  ngOnDestroy() {
-    try {
-      this.equipment$.unsubscribe();
-      this.route$.unsubscribe();
-      this.bodyLinks$.unsubscribe();
-    } catch { }
+    return this.getEquipmentBySlugGQL.fetch({ slug }).pipe(
+      mergeMap(x =>
+        x.data.equipmentCollection.items.length === 0
+          ? throwError(`Could not load equipment with slug "${slug}"`)
+          : of(x.data.equipmentCollection.items[0])
+      )
+    ) as Observable<Equipment>;
   }
 }
