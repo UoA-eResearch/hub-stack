c3cfea15e0db01e7384af0527be0093cd32d4902
Rose McColl <rosemccoll@hotmail.com>
2021-11-15 15:55:14 +1300

    fix scrolling home page image on iOS

--- research-hub-web/src/app/components/home/banner-image/banner-image.component.ts
+++ research-hub-web/src/app/components/home/banner-image/banner-image.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
 import supportsWebP from 'supports-webp';
 
 @Component({
@@ -12,19 +13,27 @@ export class BannerImageComponent implements OnInit {
   @Input() logoImageUrl: string;
   @Input() title: string;
 
+  public isMobile: Boolean;
   public supportsWebp: Boolean;
 
   //public title = "Find Services, Resources and People to accelerate your research";
   public aucklandUniUrl = 'https://auckland.ac.nz';
 
   constructor(
+    private deviceService: DeviceDetectorService
   ) {
+    this.detectDevice();
     this.detectWebP();
   }
 
   ngOnInit(): void {
     this.bannerImageUrl = this.supportsWebp ? this.bannerImageUrl + '?fm=webp' : this.bannerImageUrl;
     this.logoImageUrl = this.supportsWebp ? this.logoImageUrl + '?fm=webp' : this.logoImageUrl;
+  }
+
+  detectDevice() {
+    this.isMobile = this.deviceService.isMobile();
+    console.log(this.isMobile)
   }
 
   detectWebP() {
