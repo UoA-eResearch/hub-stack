ed7868d7d5f76d2a939b2941a4ae856390dc13ab
Luke <lukas.trombach@auckland.ac.nz>
2021-11-15 15:32:37 +1300

    changes to software

--- research-hub-web/src/app/components/softwares/software/software.component.ts
+++ research-hub-web/src/app/components/softwares/software/software.component.ts
@@ -1,19 +1,11 @@
 import { Component, OnDestroy, OnInit, Type } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import {
-  AllSoftwareGQL,
-  AllSoftwareSlugsGQL,
-  GetSoftwareBySlugGQL,
-  Software,
-  SoftwareCollection
-} from '@graphql/schema';
+import { GetSoftwareBySlugGQL, Software } from '@graphql/schema';
 import { BodyMediaService } from '@services/body-media.service';
-import { CerGraphqlService } from '@services/cer-graphql.service';
 import { PageTitleService } from '@services/page-title.service';
 import { MarkRenderer, NodeRenderer } from 'ngx-contentful-rich-text';
-import { DeviceDetectorService } from 'ngx-device-detector';
-import { Observable, Subscription } from 'rxjs';
-import { flatMap, pluck } from 'rxjs/operators';
+import { Observable, of, Subscription, throwError } from 'rxjs';
+import { map, mergeMap, switchMap } from 'rxjs/operators';
 import supportsWebP from 'supports-webp';
 
 @Component({
@@ -24,37 +16,24 @@ import supportsWebP from 'supports-webp';
 export class SoftwareComponent implements OnInit, OnDestroy {
   public nodeRenderers: Record<string, Type<NodeRenderer>>;
   public markRenderers: Record<string, Type<MarkRenderer>>;
-  public slug: string;
-  public software;
-  public software$: Subscription;
-  public route$: Subscription;
-  public bodyLinks$: Subscription;
-  public allSoftware$: Observable<SoftwareCollection>;
-  public parentSubHubs;
-  public isMobile: Boolean;
+
+  private subscriptions = new Subscription();
+
+  public software: Software;
   public supportsWebp: Boolean;
   public bannerImageUrl: string;
 
   constructor(
     public route: ActivatedRoute,
-    public allSoftwareGQL: AllSoftwareGQL,
-    public allSoftwareSlugsGQL: AllSoftwareSlugsGQL,
     public getSoftwareBySlugGQL: GetSoftwareBySlugGQL,
-    public cerGraphQLService: CerGraphqlService,
     public pageTitleService: PageTitleService,
     public bodyMediaService: BodyMediaService,
     public router: Router,
-    private deviceService: DeviceDetectorService
   ) {
-    this.detectDevice();
     this.detectWebP();
 
     this.nodeRenderers = this.bodyMediaService.nodeRenderers;
     this.markRenderers = this.bodyMediaService.markRenderers;
-  }
-
-  detectDevice() {
-    this.isMobile = this.deviceService.isMobile();
   }
 
   detectWebP() {
@@ -63,91 +42,67 @@ export class SoftwareComponent implements OnInit, OnDestroy {
     });
   }
 
-  async ngOnInit() {
-    /**
-     * Check if there is a slug URL parameter present. If so, this is
-     * passed to the getSoftwareBySlug() method.
-     */
-    this.route$ = this.route.params.subscribe(params => {
-      this.slug = params.slug || this.route.snapshot.data.slug;
-      this._loadContent();
-    });
+  ngOnInit() {
+    this.subscriptions.add(this.route.params.pipe(
+      map((params) => {
+        return (params.slug || this.route.snapshot.data.slug) as string;
+      }),
+      switchMap((slug) => this.loadSoftware(slug))
+    ).subscribe({
+      next: (software: Software) => this.software = software,
+      error: (error) => {
+        console.error(error);
+        this.router.navigate(['error', 404]);
+      }
+    }));
   }
 
-  /**
-   * Function that loads the Software/collection depending on if a slug is present.
-   */
-  private async _loadContent() {
-    // Check if the article slug is valid otherwise redirect to 404
-    this.getAllSoftwareSlugs().subscribe(data => {
-      let slugs = [];
-      data.items.forEach(data => {
-        slugs.push(data.slug)
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  private loadSoftware(slug: string): Observable<Software> {
+    return this.getSoftwareBySlug(slug).pipe(
+      map(data => {
+        // Strip nulls from related collection data.
+        data.relatedContactsCollection.items = data.relatedContactsCollection.items.filter(item => item);
+        data.relatedDocsCollection.items = data.relatedDocsCollection.items.filter(item => item);
+        data.relatedItemsCollection.items = data.relatedItemsCollection.items.filter(item => item);
+        data.relatedOrgsCollection.items = data.relatedOrgsCollection.items.filter(item => item);
+
+        // Set banner image URL for webp format if webp is supported
+        if (data.banner?.url) {
+          this.bannerImageUrl = this.supportsWebp ? data.banner?.url + '?w=1900&fm=webp' : data.banner?.url + '?w=1900';
+        } else {
+          this.bannerImageUrl = undefined;
+        }
+
+        // If Call To Action is an email address
+        if (data.callToAction?.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+          data['callToAction'] = 'mailto:' + data['callToAction'];
+        }
+
+        this.bodyMediaService.buildLinkMaps(data.bodyText?.links);
+        this.pageTitleService.title = data.title;
+
+        return data;
       })
-      if (!slugs.includes(this.slug)) { this.router.navigate(['error/404']) }
-    });
-    this.software = this.getSoftwareBySlug(this.slug);
-    this.software$ = this.getSoftwareBySlug(this.slug).subscribe(data => {
-      // Strip nulls from related collection data.
-      data.relatedContactsCollection.items = data.relatedContactsCollection.items.filter(item => item);
-      data.relatedDocsCollection.items = data.relatedDocsCollection.items.filter(item => item);
-      data.relatedItemsCollection.items = data.relatedItemsCollection.items.filter(item => item);
-      data.relatedOrgsCollection.items = data.relatedOrgsCollection.items.filter(item => item);
-
-      // Set banner image URL for webp format if webp is supported
-      if (data.banner?.url) {
-        this.bannerImageUrl = this.supportsWebp ? data.banner?.url + '?w=1900&fm=webp' : data.banner?.url + '?w=1900';
-      } else {
-        this.bannerImageUrl = undefined;
-      }
-
-      // If Call To Action is an email address
-      if (data.callToAction?.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
-        data['callToAction'] = 'mailto:' + data['callToAction'];
-      }
-
-      this.bodyMediaService.buildLinkMaps(data.bodyText?.links);
-      this.pageTitleService.title = data.title;
-    });
-    this.parentSubHubs = await this.cerGraphQLService.getParentSubHubs(this.slug);
-
+    );
   }
 
   /**
-   * Function that returns all software slugs from the SoftwareCollection as an observable
-   * of type SoftwareCollection. This is then unwrapped with the async pipe.
+   * Function that returns an individual software from the SoftwareCollection by it's slug
+   * as an observable of type Software.
    *
-   * This function called to determine if a valid slug has been searched otherwise redirect
-   *
-   */
-  public getAllSoftwareSlugs(): Observable<SoftwareCollection> {
-    try {
-      return this.allSoftwareSlugsGQL.fetch()
-        .pipe(pluck('data', 'softwareCollection')) as Observable<SoftwareCollection>
-    } catch (e) { console.error('Error loading all software:', e) };
-  }
-
-  /**
-   * Function that returns an individual Software from the SoftwareCollection by it's slug
-   * as an observable of type Software. This is then unwrapped with the async pipe.
-   *
-   * This function is only called if no slug parameter is present in the URL, i.e.
-   * the user is visiting /Software.
-   *
-   * @param slug The Software's slug. Retrieved from the route parameter of the same name.
+   * @param slug The software's slug. Retrieved from the route parameter of the same name.
    */
   public getSoftwareBySlug(slug: string): Observable<Software> {
-    try {
-      return this.getSoftwareBySlugGQL.fetch({ slug: this.slug })
-        .pipe(flatMap(x => x.data.softwareCollection.items)) as Observable<Software>;
-    } catch (e) { console.error(`Error loading Software ${slug}:`, e); }
-  }
-
-  ngOnDestroy() {
-    try {
-      this.software$.unsubscribe();
-      this.route$.unsubscribe();
-      this.bodyLinks$.unsubscribe();
-    } catch { }
+    return this.getSoftwareBySlugGQL.fetch({ slug }).pipe(
+      mergeMap(x =>
+        x.data.softwareCollection.items.length === 0
+          ? throwError(`Could not load article with slug "${slug}"`)
+          : of(x.data.softwareCollection.items[0])
+      )
+    ) as Observable<Software>;
   }
 }
