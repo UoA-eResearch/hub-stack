3dd6eb687bdaa35dfe9f70284c41b090f11cd7a2
rosemcc <rosemccoll@hotmail.com>
2021-12-23 08:36:58 +1300

    note

--- research-hub-web/src/app/services/sw-updates.service.ts
+++ research-hub-web/src/app/services/sw-updates.service.ts
@@ -11,8 +11,12 @@ export class SwUpdatesService {
   ) { }
 
   enable() {
+    // if the service worker isn't active
     if (!this.updates.isEnabled) return;
 
     console.log('SW updates service enabled. ' + this.updates.isEnabled);
+
+    // TODO: check for updates, activate updates, console.log version change
+    // note that swupdate methods used in docs examples have been deprecated. documentation is not up to date. 
   }
 }
