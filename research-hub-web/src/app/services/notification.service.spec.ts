d01a3d6f7f8a66ec542471d3ed3c59518d183734
Luke <lukas.trombach@auckland.ac.nz>
2022-01-19 13:25:52 +1300

    removed focused describe

--- research-hub-web/src/app/services/notification.service.spec.ts
+++ research-hub-web/src/app/services/notification.service.spec.ts
@@ -7,9 +7,7 @@ import { AppStorageService } from './app-storage.service';
 
 import { NotificationService } from './notification.service';
 
-fdescribe('NotificationService', () => {
-  MockInstance.scope();
-
+describe('NotificationService', () => {
   let service: NotificationService;
   let backend: ApolloTestingController;
 
