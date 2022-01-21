0fe37ba070c0b87774d66cdae267b406586f835c
rosemcc <rosemccoll@hotmail.com>
2022-01-21 15:14:53 +1300

    remove unused material modules

--- research-hub-web/src/app/app.material.module.ts
+++ research-hub-web/src/app/app.material.module.ts
@@ -4,25 +4,15 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatChipsModule } from '@angular/material/chips';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatStepperModule } from '@angular/material/stepper';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -31,26 +21,17 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
   imports: [
     CommonModule,
     MatSelectModule,
-    MatGridListModule,
     MatIconModule,
     MatButtonModule,
     MatChipsModule,
     MatCheckboxModule,
     MatCardModule,
     MatInputModule,
-    MatSidenavModule,
     MatToolbarModule,
     MatListModule,
-    MatProgressBarModule,
-    MatProgressSpinnerModule,
-    MatFormFieldModule,
     MatAutocompleteModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
     MatExpansionModule,
-    MatStepperModule,
     MatTooltipModule,
-    MatRadioModule,
     MatTabsModule,
     MatBadgeModule,
     MatSnackBarModule
@@ -58,27 +39,17 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
   declarations: [],
   exports: [
     MatSelectModule,
-    MatGridListModule,
     MatIconModule,
     MatButtonModule,
     MatChipsModule,
     MatCheckboxModule,
     MatCardModule,
     MatInputModule,
-    MatSidenavModule,
     MatToolbarModule,
     MatListModule,
-    MatProgressBarModule,
-    MatProgressSpinnerModule,
-    MatFormFieldModule,
     MatAutocompleteModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
     MatExpansionModule,
-    MatStepperModule,
     MatTooltipModule,
-    MatRadioModule,
-    MatSlideToggleModule,
     MatTabsModule,
     MatBadgeModule,
     MatSnackBarModule
