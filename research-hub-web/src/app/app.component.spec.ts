c139901ecf9c59758a7483c267cf2a1e3dd469cd
rosemcc <rosemccoll@hotmail.com>
2022-01-25 14:39:42 +1300

    refactor swUpdates, add tests

--- research-hub-web/src/app/app.component.spec.ts
+++ research-hub-web/src/app/app.component.spec.ts
@@ -6,11 +6,17 @@ import { ApolloTestingModule } from 'apollo-angular/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MockModule, MockProvider } from 'ng-mocks';
 import { AppLayoutModule } from './components/layout/layout.module';
-import { SwUpdatesService } from '@services/sw-updates.service';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatSnackBarHarness } from '@angular/material/snack-bar/testing';
+import { ServiceWorkerModule, SwUpdate } from '@angular/service-worker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -19,11 +25,14 @@ describe('AppComponent', () => {
         HttpClientTestingModule,
         ApolloTestingModule,
         RouterTestingModule.withRoutes([]),
-        MockModule(AppLayoutModule)
+        MockModule(AppLayoutModule),
+        MatSnackBarModule,
+        MockModule(BrowserAnimationsModule),
+        ServiceWorkerModule.register('', { enabled: false })
       ],
       providers: [
         MockProvider(PageTitleService),
-        MockProvider(SwUpdatesService)
+        MockProvider(SwUpdate)
       ]
     })
       .compileComponents();
@@ -33,6 +42,7 @@ describe('AppComponent', () => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.documentRootLoader(fixture);
   });
 
   afterEach(() => {
@@ -41,5 +51,41 @@ describe('AppComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load harness for snack-bar', async () => {
+    fixture.componentInstance.openSnackBar('Testing', '123');
+    let snackBars = await loader.getAllHarnesses(MatSnackBarHarness);
+    expect(snackBars.length).toBe(1);
+  });
+
+  it('should be able to get message of snack-bar', async () => {
+    fixture.componentInstance.openSnackBar('Testing', '123');
+    let snackBar = await loader.getHarness(MatSnackBarHarness);
+    expect(await snackBar.getMessage()).toBe('Testing');
+  });
+
+  it('should be able to get action description of snack-bar', async () => {
+    fixture.componentInstance.openSnackBar('Testing', '123');
+    let snackBar = await loader.getHarness(MatSnackBarHarness);
+    expect(await snackBar.getActionDescription()).toBe('123');
+  });
+
+  it('should be able to check whether simple snack-bar has action', async () => {
+    fixture.componentInstance.openSnackBar('Testing', '123');
+    let snackBar = await loader.getHarness(MatSnackBarHarness);
+    expect(await snackBar.hasAction()).toBe(true);
+  });
+
+  it('Should subscribe to router events', () => {
+    const subscribeToRouterEventsSpy = spyOn(component, 'subscribeToRouterEvents');
+    component.ngOnInit();
+    expect(subscribeToRouterEventsSpy).toHaveBeenCalled();
+  });
+
+  it('Should try to enable service worker', () => {
+    const enableServiceWorkerSpy = spyOn(component, 'enableServiceWorker');
+    component.ngOnInit();
+    expect(enableServiceWorkerSpy).toHaveBeenCalled();
   });
 });
--- research-hub-web/src/app/app.component.ts
+++ research-hub-web/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NavigationEnd, NavigationStart, Router, RouterEvent } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { filter, switchMap } from 'rxjs/operators';
 import { PageTitleService } from './services/page-title.service';
-import { SwUpdatesService } from '@services/sw-updates.service';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarRef, MatSnackBarVerticalPosition, TextOnlySnackBar } from '@angular/material/snack-bar';
+import { SwUpdate, UnrecoverableStateEvent, VersionDetectedEvent, VersionEvent, VersionInstallationFailedEvent, VersionReadyEvent } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -13,23 +14,24 @@ import { SwUpdatesService } from '@services/sw-updates.service';
 })
 export class AppComponent implements OnInit, OnDestroy {
   private subscriptions: Subscription = new Subscription();
+  private horizontalPosition: MatSnackBarHorizontalPosition = 'start';
+  private verticalPosition: MatSnackBarVerticalPosition = 'bottom';
 
   constructor(
     private router: Router,
     public titleService: PageTitleService,
-    private swUpdatesService: SwUpdatesService
+    private swUpdate: SwUpdate,
+    private snackBar: MatSnackBar
   ) { }
 
 
   ngOnInit(): void {
     this.subscribeToRouterEvents();
     this.titleService.title = ''; //sets title to welcome message
-    
-    // Start listening for Service Worker version update events.
-    this.swUpdatesService.enable();
+    this.enableServiceWorker();
   }
 
-  private subscribeToRouterEvents() {
+  subscribeToRouterEvents() {
     this.subscriptions.add(
       this.router.events.pipe(
         filter((event: RouterEvent) => event instanceof NavigationStart || event instanceof NavigationEnd)
@@ -66,6 +68,64 @@ export class AppComponent implements OnInit, OnDestroy {
     if (url.match('^/#/')) {
       this.router.navigateByUrl(url.replace('#/', ''), { replaceUrl: true });
     }
+  }
+
+  enableServiceWorker() {
+    if (!this.swUpdate.isEnabled) return; // if the service worker isn't active
+
+    this.subscriptions.add(this.swUpdate.versionUpdates.pipe(
+      filter((event: VersionEvent): event is VersionReadyEvent => event.type === 'VERSION_READY'),
+      switchMap((event: VersionReadyEvent) => {
+        console.log('The current version is', event.currentVersion.hash);
+        console.log('The latest version is', event.latestVersion.hash);
+        return this.openSnackBar('There is a new version of the ResearchHub available!', 'Update').afterDismissed()
+      })
+    ).subscribe(() => {
+      this.activateUpdate();
+    }));
+    
+    this.subscriptions.add(this.swUpdate.versionUpdates.pipe(
+      filter((event: VersionEvent): event is VersionDetectedEvent => event.type === 'VERSION_DETECTED')
+    ).subscribe(() => {
+      console.log('A new version of the ResearchHub has been detected.');
+    }));
+
+    this.subscriptions.add(this.swUpdate.versionUpdates.pipe(
+      filter((event: VersionEvent): event is VersionInstallationFailedEvent => event.type === 'VERSION_INSTALLATION_FAILED')
+    ).subscribe((event: VersionInstallationFailedEvent) => {
+      console.error(`Error installing ResearchHub version ${event.version.hash}: ${event.error}`);
+    }));
+
+    // https://angular.io/guide/service-worker-communications#handling-an-unrecoverable-state
+    this.subscriptions.add(this.swUpdate.unrecoverable.subscribe((event: UnrecoverableStateEvent) => {
+      console.error(`Service worker error occurred: ${event.reason}`);
+      window.location.reload();
+    }));
+  }
+
+  openSnackBar(message: string, action: string): MatSnackBarRef<TextOnlySnackBar> {
+    return this.snackBar.open(message, action, {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });
+  }
+
+  activateUpdate(): void {
+    this.swUpdate.activateUpdate()
+      .then((activated) => {
+        if (activated) {
+          console.log('Updated successfully.');
+          // Calling activateUpdate() without reloading the page could break lazy-loading 
+          // in a currently running app, especially if the lazy-loaded chunks use filenames 
+          // with hashes, which change every version. Therefore, it is recommended to reload 
+          // the page once the promise returned by activateUpdate() is resolved.
+          // https://angular.io/guide/service-worker-communications#forcing-update-activation
+          window.location.reload();
+        } else {
+          console.log('Client already on latest version.');
+        }
+      })
+      .catch((error) => console.error('Error updating to latest version. ', error))
   }
 
   ngOnDestroy() {
