0f1a0c9fd05a570862f446aedb29dc3811b6e9cf
rosemcc <rosemccoll@hotmail.com>
2022-02-10 14:40:49 +1300

    split out intranet search results into separate components, styling

---
+++ research-hub-web/src/app/components/search-page/intranet-search-results-list/intranet-search-results-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { IntranetSearchResults } from '@app/global/searchTypes';
+
+@Component({
+  selector: 'app-intranet-search-results-list',
+  templateUrl: './intranet-search-results-list.component.html',
+  styleUrls: ['./intranet-search-results-list.component.scss']
+})
+export class IntranetSearchResultsListComponent {
+  @Input() loading = false;
+  @Input() searchResults: IntranetSearchResults;
+
+  constructor() { }
+}
--- research-hub-web/src/app/components/search-page/intranet-search/intranet-search.component.ts
+++ research-hub-web/src/app/components/search-page/intranet-search/intranet-search.component.ts
@@ -1,31 +1,29 @@
 import { Component, NgZone, OnDestroy, OnInit } from '@angular/core';
 import { EMPTY, Observable } from 'rxjs';
 import { Subscription } from 'rxjs';
-import { SearchFilters, SearchQuery, SearchResult, SortOrder, ContentType, SearchResults } from '@app/global/searchTypes';
+import { SortOrder, IntranetSearchQuery, IntranetSearchResult, IntranetSearchResults } from '@app/global/searchTypes';
 import { SearchService } from '@services/search.service';
 import { ActivatedRoute } from '@angular/router';
-import supportsWebP from 'supports-webp';
 import { concatMap, filter, map, pairwise, switchMap, tap, throttleTime } from 'rxjs/operators';
 import { CdkScrollable, ScrollDispatcher } from '@angular/cdk/scrolling';
-import { PageTitleService } from '@services/page-title.service';
+import { IntranetSearchService } from '@services/intranet-search.service';
 import { MatTabChangeEvent } from '@angular/material/tabs';
 
 @Component({
-  selector: 'app-search-page',
-  templateUrl: './search-page.component.html',
-  styleUrls: ['./search-page.component.scss']
+  selector: 'app-intranet-search',
+  templateUrl: './intranet-search.component.html',
+  styleUrls: ['./intranet-search.component.scss']
 })
-export class SearchPageComponent implements OnInit, OnDestroy {
+export class IntranetSearchComponent implements OnInit, OnDestroy {
   public feedbackUrl = "https://docs.google.com/forms/d/e/1FAIpQLSdxSyxLBBzexHDgPmjoAukxDzDo3fRHfKi4TmqFHYxa0dB37g/viewform";
   public staffIntranet = "https://www.staff.auckland.ac.nz/";
 
-  public bannerImageUrl: string = 'https://images.ctfassets.net/vbuxn5csp0ik/dLNmMgxMJVJjdDATTpWZn/433ae5de80f78868c4fb37a256ed2801/1500_UoA_13Oct09_001.jpg';
-
-  public searchResults: SearchResult[] = [];
+  public searchResults: IntranetSearchResult[] = [];
   public totalResults: number;
   public searchText: string;
-  public activeFilters: SearchFilters;
   public sortOrder: SortOrder;
+
+  private resultsPerPage: number = 10;
   
   public loading: boolean = true;
 
@@ -33,16 +31,13 @@ export class SearchPageComponent implements OnInit, OnDestroy {
 
   constructor(
     public searchService: SearchService,
+    public intranetSearchService: IntranetSearchService,
     private route: ActivatedRoute,
     private scrollDispatcher: ScrollDispatcher,
-    private ngZone: NgZone,
-    public pageTitleService: PageTitleService
-  ) {
-    this.detectWebP();
-  }
+    private ngZone: NgZone
+  ) { }
 
   ngOnInit() {
-    this.pageTitleService.title = 'Search Results';
     /**
      * this subscription reacts to changes to the search parameters, i.e. new searches
      */
@@ -50,16 +45,11 @@ export class SearchPageComponent implements OnInit, OnDestroy {
       .pipe(
         tap(params => {
           this.searchText = params.get('q') || '';
-          this.activeFilters = {
-            category: params.getAll('cat'),
-            stage: params.getAll('ra'),
-            relatedOrgs: params.getAll('org')
-          }
           this.sortOrder = params.get('sort') as SortOrder || 'relevance';
           this.searchResults = [];
           this.totalResults = 0;
         }),
-        switchMap(() => this.search())
+        switchMap(() => this.intranetSearch())
       ).subscribe(searchResults => {
         this.searchResults = searchResults.results;
         this.totalResults = searchResults.totalResults;
@@ -78,7 +68,7 @@ export class SearchPageComponent implements OnInit, OnDestroy {
       concatMap(() => {
         return this.ngZone.run(() => {
           if (this.searchResults.length < this.totalResults) {
-            return this.search(10, this.searchResults.length);
+            return this.intranetSearch(this.resultsPerPage, this.searchResults.length / this.resultsPerPage + 1);
           } else {
             return EMPTY;
           }
@@ -94,35 +84,25 @@ export class SearchPageComponent implements OnInit, OnDestroy {
     }))
   }
 
-  detectWebP() {
-    supportsWebP.then(supported => {
-      this.bannerImageUrl = supported ? this.bannerImageUrl  + '?w=1900&fm=webp' : this.bannerImageUrl + '?w=1900';
-    });
-  }
-
   /**
    * Executes a search using the current search text, filters and sort order.
    *
    * @param size - The maximum amount of hits to be returned
-   * @param from - The offset from the first result
-   * @returns a SearchResults observable
+   * @param page - The page number of the results to be returned
+   * @returns an array observable
    *
    */
-  private search(size: number = 10, from: number = 0): Observable<SearchResults> {
+  private intranetSearch(size: number = 10, page: number = 1): Observable<IntranetSearchResults> {
     this.loading = true;
 
-    const contentTypes: ContentType[] = ['article', 'caseStudy', 'equipment', 'event', 'funding', 'service', 'software', 'subHub']
-
-    const searchQuery: SearchQuery = {
+    const searchQuery: IntranetSearchQuery = {
       query: this.searchText,
       size: size,
-      from: from,
-      filters: this.activeFilters,
-      sort: this.sortOrder,
-      includeContentTypes: contentTypes
+      page: page,
+      sort: this.sortOrder
     };
 
-    return this.searchService.search(searchQuery);
+    return this.intranetSearchService.search(searchQuery);
   }
 
   scrollToTop() {
