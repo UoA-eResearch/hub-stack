2c13a97a8599439f4d7d8b75d31a56a7d6e3d7a7
rosemcc <rosemccoll@hotmail.com>
2022-02-14 11:01:32 +1300

    unit tests WIP

--- research-hub-web/src/app/components/search-page/intranet-search-results-list/intranet-search-results-list.component.spec.ts
+++ research-hub-web/src/app/components/search-page/intranet-search-results-list/intranet-search-results-list.component.spec.ts
@@ -1,25 +1,71 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MockModule, MockComponent } from 'ng-mocks';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { IntranetSearchResultsListComponent } from './intranet-search-results-list.component';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatCardHarness } from '@angular/material/card/testing';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { By } from '@angular/platform-browser';
+import { NgxSkeletonLoaderComponent } from 'ngx-skeleton-loader';
 
 describe('IntranetSearchResultsListComponent', () => {
   let component: IntranetSearchResultsListComponent;
   let fixture: ComponentFixture<IntranetSearchResultsListComponent>;
 
+  let loader: HarnessLoader;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ IntranetSearchResultsListComponent ]
+      declarations: [
+        IntranetSearchResultsListComponent,
+        MockComponent(NgxSkeletonLoaderComponent)
+      ],
+      imports: [
+        HttpClientTestingModule,
+        MatCardModule,
+        MockModule(MatListModule),
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(IntranetSearchResultsListComponent);
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
+    component.searchResults = {
+      totalResults: 1,
+      results: [
+        {
+          url: 'https://www.google.co.nz/',
+          summary: 'test summary',
+          title: 'test title'
+        }
+      ]
+    }
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show skeleton when loading', () => {
+    const noLoading = fixture.debugElement.query(By.css('.skeleton-wrapper'));
+    expect(noLoading).toBeNull();
+
+    component.loading = true;
+    fixture.detectChanges();
+
+    const withLoading = fixture.debugElement.query(By.css('.skeleton-wrapper'));
+    expect(withLoading).toBeTruthy();
+  });
+
+  it('should show 1 results card for each search result', async () => {
+    const cards = await loader.getAllHarnesses(MatCardHarness.with({selector: '.card-content'}));
+
+    expect(cards.length).toBe(component.searchResults.totalResults);
   });
 });
--- research-hub-web/src/app/components/search-page/intranet-search/intranet-search.component.spec.ts
+++ research-hub-web/src/app/components/search-page/intranet-search/intranet-search.component.spec.ts
@@ -1,25 +1,71 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MaterialModule } from '@app/app.material.module';
+import { MockComponent, MockInstance, MockModule, MockProvider } from 'ng-mocks';
+import { NgxSkeletonLoaderComponent } from 'ngx-skeleton-loader';
+import { SearchFilterBarComponent } from './../search-filter-bar/search-filter-bar.component';
 import { IntranetSearchComponent } from './intranet-search.component';
+import { IntranetSearchResultsListComponent } from './../intranet-search-results-list/intranet-search-results-list.component';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from './../../shared/app.shared.module';
+import { IntranetSearchService } from '@services/intranet-search.service';
+import { LoginService } from '@uoa/auth';
+import { EMPTY } from 'rxjs';
 
-describe('IntranetSearchComponent', () => {
+fdescribe('IntranetSearchComponent', () => {
   let component: IntranetSearchComponent;
   let fixture: ComponentFixture<IntranetSearchComponent>;
+  let searchSpy: jasmine.Spy;
+
+  beforeAll(MockInstance.remember);
+  afterAll(MockInstance.restore);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ IntranetSearchComponent ]
+      declarations: [
+        IntranetSearchComponent,
+        MockComponent(NgxSkeletonLoaderComponent),
+        MockComponent(IntranetSearchResultsListComponent),
+        MockComponent(SearchFilterBarComponent)
+      ],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        MockModule(MaterialModule),
+        MockModule(FormsModule),
+        MockModule(CommonModule),
+        MockModule(SharedModule)
+      ],
+      providers: [
+        IntranetSearchService,
+        MockProvider(LoginService)
+      ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    MockInstance(LoginService, (instance) => {
+      instance.isAuthenticated = jasmine.createSpy().and.returnValue(Promise.resolve(false));
+      instance.loggedIn$ = EMPTY;
+      instance.userInfo$ = EMPTY;
+    });
+
     fixture = TestBed.createComponent(IntranetSearchComponent);
     component = fixture.componentInstance;
+
+    searchSpy = spyOn(component, 'search').and.callThrough();
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not call intranet search if not logged in', async () => {
+    expect(searchSpy).not.toHaveBeenCalled();
   });
 });
