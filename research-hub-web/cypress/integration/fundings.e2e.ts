9bc1592b7fa97ab79972e8c31f017ac246c8ff08
rosemcc <rosemccoll@hotmail.com>
2022-03-01 16:55:29 +1300

    WIP

--- research-hub-web/cypress/integration/fundings.e2e.ts
+++ research-hub-web/cypress/integration/fundings.e2e.ts
@@ -1,16 +1,31 @@
-import { aliasQuery } from '../utils/graphql-utils';
+import { aliasQuery, hasOperationName } from '../utils/graphql-utils';
 
 describe('ResearchHubs Funding Pages', () => {
 
     beforeEach(() => {
-        cy.intercept('POST', '/cer-graphql-service', (req) => {
+        cy.intercept('POST', 'http://localhost:4000/', (req) => {
             aliasQuery(req, 'GetFundingBySlug');
             req.reply({ fixture: 'funding' });
         });
+        
         cy.visit('/funding/internal-funding/hikina-kia-tutuki');
     });
 
     it('can visit a funding page and display its title', () => {
+        cy.intercept('POST', 'http://localhost:4000/', (req) => {
+            if (hasOperationName(req, 'GetFundingBySlug')) {
+                req.alias = 'gqlGetFundingBySlug';
+                req.reply({ fixture: 'funding' });
+            }
+        });
+
+        cy.visit('/internal-funding/hikina-kia-tutuki');
+
+        cy.wait('@gqlGetFundingBySlug').its('response.body.data.fundingCollection').should((funding) => {
+            console.log(funding)
+            expect(funding.items.length).to.equal(1);
+        });
+
         cy.get('h1.content-title').should('not.be.empty');
     });
 
