7fd9a40763144361d0f49d011a33186aaa887822
rosemcc <rosemccoll@hotmail.com>
2022-03-02 16:38:50 +1300

    subhub mock data and tests

--- research-hub-web/cypress/integration/subhubs.e2e.ts
+++ research-hub-web/cypress/integration/subhubs.e2e.ts
@@ -1,45 +1,59 @@
+import { hasOperationName } from '../utils/graphql-utils';
+
 describe('ResearchHubs SubHub Pages', () => {
+    beforeEach(() => {
+        console.log(Cypress.env('graphql_server'));
 
-  beforeEach(() => {
-    cy.visit('/subhub/communication-and-productivity');
-  });
+        cy.intercept('POST', Cypress.env('graphql_server'), (req) => {
+            if (hasOperationName(req, 'GetSubHubBySlug')) {
+                req.alias = 'gqlGetSubHubBySlug';
+                req.reply({ fixture: 'subhub' });
+            }
+        });
 
-  it('can visit an subhub and display its title', () => {
-    cy.get('h1.content-title').should('not.be.empty');
-  });
+        cy.visit('/subhub/research-impact');
 
-  it('can visit an subhub and display its subtitle', () => {
-    cy.get('#subhub-container .content-summary small').should('not.be.empty');
-  });
+        cy.wait('@gqlGetSubHubBySlug');
+    });
 
-  it('subhub displays body text', () => {
-    cy.get('.body-media-list > ng-component > ngx-contentful-rich-text > ng-component > p > ngx-contentful-rich-text > ng-component').should('not.be.empty');
-  });
+    it('can visit a subhub page and display the banner', () => {
+        cy.get('.banner-container').should('be.visible');
+    });
 
-  it('displays subhub children', () => {
-    cy.get('#subhub-children').should('exist');
-  });
+    it('can visit a subhub page and display its title', () => {
+        cy.get('h1.content-title').should('have.text', ' Subhub Title ');
+    });
 
-  it('clicking a subhub child takes you to its page', () => {
-    cy.get('#subhub-children>app-standard-card').first().click();
-    cy.get('h1.content-title').should('not.be.empty');
-  });
+    it('can visit a subhub page and display its subtitle', () => {
+        cy.get('.content-summary').should('exist');
+    });
+    
+    it('can visit a subhub page and display its Maori proverb', () => {
+        cy.get('.maori-proverb').should('exist').and('have.text', ' Maori Proverb ');
+    });
 
-  it('displays a list of related items', () => {
-    cy.get('#you-might-be-interested-in mat-nav-list:first-child .card-title').should('not.be.empty');
-  });
+    it('subhub page displays body text', () => {
+        cy.get('#subhub-body ng-component.ng-star-inserted p .ng-star-inserted').should('exist');
+    });
 
-  it('clicking a related item takes you to its page', () => {
-    cy.get('#you-might-be-interested-in>app-standard-card').first().click();
-    cy.get('h1.content-title').should('not.be.empty');
-  });
+    it('displays subhub children (internal pages) and external pages', () => {
+      cy.get('#subhub-children').should('exist');
+      cy.get('#subhub-children > app-standard-card').should('have.length', 2);
+    });
 
-  it('displays a list of contacts', () => {
-    cy.get('#contacts .card-title span').should('not.be.empty');
-  });
+    it('displays a list of related items', () => {
+        cy.get('#you-might-be-interested-in > app-standard-card').should('have.length', 1);
+    });
 
-  it('displays a list of organisations', () => {
-    cy.get('#organisations mat-nav-list:first-child a').should('not.be.empty');
-  });
+    it('displays a list of contacts', () => {
+        cy.get('app-contact-card').should('have.length', 1);
+    });
 
-});
+    it('displays a list of organisations', () => {
+        cy.get('app-org-unit-card').should('have.length', 1);
+    });
+
+    it('displays a list of documents', () => {
+        cy.get('app-document-card').should('have.length', 1);
+    });
+}); 
