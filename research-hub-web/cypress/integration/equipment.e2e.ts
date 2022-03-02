21bc21b946f2fced7ce4c6b5e92b216f27ae8779
rosemcc <rosemccoll@hotmail.com>
2022-03-03 10:36:30 +1300

    equipment mock data and tests

--- research-hub-web/cypress/integration/equipment.e2e.ts
+++ research-hub-web/cypress/integration/equipment.e2e.ts
@@ -1,19 +1,33 @@
-describe('ResearchHubs Equipment Pages', () => {
+import { hasOperationName } from '../utils/graphql-utils';
 
+describe('ResearchHubs Equipment Pages', () => {
     beforeEach(() => {
+        cy.intercept('POST', Cypress.env('graphql_server'), (req) => {
+            if (hasOperationName(req, 'GetEquipmentBySlug')) {
+                req.alias = 'gqlGetEquipmentBySlug';
+                req.reply({ fixture: 'equipment' });
+            }
+        });
+
         cy.visit('equipment/operetta-high-content-imaging-system');
+
+        cy.wait('@gqlGetEquipmentBySlug');
+    });
+
+    it('can visit an equipment page and display the banner', () => {
+        cy.get('.banner-container').should('be.visible');
     });
 
     it('can visit an equipment and display its title', () => {
-        cy.get('h1.content-title').should('not.be.empty');
+        cy.get('h1.content-title').should('exist').and('have.text', ' Equipment Title ');
     });
 
-    it('can visit an equipment page and display its subtitle', () => {
-        cy.get('.content-summary').should('not.be.empty');
+    it('can visit an equipment and display its subtitle', () => {
+        cy.get('#equipment-container .content-summary').should('exist');
     });
 
-    it('equipment displays body text', () => {
-      cy.get('.body-media-list > ng-component > ngx-contentful-rich-text > ng-component > p > ngx-contentful-rich-text > ng-component').should('not.be.empty');
+    it('displays a call to action button', () => {
+        cy.get('.standard-button > span').should('have.text', 'Test');
     });
 
     it('equipment displays specifications table', () => {
@@ -26,17 +40,23 @@ describe('ResearchHubs Equipment Pages', () => {
         cy.get('#specifications-table2').contains('Description').should('exist');
     });
 
-    it('displays a list of related items', () => {
-        cy.get('#you-might-be-interested-in mat-nav-list:first-child .card-title').should('not.be.empty');
+    it('equipment displays body text', () => {
+        cy.get('#equipment-body > ng-component > ngx-contentful-rich-text > ng-component > p > ngx-contentful-rich-text > ng-component').should('exist');
     });
 
-    it('clicking a related item takes you to its page', () => {
-      cy.get('#you-might-be-interested-in>app-standard-card').first().click();
-      cy.get('h1.content-title').should('not.be.empty');
-});
+    it('displays a list of related items', () => {
+        cy.get('#you-might-be-interested-in > app-standard-card').should('have.length', 1);
+    });
+
+    it('displays a list of contacts', () => {
+        cy.get('app-contact-card').should('have.length', 1);
+    });
 
     it('displays a list of organisations', () => {
-        cy.get('#organisations mat-nav-list:first-child a').should('not.be.empty');
+        cy.get('app-org-unit-card').should('have.length', 1);
     });
 
+    it('displays a list of documents', () => {
+        cy.get('app-document-card').should('have.length', 1);
+    });
 });
