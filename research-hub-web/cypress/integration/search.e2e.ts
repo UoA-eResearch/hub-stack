709baa94db225421de36b750b5a19d2008f135e9
rosemcc <rosemccoll@hotmail.com>
2022-03-03 12:25:47 +1300

    add search page tests

--- research-hub-web/cypress/integration/search.e2e.ts
+++ research-hub-web/cypress/integration/search.e2e.ts
@@ -3,11 +3,40 @@ describe('ResearchHubs Search Page', () => {
         cy.visit('/search?q=');
     });
 
+    it('displays banner image', () => {
+        cy.get('.banner-container').should('be.visible');
+    });
+
+    it('displays breadcrumbs', () => {
+        cy.get('app-breadcrumbs').should('exist')
+            .and('contain.text', 'Search');
+    });
+
+    it('displays number of results found', () => {
+        cy.get('.result-total > span > b').should('exist');
+    });
+
     it('search filters should exist', () => {
         cy.get('.sticky-bar').should('exist');
     });
 
+    it('displays number of search filters applied', () => {
+        cy.get('div.applied-filters > span > b').should('have.text', '0');
+
+        cy.visit('/search?q=test&cat=7HtS5VQdP1nWr5SRTIOOrS');
+
+        cy.get('div.applied-filters > span > b').should('have.text', '1');
+    });
+
     it('search order selector should exist', () => {
         cy.get('#order-picker').should('exist');
+    });
+
+    it('displays search results list', () => {
+        cy.get('#search-results > app-search-results-list').should('exist');
+    });
+
+    it('displays scroll to top button', () => {
+        cy.get('.floating-button').should('be.visible');
     });
 });
