a3b2a8c9d52a3465a8a49143e45ab8ef34250fa2
Rose <31844476+rosemcc@users.noreply.github.com>
2022-03-07 16:44:02 +1300

    E2e fixtures (#243)
    * WIP
    
    * WIP
    
    * set graphql server as ecypress env variable, clean up
    
    * bodyMediaList should be a class, fix naming style also, give body media specific ids for testing
    
    * improvements,  .should('not.be.empty') to .should('exist')
    
    * add more funding tests
    
    * add caseStudy mock and tests
    
    * subhub mock data and tests
    
    * article mock and tests, remove console logs
    
    * software mock and tests, remove example.json
    
    * events mock and tests
    
    * service mock data and tests
    
    * equipment mock data and tests
    
    * improved homepage tests
    
    * set wider viewport for testing
    
    * rename basic-functionality to navbar, add navbar tests
    
    * add sso content test
    
    * categories and activities pages tests
    
    * add search page tests
    
    * minor changes for routing tests
    
    * try to fix e2e fixtures on ci
    
    * still fixing ci
    
    * comments, remove unneeded log message
    
    * wording
    
    Co-authored-by: Lukas Trombach <19306765+Trombach@users.noreply.github.com>
    
    * Update research-hub-web/cypress/utils/graphql-utils.ts
    
    Co-authored-by: Lukas Trombach <19306765+Trombach@users.noreply.github.com>
    
    * Update research-hub-web/cypress/integration/navbar.e2e.ts
    
    Co-authored-by: Lukas Trombach <19306765+Trombach@users.noreply.github.com>
    
    * minor fix
    
    Co-authored-by: Lukas Trombach <19306765+Trombach@users.noreply.github.com>

--- research-hub-web/cypress/integration/service.e2e.ts
+++ research-hub-web/cypress/integration/service.e2e.ts
@@ -1,19 +1,33 @@
+import { hasOperationName } from '../utils/graphql-utils';
+
 describe('ResearchHubs services Pages', () => {
-
     beforeEach(() => {
+        cy.intercept('POST', Cypress.env('graphql_server'), (req) => {
+            if (hasOperationName(req, 'GetServiceBySlug')) {
+                req.alias = 'gqlGetServiceBySlug';
+                req.reply({ fixture: 'service' });
+            }
+        });
+
         cy.visit('/service/research-virtual-machines');
+
+        cy.wait('@gqlGetServiceBySlug');
     });
 
-    it('can visit an service and display its title', () => {
-        cy.get('h1.content-title').should('not.be.empty');
+    it('can visit a service page and display the banner', () => {
+        cy.get('.banner-container').should('be.visible');
     });
 
-    it('can visit an service and display its subtitle', () => {
-        cy.get('#service-container .content-summary').should('not.be.empty');
+    it('can visit a service and display its title', () => {
+        cy.get('h1.content-title').should('exist').and('have.text', ' Service Title ');
     });
 
-    it('service displays body text', () => {
-      cy.get('#bodyMediaList > ng-component > ngx-contentful-rich-text > ng-component > p > ngx-contentful-rich-text > ng-component').should('not.be.empty');
+    it('can visit a service and display its subtitle', () => {
+        cy.get('#service-container .content-summary').should('exist');
+    });
+
+    it('displays a call to action button', () => {
+        cy.get('.standard-button > span').should('have.text', 'Test');
     });
 
     it('service displays specifications table', () => {
@@ -21,25 +35,23 @@ describe('ResearchHubs services Pages', () => {
         cy.get('#specifications-table').contains('Description').should('exist');
     });
 
-    it('displays a list of related items', () => {
-        cy.get('#you-might-be-interested-in').should('exist');
+    it('service displays body text', () => {
+        cy.get('#service-body > ng-component > ngx-contentful-rich-text > ng-component > p > ngx-contentful-rich-text > ng-component').should('exist');
     });
 
-    it('clicking a related item takes you to its page', () => {
-      cy.get('#you-might-be-interested-in>app-standard-card').first().click();
-      cy.get('h1.content-title').should('not.be.empty');
-  });
+    it('displays a list of related items', () => {
+        cy.get('#you-might-be-interested-in > app-standard-card').should('have.length', 1);
+    });
 
     it('displays a list of contacts', () => {
-        cy.get('#contacts .card-title span').should('not.be.empty');
-    });
-
-    it('displays a list of documents', () => {
-        cy.get('#documents mat-nav-list:first-child a').should('not.be.empty');
+        cy.get('app-contact-card').should('have.length', 1);
     });
 
     it('displays a list of organisations', () => {
-        cy.get('#organisations mat-nav-list:first-child a').should('not.be.empty');
+        cy.get('app-org-unit-card').should('have.length', 1);
     });
 
+    it('displays a list of documents', () => {
+        cy.get('app-document-card').should('have.length', 1);
+    });
 });
