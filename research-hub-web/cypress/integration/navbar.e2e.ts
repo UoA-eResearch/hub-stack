d5dddd918e0ce8a77aeceda6ee64522226d57eec
Rose <31844476+rosemcc@users.noreply.github.com>
2022-03-07 16:36:28 +1300

    Update research-hub-web/cypress/integration/navbar.e2e.ts
    Co-authored-by: Lukas Trombach <19306765+Trombach@users.noreply.github.com>

--- research-hub-web/cypress/integration/navbar.e2e.ts
+++ research-hub-web/cypress/integration/navbar.e2e.ts
@@ -9,7 +9,7 @@ describe('ResearchHubs NavBar', () => {
     });
     
     it('The ResearchHub logo is displayed', () => {
-        cy.get('[alt="hub-logo"]')
+        cy.get('a.hub-logo > img')
             .should('be.visible')
             .invoke('width')
             .should('be.greaterThan', 0);
