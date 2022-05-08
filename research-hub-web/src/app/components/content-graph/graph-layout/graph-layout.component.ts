065913b0a6d4bde97eebcc3dc0b89d63c89155a2
Lukas Trombach <19306765+Trombach@users.noreply.github.com>
2022-05-09 09:52:25 +1200

    Graph playground (#272)
    * initial commit for content graph
    
    * added resolver and adjustments, highlight node on hover
    
    * update lockfile version
    
    * minor fixes for null checks
    
    * added auth guard to graph route
    
    * first version of node info box
    
    * added node highlighting
    
    * improved details list and highlighting
    
    * first version of graph legend in drawer
    
    * layout fixes
    
    * layout improvements
    
    * renamed graph-legend to graph-filter
    
    * added legend, added dev env
    
    * added graph link to footer
    
    * fixed capitalisation
    
    * some fixes for unit tests
    
    * more fixes for tests
    
    * exclude GraphFilter test for now
    
    * added graph API to CSP headers (dev)
    
    * renamed graph-container
    
    * refactored graph into component
    
    * tidy up field order
    
    * changed loading behaviour, removed route resolver
    
    * fixed canvas width, colour changes
    
    * fixed color legend and search box
    
    * minor improvements
    
    * many UI improvements, added contentful link to env files
    
    * updated test and prod environment files with graph api url
    
    * some fixes for unit tests
    
    * more fixes for unit tests
    
    * minor improvements for node details UI
    
    * added graph api to csp headers in cloudfront functions
    
    * rebuild package-lock.json
    
    * added esbuild dep

---
+++ research-hub-web/src/app/components/content-graph/graph-layout/graph-layout.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { ContentNode } from '@services/content-graph.service';
+
+@Component({
+  selector: 'app-graph-layout',
+  template: `
+    <mat-drawer-container>
+      <mat-drawer mode="side" opened>
+        <div class="graph-drawer-container">
+          <h1>HubGraph</h1>
+          <app-color-legend [colorMap]="colorMap"></app-color-legend>
+          <app-graph-filter [nodes]="nodes" [(selectedNode)]="selectedNode"></app-graph-filter>
+          <app-node-details *ngIf="selectedNode" [(node)]="selectedNode" [colorMap]="colorMap"></app-node-details>
+        </div>
+      </mat-drawer>
+      <mat-drawer-content>
+        <app-graph-container [(nodes)]="nodes" [(selectedNode)]="selectedNode" [colorMap]="colorMap"></app-graph-container>
+      </mat-drawer-content>
+    </mat-drawer-container>
+  `,
+  styles: [
+    `h1 {font-size: 52px;}`,
+    `mat-drawer-container {height: calc(100vh - 64px); width: 100vw}`,
+    `mat-drawer {width: 30vw; max-width: 500px;}`,
+    `.graph-drawer-container {
+      margin-bottom: 25px;
+      padding: 0 20px;
+      width: 100%;
+    }`
+  ]
+})
+export class GraphLayoutComponent {
+  public nodes: ContentNode[] = [];
+  public selectedNode: ContentNode | null;
+
+  // colorbrewer qualitative Set1
+  private colors = ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf'];
+
+  public colorMap = new Map([
+    ['subHub', this.colors[0]],
+    ['article', this.colors[1]],
+    ['event', this.colors[2]],
+    ['equipment', this.colors[3]],
+    ['funding', this.colors[4]],
+    ['service', this.colors[5]],
+    ['software', this.colors[6]],
+    ['caseStudy', this.colors[7]],
+  ]);
+}
