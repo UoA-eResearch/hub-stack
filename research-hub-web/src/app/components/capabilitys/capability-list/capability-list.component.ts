b7b35b71a0b2e8c62c1cef85827d882e7f032b56
Lukas Trombach <lukas.trombach@auckland.ac.nz>
2022-11-23 13:56:39 +1300

    add capability list

--- research-hub-web/src/app/components/capabilitys/capability-list/capability-list.component.ts
+++ research-hub-web/src/app/components/capabilitys/capability-list/capability-list.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AllCapabilitiesGQL, CapabilityCollection } from '@app/graphql/schema';
+import { PageTitleService } from '@services/page-title.service';
+import { map, Observable, Subscription, tap } from 'rxjs';
 
 @Component({
   selector: 'app-capability-list',
   templateUrl: './capability-list.component.html',
   styleUrls: ['./capability-list.component.scss']
 })
-export class CapabilityListComponent implements OnInit {
+export class CapabilityListComponent implements OnInit, OnDestroy {
+  public capabilities: CapabilityCollection;
+  public title: string = 'Capability Collection';
 
-  constructor() { }
+  private subscription = new Subscription();
+
+  constructor(
+    private allCapabilitiesGQL: AllCapabilitiesGQL,
+    public pageTitleService: PageTitleService
+  ) { }
 
   ngOnInit(): void {
+    this.pageTitleService.title = this.title;
+    this.subscription.add(
+      this.loadContent().subscribe((collection) => this.capabilities = collection)
+    );
+  }
+
+  public loadContent(): Observable<CapabilityCollection> {
+    return this.allCapabilitiesGQL.fetch().pipe(
+      map((result) => result.data.capabilityCollection as CapabilityCollection),
+      tap((c) => console.log(c))
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
