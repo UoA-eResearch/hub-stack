c7535533b9156f8af62e75a672477d830a45feaa
Lukas Trombach <lukas.trombach@auckland.ac.nz>
2022-11-24 15:51:24 +1300

    add capability list unit test

--- research-hub-web/src/app/components/capabilitys/capability-list/capability-list.component.spec.ts
+++ research-hub-web/src/app/components/capabilitys/capability-list/capability-list.component.spec.ts
@@ -1,12 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CapabilityCollection } from '@app/graphql/schema';
 import { PageTitleService } from '@services/page-title.service';
 import { MockProvider } from 'ng-mocks';
+import { Observable, of } from 'rxjs';
 
 import { CapabilityListComponent } from './capability-list.component';
 
 describe('CapabilityListComponent', () => {
   let component: CapabilityListComponent;
   let fixture: ComponentFixture<CapabilityListComponent>;
+
+  const mockAllCapabilities$: Observable<CapabilityCollection> = of({
+    'items': [
+      {
+        '__typename': 'Capability',
+        'slug': 'first-capability',
+        'title': 'First capability',
+        'summary': 'A brief description of the first capability. I\'m writing some more stuff here just so that this seems a little more realistic. Sam was here. Have a good day.',
+        'ssoProtected': false,
+        'searchable': true
+      },
+      {
+        '__typename': 'Capability',
+        'slug': 'top-secret-capability',
+        'title': 'Top Secret Capability',
+        'summary': 'For testing SSO',
+        'ssoProtected': true,
+        'searchable': true
+      }
+    ],
+    '__typename': 'CapabilityCollection'
+  } as CapabilityCollection);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -26,5 +50,15 @@ describe('CapabilityListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should get all capabilities', () => {
+    spyOn(component, 'loadContent').and.returnValue(mockAllCapabilities$);
+
+    fixture.whenStable().then(() => {
+      component.loadContent().subscribe(res => {
+        expect(res.items.length).toBe(1);
+      });
+    })
   });
 });
