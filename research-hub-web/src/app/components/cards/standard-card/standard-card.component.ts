ec1157270e40867e13198ef56ec78a57537e0597
Lukas Trombach <lukas.trombach@auckland.ac.nz>
2022-11-30 17:33:02 +1300

    fix standard card default image loading

--- research-hub-web/src/app/components/cards/standard-card/standard-card.component.ts
+++ research-hub-web/src/app/components/cards/standard-card/standard-card.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { Article, Capability, CaseStudy, Equipment, Event, Funding, Service, Software, SubHub } from '@app/graphql/schema';
+import { Component, Input, OnInit } from '@angular/core';
+import { Article, Capability, CaseStudy, Equipment, Event, Funding, GetAssetByIdGQL, Service, Software, SubHub } from '@app/graphql/schema';
+import { defaultIfEmpty, EMPTY, filter, map, Observable } from 'rxjs';
 
 export type PossibleContentItems
   = Article
@@ -20,21 +21,45 @@ export type PossibleContentItems
     '../cards-common.scss'
   ]
 })
-export class StandardCardComponent {
+export class StandardCardComponent implements OnInit {
   @Input() contentItem: PossibleContentItems;
   @Input() isSubhubChild = false;
 
-  public defaultImage = new Map<Exclude<PossibleContentItems['__typename'], undefined>, string>([
-    ['Article', 'https://images.ctfassets.net/vbuxn5csp0ik/7dPrwEcbk56xKfz5zTLvEP/0efddb4b6c9e1eda80d2fb8d1ee47275/card-background-article.png'],
-    ['Capability', 'https://images.ctfassets.net/vbuxn5csp0ik/QT1QGR7KkqaSswnmg7L97/d90bdfb944aa0f18640553a21cbf45a5/card-background-capability.png'],
-    ['CaseStudy', 'https://images.ctfassets.net/vbuxn5csp0ik/2qmi1RS1lZSgXj9xP47h8E/54eb4e54bfc00d26f34401293af1ed80/card-background-case-study.png'],
-    ['Equipment', 'https://images.ctfassets.net/vbuxn5csp0ik/1aSspX7erQzo9jVKStvwO9/6b141535dc463e8af1394f269100b9d7/card-background-equipment.png'],
-    ['Event', 'https://images.ctfassets.net/vbuxn5csp0ik/lwzGPgcdAAwSIR7PruzdG/de3e9093f3504a0e64d12a7751d5752e/card-background-event.png'],
-    ['Funding', 'https://images.ctfassets.net/vbuxn5csp0ik/3n0YDaiEq2xCtp8sols7LG/2ff1b20bb49bfc0da132f2500c8ab0eb/card-background-funding.png'],
-    ['Service', 'https://images.ctfassets.net/vbuxn5csp0ik/5NwXkH2EEsYbGj20IefKyp/102be11747b9de2e07980926e4498d27/card-background-service.png'],
-    ['Software', 'https://images.ctfassets.net/vbuxn5csp0ik/16eyRnz65svAAyUF08yfAZ/ea0cff6e1c51e1800e7d210168532516/card-background-software.png'],
-    ['SubHub', 'https://images.ctfassets.net/vbuxn5csp0ik/4A7fKybLu0221iqacf7BAz/df2882a80873bea2e3297e7ebc3f6f41/card-background-subhub.png']
-  ]);
+  defaultImageUrl$: Observable<string>;
+  url: string | null | undefined;
 
-  constructor() { }
+  private readonly fallbackUrl = 'https://images.ctfassets.net/vbuxn5csp0ik/7dPrwEcbk56xKfz5zTLvEP/0efddb4b6c9e1eda80d2fb8d1ee47275/card-background-article.png';
+
+  private readonly defaultImageId: Record<Exclude<PossibleContentItems['__typename'], undefined>, string> = {
+    'Article': '7dPrwEcbk56xKfz5zTLvEP',
+    'Capability': 'QT1QGR7KkqaSswnmg7L97',
+    'CaseStudy': '2qmi1RS1lZSgXj9xP47h8E',
+    'Equipment': '1aSspX7erQzo9jVKStvwO9',
+    'Event': 'lwzGPgcdAAwSIR7PruzdG',
+    'Funding': '3n0YDaiEq2xCtp8sols7LG',
+    'Service': '5NwXkH2EEsYbGj20IefKyp',
+    'Software': '16eyRnz65svAAyUF08yfAZ',
+    'SubHub': '4A7fKybLu0221iqacf7BAz'
+  }
+
+  constructor(private getAssetById: GetAssetByIdGQL) { }
+
+  ngOnInit(): void {
+    this.url = this.contentItem.banner?.url;
+    this.defaultImageUrl$ = this.getDefaultImageUrl(this.contentItem.__typename);
+  }
+
+  private getAssetId(type: Exclude<PossibleContentItems['__typename'], undefined>): string {
+    return this.defaultImageId[type];
+  }
+
+  private getDefaultImageUrl(type: PossibleContentItems['__typename']): Observable<string> {
+    if (!type) {
+      type = 'Article';
+    }
+    const id = this.getAssetId(type);
+    return this.getAssetById.fetch({ id }).pipe(
+      map(result => result.data.asset?.url ? result.data.asset.url : this.fallbackUrl),
+    )
+  }
 }
