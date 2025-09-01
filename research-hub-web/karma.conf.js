cb11947c4d1e72c2b1ec5b19fb1079cd78699b5d
Lynette Nguyen <lynette.nguyen@auckland.ac.nz>
2025-09-01 13:05:32 +1200

    Change test browser for deployment

--- research-hub-web/karma.conf.js
+++ research-hub-web/karma.conf.js
@@ -58,7 +58,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
+    browsers: ['ChromeHeadless', 'ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: "ChromeHeadless",
