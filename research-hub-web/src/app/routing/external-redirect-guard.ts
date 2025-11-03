f411791638a6e07284576ab6e4b2ddf3971846dd
Lynette Nguyen <lynette.nguyen@auckland.ac.nz>
2025-11-03 13:01:59 +1300

    Add external URL redirect

---
+++ research-hub-web/src/app/routing/external-redirect-guard.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ExternalRedirectGuard implements CanActivate {
+    constructor(private router: Router) {}
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+        window.location.href = route.data['externalUrl'];
+        return true;
+    }
+}
