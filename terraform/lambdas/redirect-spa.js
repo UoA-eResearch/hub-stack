364d57541c91c01d949ccc0b073ca72b9a3c404d
Lynette Nguyen <lynette.nguyen@auckland.ac.nz>
2025-11-05 09:49:28 +1300

    Add URL redirects for server-side routing

--- terraform/lambdas/redirect-spa.js
+++ terraform/lambdas/redirect-spa.js
@@ -2,6 +2,26 @@
 
 function handler(event) {
     var request = event.request;
+    var headers = request.headers;
+    var host = headers.host && headers.host.value ? headers.host.value : 'research-hub.auckland.ac.nz'; // replace with hardcoded host 
+    
+    
+    if (request.uri === '/service/media-productions') { //SCTASK0366067
+        return {
+            statusCode: 302,
+            statusDescription: 'Found',
+            headers: { 'location': { 'value' : 'https://www.auckland.ac.nz/en/intranet/services/buying-payment/approved-suppliers/photographers-videographers-designers.html' } }
+        };   
+    } 
+    
+    if (request.uri === '/platforms' || request.uri === '/article/share') { //SCTASK0366060, RSM-4221
+        return {
+            statusCode: 302,
+            statusDescription: 'Found',
+            headers: { 'location': { 'value' : `https://${host}/subhub/research-platforms` } }
+        }
+    }
+    
     
     if (!hasExtension(request.uri)) {
         request.uri = '/index.html';
